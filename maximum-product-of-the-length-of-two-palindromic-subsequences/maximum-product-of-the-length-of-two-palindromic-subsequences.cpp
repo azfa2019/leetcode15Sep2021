ccdf7efbe1934993dd83fbd485588f4df814ce25
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-09-16 15:58:00 -0700

    Time: 112 ms (82.27%), Space: 7 MB (35.36%) - LeetHub

--- maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,13 +1,16 @@
 class Solution {
   public:
   int maxProduct(string s) {
-    int n=s.size(),m=1<<n; //m: total # of states
-    vector<pair<int,int>>list; //list saves all possible palindromic subsequences
+    int n=s.size();
+    int m=(1<<n); //# of states
+    vector<pair<int,int>>list;//saves all palindromic subsequences, first: state; second=# of 1s, or the length of the palindrome
+
     for(int i=1;i<m;i++){
-      if(check(s,i)) 
-        list.push_back({i,__builtin_popcount(i)});
+      if(check(s,i)) list.push_back({i,__builtin_popcount(i)});
     }
-    int res=0,len=list.size();
+
+    int len=list.size();
+    int res=0;
     for(int i=0;i<len;i++){
       int x=list[i].first,lenx=list[i].second;
       for(int j=i+1;j<len;j++){
@@ -17,15 +20,14 @@ class Solution {
     }
     return res;
   }
-  bool check(string& s,int state){
+  bool check(string&s,int state){
     int left=0,right=s.size()-1;
     while(left<right){
-      while(left<right&&(state&(1<<left))==0) left++;
-      while(left<right&&(state&(1<<right))==0) right--;
+      while(left<right && (state&(1<<left))==0) left++;
+      while(left<right && (state&(1<<right))==0) right--;
       if(s[left]!=s[right]) return false;
       left++,right--;
-      }
+    }
     return true;
   }
-
 };
