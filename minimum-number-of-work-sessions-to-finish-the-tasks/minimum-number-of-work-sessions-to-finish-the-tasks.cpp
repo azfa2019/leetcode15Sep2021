58047f55233f5191b3ea6eb6066ae0d5e3a0853c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-09-16 02:03:25 -0700

    Time: 1831 ms (5.01%), Space: 9.2 MB (58.27%) - LeetHub

---
+++ minimum-number-of-work-sessions-to-finish-the-tasks/minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSessions(vector<int>& tasks, int sessionTime) {
+      int n=tasks.size();
+      int m=(1<<n);
+      vector<int>dp(m,INT_MAX/2);
+      for(int state=0;state<m;state++){
+        int sum=0;
+        for(int i=0;i<n;i++)
+          if((state>>i)&1) sum+=tasks[i];
+        if(sum<=sessionTime) dp[state]=1;
+      }
+      for(int state=1;state<m;state++){
+        for(int subset=state;subset>0;subset=(subset-1)&state)
+          dp[state]=min(dp[state],dp[subset]+dp[state-subset]);
+      }
+      return dp[m-1];
+    }
+};
