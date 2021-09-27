601e59a19630572ea5545a3eb821fce2aeb78237
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-09-27 07:29:27 -0700

    Time: 64 ms (91.82%), Space: 43.3 MB (67.25%) - LeetHub

--- design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -4,36 +4,38 @@ class WordDictionary {
     Trie* children[26]={};
     bool isWord=false;
     Trie(){
+      
     }
   };
   Trie* root;
-  public:
-  WordDictionary():root(new Trie()) {
-
-  }
-
-  void addWord(string word) {
-    Trie* node=root;
-    for(char c:word){
-      if(node->children[c-'a']==nullptr) node->children[c-'a']=new Trie();
-      node=node->children[c-'a'];
+public:
+    WordDictionary():root(new Trie()) {
+        
     }
-    node->isWord=true;
-  }
-
-  bool search(string word) {
-    Trie* node=root;
-    return find(node,word,0);
-  }
-  bool find(Trie* node,string& word,int idx){
+    
+    void addWord(string word) {
+      Trie* node=root;
+      for(char c:word){
+        if(node->children[c-'a']==nullptr) node->children[c-'a']=new Trie();
+        node=node->children[c-'a'];
+      }
+      node->isWord=true;
+    }
+    
+    bool search(string word) {
+      Trie* node=root;
+      return find(node,0,word);
+    }
+  bool find(Trie* node,int idx,string& word){
     if(node==nullptr) return false;
     if(idx==word.size()) return node->isWord;
     if(word[idx]=='.'){
-      for(auto next:node->children)
-        if(find(next,word,idx+1)) return true;
+      for(auto next:node->children){
+        if(find(next,idx+1,word)) return true;
+      }
     }else{
-      Trie* next=node->children[word[idx]-'a'];
-      return find(next,word,idx+1);
+      auto next=node->children[word[idx]-'a'];
+      return find(next,idx+1,word);
     }
     return false;
   }
