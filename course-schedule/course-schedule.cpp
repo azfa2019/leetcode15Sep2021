d1e93f94eb25f056d09a3afba6a25461270f26e2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-09-26 22:33:37 -0700

    Time: 16 ms (94.59%), Space: 14.5 MB (32.88%) - LeetHub

---
+++ course-schedule/course-schedule.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+  bool canFinish(int numCourses, vector<vector<int>>& pre) {
+    vector<vector<int>>graph(numCourses);
+    for(auto p:pre) graph[p[1]].push_back(p[0]);
+    vector<int>flags(numCourses,0);
+    for(int i=0;i<numCourses;i++)
+      if(!dfs(i,flags,graph)) return false;
+    return true;
+  }
+  bool dfs(int i,vector<int>&flags,vector<vector<int>>&graph){
+    if(flags[i]==-1) return true;
+    if(flags[i]==1) return false;
+    flags[i]=1;
+    for(int next:graph[i])
+      if(!dfs(next,flags,graph)) return false;
+    flags[i]=-1;
+    return true;
+  }
+};
+//class Solution {
+//  public:
+//  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//    //topological sort
+//    //inDegree: map
+//    //source: vector
+//    //res: vector
+//    //graph: map
+//    //initialize the graph and inDegree
+//    unordered_map<int,int>inDegree;
+//    unordered_map<int,vector<int>>graph;
+//    vector<int>res;
+//    queue<int>source;
+//    
+//    for(int i=0;i<numCourses;i++) inDegree[i]=0;
+//    for(int i=0;i<prerequisites.size();i++) {
+//      int k=prerequisites[i][0];
+//      int v=prerequisites[i][1];
+//      graph[k].push_back(v);
+//      inDegree[v]++;
+//    }
+//    for(auto e:inDegree) if(e.second==0) source.push(e.first);
+//    while(!source.empty()){
+//      int cur=source.front();
+//      source.pop();
+//      res.push_back(cur);
+//      for(int child:graph[cur]){
+//        inDegree[child]--;
+//        if(inDegree[child]==0) source.push(child);
+//      }
+//    }
+//    return res.size()==numCourses;
+//  }
+//};
