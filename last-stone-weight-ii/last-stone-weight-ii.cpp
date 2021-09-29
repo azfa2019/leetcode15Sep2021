7f60c27a656e969696e7bf0a52c3f1143857b579
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-09-28 17:13:37 -0700

    Time: 4 ms (82.33%), Space: 8 MB (92.96%) - LeetHub

--- last-stone-weight-ii/last-stone-weight-ii.cpp
+++ last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
-  public:
-  int lastStoneWeightII(vector<int>& stones) {
-    unordered_set<int>combo{0};
-    for(int stone:stones){
-      unordered_set<int>tmp;
-      for(int x:combo){
-        tmp.insert(x+stone);
-        tmp.insert(x-stone);
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+      vector<bool>dp(3000,false);
+      dp[0]=true;
+      int sum=0;
+      for(int stone:stones){
+        sum+=stone;
+      //cout<<sum<<endl;
+        for(int weight=sum;weight>=stone;weight--){
+          if(dp[weight-stone]) dp[weight]=true;
+        }
       }
-      combo=move(tmp);
-      //for(int i:combo) cout<<i<<" ";cout<<endl;
+      for(int w=sum/2;w>=0;--w){
+        if(dp[w]) return sum-2*w;
+      }
+      return 0;
     }
-    int ans=INT_MAX;
-    for(int i:combo){
-      if(i>=0) ans=min(ans,i);
-    }
-    return ans;
-  }
 };
