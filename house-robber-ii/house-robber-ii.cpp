b2f3b52f971a9bf23e9f26d070d5286a45047f33
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-10-07 08:05:29 -0700

    Time: 0 ms (100.00%), Space: 8 MB (16.17%) - LeetHub

--- house-robber-ii/house-robber-ii.cpp
+++ house-robber-ii/house-robber-ii.cpp
@@ -4,15 +4,17 @@ class Solution {
     int n=nums.size();
     if(n==1) return nums[0];
     if(n==2) return max(nums[0],nums[1]);
-    vector<vector<int>>dp(n,vector<int>(n,0));//dp[i][j]: max money in interval i:j
-    for(int i=0;i<n;i++) dp[i][i]=nums[i];
-    for(int len=2;len<=n;len++){
-      for(int i=0;i+len-1<=n-1;i++){
-        int j=i+len-1;
-        dp[i][j]=max(dp[i+1][j],nums[i]+((i+2==n)?0:dp[i+2][j])); //when len=2, j=i+1
-      }
+    vector<int>dp(n+1,0);
+    /// 0 1 2 3 4
+    return max(helper(0,n-2,nums),helper(1,n-1,nums));
+  }
+  int helper(int start,int end,vector<int>&nums){
+    //cout<<start+2<<","<<end<<endl;
+    vector<int>dp(end+1,0);
+    dp[start]=nums[start];
+    for(int i=start+1;i<=end;i++){
+      dp[i]=max(i-2<0?0:dp[i-2]+nums[i],dp[i-1]);
     }
-    return max(dp[0][n-2],dp[1][n-1]);
+    return dp[end];
   }
 };
-
