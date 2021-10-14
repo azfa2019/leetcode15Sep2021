1f0346237ed3a6120cbc3a7033469d382c917b12
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-10-14 08:00:38 -0700

    Time: 19 ms (42.72%), Space: 12.9 MB (73.79%) - LeetHub

---
+++ coin-path/coin-path.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> cheapestJump(vector<int>& coins, int maxJump) {
+      //dp[i]: min cost when starting from i to reach the end
+      //dp[i]=min(dp[i],dp[i+x]+coins[i]) for all x: 1:maxJump
+      int n=coins.size();
+      vector<int>dp(n,INT_MAX/2);
+      vector<int>path(n,-1);
+      //path[i]: the next index position when starting from index i
+      dp[n-1]=coins[n-1];
+      for(int i=n-1;i>=0;i--){
+        for(int k=1;k<=maxJump;k++){
+          if(i+k>=coins.size()) continue;
+          if(coins[i+k]==-1) continue;
+          if(dp[i+k]+coins[i]<dp[i]){
+            dp[i]=dp[i+k]+coins[i];
+            path[i]=i+k;
+          }
+        }
+      }
+      //for(int i:path)
+      //  cout<<i<<endl;
+      vector<int>ans;
+      int i=0;
+      while(i<coins.size()){
+        ans.push_back(i+1);//when index=0, ouput 1 (1 based)
+        if(i==coins.size()-1) break;
+        if(path[i]==-1) return {};
+        else i=path[i];
+      }
+      return ans;
+    }
+};
