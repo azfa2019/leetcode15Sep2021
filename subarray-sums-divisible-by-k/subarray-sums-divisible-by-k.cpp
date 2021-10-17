e4a13176e5625db96f1580e02a0b45908f19500b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-10-17 12:57:12 -0700

    Time: 48 ms (84.00%), Space: 31.7 MB (26.98%) - LeetHub

---
+++ subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+      int prefix=0,ans=0;
+      unordered_map<int,int>map;
+      map[0]=1;
+      for(int num:nums){
+        prefix=(prefix+num%k+k)%k;
+        ans+=map[prefix];
+        map[prefix]++;
+      }
+      return ans;
+        
+    }
+};
