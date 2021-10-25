039b450f2e39f3977875c61dec7b2fef8bdc0a14
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-10-24 23:52:43 -0700

    Time: 31 ms (53.53%), Space: 10 MB (55.80%) - LeetHub

--- combinations/combinations.cpp
+++ combinations/combinations.cpp
@@ -1,20 +1,21 @@
 class Solution {
+  vector<vector<int>>ans;
+  vector<int>cur;
   public:
   vector<vector<int>> combine(int n, int k) {
-    vector<vector<int>>ans;
-    vector<int>cur{};
-    dfs(0,n,k,cur,ans);
+    cur={};
+    dfs(1,k,n);
     return ans;
   }
-  void dfs(int curPos,int n,int k,vector<int>&cur,vector<vector<int>>&ans){
+  void dfs(int idx,int k,int n){
+    //if(idx==n+1) return;
     if(cur.size()==k) {
       ans.push_back(cur);
       return;
     }
-    if(curPos==n) return;
-    for(int i=curPos+1;i<=n;i++){
+    for(int i=idx;i<=n;i++){
       cur.push_back(i);
-      dfs(i,n,k,cur,ans);
+      dfs(i+1,k,n);
       cur.pop_back();
     }
   }
