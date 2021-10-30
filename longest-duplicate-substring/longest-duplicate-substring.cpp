e46426c078610bc8cf8f5be217f5c2a42c997989
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-10-29 21:38:00 -0700

    Time: 824 ms (80.20%), Space: 200 MB (66.44%) - LeetHub

--- longest-duplicate-substring/longest-duplicate-substring.cpp
+++ longest-duplicate-substring/longest-duplicate-substring.cpp
@@ -1,28 +1,26 @@
 typedef uint64_t ULL;
 class Solution {
   unordered_map<int,int>len2start;
-public:
-    string longestDupSubstring(string s) {
-      int left=1,right=s.size()-1;
-      while(left<right){
-        int mid=left+(right-left+1)/2;
-        if(found(s,mid)) left=mid;
-        else right=mid-1;
-      }
-      if(found(s,left)){
-        return s.substr(len2start[left],left);
-      }else return "";
+  public:
+  string longestDupSubstring(string s) {
+    int left=1,right=s.size()-1;
+    while(left<right){
+      int mid=left+(right-left+1)/2;
+      if(found(s,mid)) left=mid;
+      else right=mid-1;
     }
+    if(found(s,left)) return s.substr(len2start[left],left);
+    else return "";
+  }
   bool found(string&s,int len){
-    unordered_set<ULL>set0;
+    ULL base=31;
     ULL hash=0;
     ULL powBase=1;
-    ULL base=31;
+    unordered_set<ULL>set0;
     for(int i=0;i<len-1;i++) powBase*=base;
     for(int i=0;i<s.size();i++){
-      if(i>=len)
-        hash-=powBase*(s[i-len]-'a');
-      hash=hash*base+s[i]-'a';
+      if(i>=len) hash-=powBase*(s[i-len]-'a');
+      hash=hash*base+(s[i]-'a');
       if(i>=len-1){
         if(set0.find(hash)!=set0.end()){
           len2start[len]=i-len+1;
