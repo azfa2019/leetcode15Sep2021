305bc5e9823e3ce773a2ab0f7895abae4e9c1879
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-01 07:04:15 -0700

    Time: 4 ms (96.60%), Space: 8.9 MB (55.55%) - LeetHub

--- redundant-connection/redundant-connection.cpp
+++ redundant-connection/redundant-connection.cpp
@@ -1,39 +1,37 @@
-class UnionFind{
+class UF{
   public:
   vector<int>parent;
   vector<int>rank;
-  UnionFind(int n){
+  UF(int n){
     parent=vector<int>(n+1,0);
     rank=vector<int>(n+1,0);
     for(int i=0;i<parent.size();i++) parent[i]=i;
   }
-  bool unionnodes(int n1,int n2){
-    int p1=findroot(n1);
-    int p2=findroot(n2);
+  bool unionNodes(int n1,int n2){
+    int p1=findRoot(n1);
+    int p2=findRoot(n2);
     if(p1==p2) return false;
-    if(rank[p1]<rank[p2]) parent[p1]=p2;
-    else if(rank[p1]>rank[p2]) parent[p2]=p1;
-    else{
+    if(rank[p1]<rank[p2]) {
+      parent[p1]=p2;
+    }else if(rank[p1]>rank[p1]) parent[p2]=p1;
+    else {
       parent[p2]=p1;
       rank[p1]++;
     }
     return true;
   }
-  int findroot(int n){
-    if(n!=parent[n] ) parent[n]=findroot(parent[n]);
+  int findRoot(int n){
+    if(parent[n]!=n) parent[n]=findRoot(parent[n]);
     return parent[n];
   }
-  
 };
 class Solution {
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-      UnionFind uf=UnionFind(edges.size());
-      for (auto e: edges){
-        for(auto e:edges){
-          if(!uf.unionnodes(e[0],e[1])) return e;
-        }
-      }
-      return {};
+  public:
+  vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+    UF uf0=UF(edges.size());
+    for(auto e:edges){
+        if(!uf0.unionNodes(e[0],e[1])) return e;
     }
+    return {};
+  }
 };
