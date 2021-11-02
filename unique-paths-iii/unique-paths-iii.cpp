3d5d5f6252504918be6faf1a0fb71538fd21d9fe
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-01 20:29:26 -0700

    Time: 0 ms (100.00%), Space: 7.1 MB (36.83%) - LeetHub

--- unique-paths-iii/unique-paths-iii.cpp
+++ unique-paths-iii/unique-paths-iii.cpp
@@ -1,27 +1,25 @@
 class Solution {
   int m,n;
-  public:
-  int uniquePathsIII(vector<vector<int>>& grid) {
-    m=grid.size();
-    n=grid[0].size();
-    int startx,starty,step=1;
-    for(int i=0;i<m;i++){
-      for(int j=0;j<n;j++){
-        if(grid[i][j]==1) {
-          startx=i;
-          starty=j;
-          continue;
+public:
+    int uniquePathsIII(vector<vector<int>>& grid) {
+      m=grid.size(),n=grid[0].size();
+      int step=1,startx,starty;
+      for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+          if(grid[i][j]==1) {
+            startx=i,starty=j;
+         //   continue;
+          }
+          if(grid[i][j]==0) step++;
         }
-        if(grid[i][j]==0) step++;
       }
+      return dfs(startx,starty,grid,step);
     }
-    return dfs(startx,starty,grid,step);
-  }
   int dfs(int x,int y,vector<vector<int>>& grid,int step){
     if(x<0||x>m-1||y<0||y>n-1||grid[x][y]==-1) return 0;
     if(grid[x][y]==2) return step==0?1:0;
-    int ans=0;
     grid[x][y]=-1;
+    int ans=0;
     ans+=dfs(x-1,y,grid,step-1);
     ans+=dfs(x+1,y,grid,step-1);
     ans+=dfs(x,y-1,grid,step-1);
