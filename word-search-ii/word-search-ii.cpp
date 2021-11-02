f21e713bb2cbab6a9a29b0c4e127920f5d3d5dda
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-02 12:47:39 -0700

    Time: 312 ms (62.66%), Space: 12.5 MB (14.35%) - LeetHub

--- word-search-ii/word-search-ii.cpp
+++ word-search-ii/word-search-ii.cpp
@@ -7,27 +7,28 @@ class Trie{
 class Solution {
   int m,n;
   int dir[5]={1,0,-1,0,1};
-public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-      Trie* root=new Trie();
-      for(string w:words){
-        Trie* node=root;
-        for(char c:w){
-          if(node->children[c-'a']==nullptr) node->children[c-'a']=new Trie();
-          node=node->children[c-'a'];
-        }
-        node->word=w;
+  public:
+  vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    m=board.size(),n=board[0].size();
+    Trie* root=new Trie();
+    for(string w:words){
+      Trie* node=root;
+      for(char c:w){
+        if(node->children[c-'a']==nullptr) node->children[c-'a'] =new Trie();
+        node=node->children[c-'a'];
       }
-      m=board.size(),n=board[0].size();
-      vector<string>ans;
-      for(int i=0;i<m;i++)
-        for(int j=0;j<n;j++){
-          dfs(i,j,board,root,ans);
-        }
-      return ans;
+      node->word=w;
     }
-  void dfs(int i,int j, vector<vector<char>>& board,Trie* node,vector<string>&ans){
-    if(i<0||i>m-1||j<0||j>n-1||board[i][j]=='#') return;
+    vector<string>ans;
+    for(int i=0;i<m;i++){
+      for(int j=0;j<n;j++){
+        dfs(i,j,root,board,ans);
+      }
+    }
+    return ans;
+  }
+  void dfs(int i,int j,Trie* node,vector<vector<char>>& board,vector<string>&ans){
+    if(i<0||j<0||i>m-1||j>n-1||board[i][j]=='#') return;
     char c=board[i][j];
     if(node->children[c-'a']==nullptr) return;
     node=node->children[c-'a'];
@@ -36,7 +37,7 @@ public:
     for(int k=0;k<4;k++){
       int r=i+dir[k];
       int c=j+dir[k+1];
-      dfs(r,c,board,node,ans);
+      dfs(r,c,node,board,ans);
     }
     board[i][j]=c;
   }
