837238ddb52dcd411b2c30848b19c743fd4a9a60
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-02 08:00:09 -0700

    Time: 4 ms (96.76%), Space: 9.6 MB (84.30%) - LeetHub

--- regions-cut-by-slashes/regions-cut-by-slashes.cpp
+++ regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -1,62 +1,44 @@
 class Solution {
-  int dirs[5]={1,0,-1,0,1};
-  int m;
+  vector<int>parent;
+  int n;
   public:
   int regionsBySlashes(vector<string>& grid) {
-    int n=grid.size();
-    vector<vector<int>>g(3*n,vector<int>(3*n,0));
-    for(int i=0;i<n;i++){
+    n=grid.size();
+    parent.resize((n+1)*(n+1));
+    for(int i=0;i<=n;i++)
+      for(int j=0;j<=n;j++){
+        int idx=i*(n+1)+j;
+        parent[idx]=idx;
+        if(i==0||i==n||j==0||j==n) parent[idx]=0;
+      }
+    int count=1;
+    for(int i=0;i<n;i++)
       for(int j=0;j<n;j++){
+        if(grid[i][j]==' ') continue;
+        int a,b;
         if(grid[i][j]=='/'){
-          g[3*i][3*j+2]=1;
-          g[3*i+1][3*j+1]=1;
-          g[3*i+2][3*j+0]=1;
+          a=node(i,j+1);
+          b=node(i+1,j);
+        }else{
+          a=node(i,j);
+          b=node(i+1,j+1);
         }
-        if(grid[i][j]=='\\'){
-          g[3*i][3*j]=1;
-          g[3*i+1][3*j+1]=1;
-          g[3*i+2][3*j+2]=1;
-        }
+        if(find(a)==find(b)) count++;
+        else unionNodes(a,b);
       }
-    }
-    m=3*n;
-    int ans=0;
-    for(int i=0;i<3*n;i++){
-      for(int j=0;j<3*n;j++){
-        if(g[i][j]==0) {
-          dfs(i,j,g);
-          //DFS(g,i,j);
-          ans++;
-        }
-      }
-    }
-    return ans;
+    return count;
   }
-  void DFS(vector<vector<int>>&Grid, int x, int y)
-    {
-        int M = Grid.size();
-        if (x<0||x>=M||y<0||y>=M) return;
-        if (Grid[x][y]!=0) return;
-        Grid[x][y]=2;
-        
-        vector<pair<int,int>>dir({{0,1},{0,-1},{1,0},{-1,0}});
-        for (int k=0; k<4; k++)
-        {
-            int i = x+dir[k].first;
-            int j = y+dir[k].second;
-            DFS(Grid,i,j);
-        }
-    }
-  void dfs(int i,int j,vector<vector<int>>&g){
-    if(i<0||i>m-1||j<0||j>m-1) return;
-    if(g[i][j]!=0) return;
-    g[i][j]=2;
-    for(int d=0;d<4;d++){
-      int r=i+dirs[d];
-      int c=j+dirs[d+1];
-      dfs(r,c,g);
-    }
+  int node(int i,int j){
+    return i*(n+1)+j;
+  }
+  int find(int x){
+    if(parent[x]!=x) parent[x]=find(parent[x]);
+    return parent[x];
+  }
+  void unionNodes(int a, int b){
+    a=parent[a];
+    b=parent[b];
+    if(a<b) parent[b]=a;
+    else    parent[a]=b;
   }
 };
-// //
-// /  
