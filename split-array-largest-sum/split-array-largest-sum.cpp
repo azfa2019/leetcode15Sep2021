d63bf9a03f5b67532088038a44086245c5ea498c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-05 23:18:29 -0700

    Time: 4 ms (61.25%), Space: 7.1 MB (73.16%) - LeetHub

--- split-array-largest-sum/split-array-largest-sum.cpp
+++ split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
+  int n;
 public:
     int splitArray(vector<int>& nums, int m) {
+      n=nums.size();
       long left=0,right=INT_MAX;
       while(left<right){
-        int mid=left+(right-left)/2;
+        long mid=left+(right-left)/2;
         if(check(nums,m,mid)) right=mid;
         else left=mid+1;
       }
       return left;
     }
-  bool check(vector<int>&nums,int m, long val){
+  bool check(vector<int>&nums,int m,long val){
     int count=0;
-    for(int i=0;i<nums.size();i++){
+    for(int i=0;i<n;i++){
       if(nums[i]>val) return false;
       int j=i;
-      long sum=0;
-      while(j<nums.size() && sum+(long)nums[j]<=val){
-        sum+=(long) nums[j];
+      int sum=0;
+      while(j<n && sum+nums[j]<=val){
+        sum+=nums[j];
         j++;
       }
       count++;
