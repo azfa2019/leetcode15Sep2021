13a4ff51a7ce36aec3efe643de730537043a6f06
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-06 08:28:49 -0700

    Time: 40 ms (79.97%), Space: 9.6 MB (79.76%) - LeetHub

--- cherry-pickup/cherry-pickup.cpp
+++ cherry-pickup/cherry-pickup.cpp
@@ -3,8 +3,6 @@ class Solution {
   int cherryPickup(vector<vector<int>>& grid) {
     int n=grid.size();
     int dp[n+1][n+1][n+1];
-    //dp[i][j][x][y]: max cherry when at position [i][j] & [x][y], consider going along two paths from top left to bot right
-    //dp is 1 based, so n+1. Note grid[i][j] becomes grid[i-1][j-1]
     for(int i=0;i<=n;i++)
       for(int j=0;j<=n;j++)
         for(int x=0;x<=n;x++)
@@ -14,20 +12,19 @@ class Solution {
         for(int x=1;x<=n;x++){
           int y=i+j-x;
           if(y<1||y>n) continue;
-          if(grid[i-1][j-1]==-1||grid[x-1][y-1]==-1) continue;
-          if(i==1&&j==1&&y==1) {
+          if(i==1&&j==1&&x==1) {
             dp[i][j][x]=grid[0][0];
             continue;
           }
+          if(grid[i-1][j-1]==-1||grid[x-1][y-1]==-1) continue;
+
           dp[i][j][x]=max(dp[i][j][x],dp[i-1][j][x]);
           dp[i][j][x]=max(dp[i][j][x],dp[i][j-1][x]);
           dp[i][j][x]=max(dp[i][j][x],dp[i-1][j][x-1]);
           dp[i][j][x]=max(dp[i][j][x],dp[i][j-1][x-1]);
-          if(i==x&&j==y) dp[i][j][x]+=grid[i-1][j-1];
+          if(i==x && j==y) dp[i][j][x]+=grid[i-1][j-1];
           else dp[i][j][x]+=grid[i-1][j-1]+grid[x-1][y-1];
         }
     return max(0,dp[n][n][n]);
-    }
+  }
 };
-//top left to bot right
-//dp[i][j]=max(dp[i-1][j], dp[i][j-1])+grid[i][j]
