1f857022cf7e73c9ea171b68263259dfcf468306
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-05 19:55:26 -0700

    Time: 4 ms (98.65%), Space: 10 MB (42.40%) - LeetHub

--- single-number-iii/single-number-iii.cpp
+++ single-number-iii/single-number-iii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-      unordered_set<int>set0;
+        int n1n2=0;
+      for(int num:nums) n1n2^=num;
+      int right1=1;
+      while((n1n2&right1)==0) right1=(right1<<1);
+      int n1=0,n2=0;
       for(int num:nums){
-        if(set0.find(num)!=set0.end()) set0.erase(num);
-        else set0.insert(num);
+        if((right1&num)==0) n1^=num;
+        else n2^=num;
       }
-      vector<int>ans;
-      for(int num:set0) ans.push_back(num);
-      return ans;
+      return {n1,n2};
     }
 };
