18bb31dcab661c68b9109991f33b4f033374e176
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-07 11:13:23 -0800

    Time: 16 ms (98.44%), Space: 13.8 MB (42.33%) - LeetHub

---
+++ number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,38 @@
+class UF{
+  public:
+  vector<int>parent;
+  vector<int>rank;
+  UF(int n){
+    parent=vector<int>(n,0);
+    rank=vector<int>(n,0);
+    for(int i=0;i<n;i++) parent[i]=i;
+  }
+  bool unionNodes(int n1,int n2){
+    int p1=findParent(n1);
+    int p2=findParent(n2);
+    if(p1==p2) return false;
+    if(rank[p1]<rank[p2]) parent[p1]=p2;
+    else if(rank[p1]>rank[p2]) parent[p2]=p1;
+    else parent[p1]=p2,rank[p2]++;
+    return true;
+  }
+  int findParent(int n){
+    if(n!=parent[n]) parent[n]=findParent(parent[n]);
+    return parent[n];
+  }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+      int n=isConnected.size();
+      UF uf0=UF(n);
+      for(int i=0;i<n;i++)
+        for(int j=i+1;j<n;j++){
+          if(isConnected[i][j]==1) uf0.unionNodes(i,j);
+        }
+      unordered_set<int>set0;
+      for(int i=0;i<n;i++) set0.insert(uf0.findParent(i));
+      return set0.size();
+        
+    }
+};
