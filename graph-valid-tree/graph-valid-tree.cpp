9f9734a1f029a63c58beaea544cda09006dac058
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-07 22:17:54 -0800

    Time: 24 ms (42.88%), Space: 15.2 MB (16.35%) - LeetHub

--- graph-valid-tree/graph-valid-tree.cpp
+++ graph-valid-tree/graph-valid-tree.cpp
@@ -1,25 +1,25 @@
 class Solution {
-public:
-    bool validTree(int n, vector<vector<int>>& edges) {
-      stack<int>stack0;
-      stack0.push(0);
-      unordered_map<int,vector<int>>map0;
-      for(auto e:edges){
-        map0[e[0]].push_back(e[1]);
-        map0[e[1]].push_back(e[0]);
-      }
-      unordered_map<int,int>parent;
-      parent[0]=-1;
-      while(!stack0.empty()){
-        int cur=stack0.top();
-        stack0.pop();
-        for(int nei:map0[cur]){
-          if(nei==parent[cur]) continue;
-          if(parent.find(nei)!=parent.end()) return false;
-          parent[nei]=cur;
-          stack0.push(nei);
-        }
-      }
-      return parent.size()==n;
+  public:
+  bool validTree(int n, vector<vector<int>>& edges) {
+    unordered_map<int,vector<int>>g;
+    for(auto e:edges){
+      g[e[0]].push_back(e[1]);
+      g[e[1]].push_back(e[0]);
     }
+    stack<int>stack0;
+    stack0.push(0);
+    unordered_map<int,int>parent;
+    parent[0]=-1;
+    while(!stack0.empty()){
+      int cur=stack0.top();
+      stack0.pop();
+      for(int nei:g[cur]){
+        if(parent[cur]==nei) continue;
+        if(parent.find(nei)!=parent.end()) return false;
+        parent[nei]=cur;
+        stack0.push(nei);
+      }
+    }
+    return parent.size()==n;
+  }
 };
