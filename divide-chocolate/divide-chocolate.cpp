731127359a5e2734b0bd4e24642c3874fe5a51cd
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-07 21:05:58 -0800

    Time: 24 ms (67.99%), Space: 15.2 MB (74.55%) - LeetHub

--- divide-chocolate/divide-chocolate.cpp
+++ divide-chocolate/divide-chocolate.cpp
@@ -2,25 +2,36 @@ class Solution {
   int n;
   public:
   int maximizeSweetness(vector<int>& sweetness, int k) {
-    int sum=0;
     n=sweetness.size();
+    int sum=0;
     for(int s:sweetness) sum+=s;
     int left=0,right=sum;
     while(left<right){
-      int mid=left+(right-left+1)/2;
-      if(check(sweetness,k,mid)) left=mid;
-      else right=mid-1;
+      int mid=left+(right-left)/2;
+      if(check(sweetness,k,mid)) right=mid;
+      else left=mid+1;
     }
     return left;
   }
   bool check(vector<int>& sweetness, int k,int val){
-    int count=0;
-    int sum=0;
+    int count=1,sum=0;
+    for(int num:sweetness){
+      sum+=num;
+      if(sum>val){
+        sum=0;
+        count++;
+        if(count>k+1) return false;
+      }
+    }
+    return true;
+  }
+  bool check1(vector<int>& sweetness, int k,int val){
+    int sum=0,count=0;
     for(int i=0;i<n;i++){
       sum+=sweetness[i];
       if(sum>=val){
-        sum=0;
         count++;
+        sum=0;
       }
     }
     return count>=k+1;
