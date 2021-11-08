42d882d70490d0990dd03335bc72d180e49e3b90
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-07 22:00:46 -0800

    Time: 80 ms (71.37%), Space: 21 MB (53.33%) - LeetHub

--- word-squares/word-squares.cpp
+++ word-squares/word-squares.cpp
@@ -1,19 +1,21 @@
 class Solution {
-  vector<vector<string>>ans;
-  unordered_map<string,vector<string>>map0;
   int n;
+  unordered_map<string,vector<string>>map0;
+  vector<vector<string>>ans;
 public:
     vector<vector<string>> wordSquares(vector<string>& words) {
+        
       this->n=words[0].size();
       for(string w:words){
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
           map0[w.substr(0,i)].push_back(w);
+        }
       }
       vector<string>cur;
       dfs(0,cur,words);
       return ans;
     }
-  void dfs(int row,vector<string>&cur,vector<string>&words){
+  void dfs(int row,vector<string>&cur,vector<string>& words){
     if(row==n) {
       ans.push_back(cur);
       return;
@@ -22,10 +24,13 @@ public:
     for(int i=0;i<row;i++){
       prefix.push_back(cur[i][row]);
     }
-    for(auto word:map0[prefix]){
-      cur.push_back(word);
+    for(string w:map0[prefix]){
+      cur.push_back(w);
       dfs(row+1,cur,words);
       cur.pop_back();
     }
   }
 };
+//b a l l
+//a r e a
+//l e x x
