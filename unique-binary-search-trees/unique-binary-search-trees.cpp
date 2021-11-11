5271d9daf8a0a0a1443148ac7168c4dbbbcc29a9
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-11 07:02:49 -0800

    Time: 0 ms (100.00%), Space: 6.2 MB (9.77%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,31 +1,15 @@
-class Solution1 {
-  public:
-  unordered_map<int,int>map0;
-  int numTrees(int n) {//71
-    if(n<=1) return 1;
-    if(map0.find(n)!=map0.end()) return map0[n];
-    int count=0;
-    for(int i=0;i<n;i++){
-      int left=numTrees(i-1);
-      int right=numTrees(n-i);
-      count+=left*right;
-    }
-    return map0[n]=count;
-  }
-};
 class Solution {
-    public:
-    unordered_map<int,int>m;
-    int numTrees(int n) {//00
-        //memoization + recursive
-        if(n<=1) return 1;
-        if(m.count(n)) return m[n];
-        int count=0;
-        for(int i=1;i<=n;i++){
-            int left=numTrees(i-1);
-            int right=numTrees(n-i);
-            count+=left*right;
-        }
-        return m[n]=count;
+  unordered_map<int,int>map0;
+public:
+    int numTrees(int n) {
+      if(n<=1) return 1;
+      if(map0.find(n)!=map0.end()) return map0[n];
+      int count=0;
+      for(int i=1;i<=n;i++){
+        int left=numTrees(i-1);
+        int right=numTrees(n-i);
+        count+=left*right;
+      }
+      return map0[n]=count;
     }
 };
