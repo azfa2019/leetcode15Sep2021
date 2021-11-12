5ec6455bc7dd74ee9bdb9eedf8b8b9cb8eba5c5e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-12 08:18:22 -0800

    Time: 40 ms (54.93%), Space: 16.3 MB (35.92%) - LeetHub

--- k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -2,24 +2,22 @@ class Solution {
   long M=1e9+7;
   public:
   int kInversePairs(int n, int k) {
-
     vector<vector<int>>dp(n+1,vector<int>(k+1,0));
     dp[0][0]=1;
     for(int i=1;i<=n;i++) dp[i][0]=1;
     for(int i=1;i<=n;i++){
       for(int j=1;j<=k;j++){
-        dp[i][j]=(dp[i-1][j]%M+dp[i][j-1]%M-(j-i>=0?dp[i-1][j-i]:0)%M+M)%M;
+        dp[i][j]= (dp[i][j-1]-(j-i>=0?dp[i-1][j-i]:0)+dp[i-1][j]+M)%M;
       }
     }
     return dp[n][k];
   }
 };
-
-//dp[i][j]: # of arrays from 1 to i, with j inverse pairs 
-// 1 2 3 4 .. i-1
-// i 1 2 3 4 .. i-1 dp[i-1][j-(i-1)]
-// 1 i 2 3 4 .. i-1 dp[i-1][j-(i-2)]
-// 1 2 3 4 .. i-1 i dp[i-1][j-(0))]
-// dp[i][j-1]=          dp[i-1][j-1]+dp[i-1][j-2]+... dp[i-1][j-(i-1)]+dp[i-1][j-i])]
-// dp[i][j]= dp[i-1][j]+dp[i-1][j-1] ... +           +dp[i-1][j-(i-1)]
-//dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-i];
+//dp[i][j]: # of ways for numbers 1 to i, with j inverse pairs
+// 1 2 3 4 ... i-1 
+// i 1 2 3 4 ... i-1 : dp[i-1][j-(i-1)]
+// 1 i 2 3 4 ... i-1 : dp[i-1][j-(i-2)]
+// 1 2 3 4 ... i-1 i : dp[i-1][j]
+//dp[i][j] = dp[i-1][j]+ dp[i-1][j-1] + dp[i-1][j-2] + ... + dp[i-1][j-(i-1)]
+//dp[i][j-1] =           dp[i-1][j-1]+ dp[i-1][j-2] + dp[i-1][j-3] + ...     + dp[i-1][j-i]
+//dp[i][j]= dp[i][j-1]-dp[i-1][j-i]+dp[i-1][j]
