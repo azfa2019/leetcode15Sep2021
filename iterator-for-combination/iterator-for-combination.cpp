10dd81a3f49ed66f9c334bd7bdfc992bf0bc347d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-13 18:28:58 -0800

    Time: 8 ms (95.09%), Space: 12 MB (94.20%) - LeetHub

--- iterator-for-combination/iterator-for-combination.cpp
+++ iterator-for-combination/iterator-for-combination.cpp
@@ -1,51 +1,51 @@
 class CombinationIterator {
   string cur,end;
-  string characters;
   int flag;
+  string characters;
   int combinationLength;
   public:
   CombinationIterator(string characters, int combinationLength) {
-    this->characters=characters;
-    this->combinationLength=combinationLength;
-    cur=characters.substr(0,combinationLength);
-    end=characters.substr(characters.size()-combinationLength);
+    this-> characters=characters;
+    this-> combinationLength=combinationLength;
+
+    this->cur=characters.substr(0,combinationLength);
+    this->end=characters.substr(characters.size()-combinationLength);
     this->flag=1;
   }
   string next() {
-    if(flag==1){
+    if(flag) {
       flag=0;
       return cur;
     }
+
     int i=cur.size()-1;
     while(i>=0 && cur[i]==characters[i+characters.size()-combinationLength]) i--;
     int j=0;
-    //while(j<characters.size() && characters[j]==cur[j]) j++;
-    while( characters[j]!=cur[i]) j++;
+    while(j<characters.size() && characters[j]!=cur[i]) j++;
     for(int k=i;k<cur.size();k++){
       cur[k]=characters[j+1+k-i];
     }
     return cur;
   }
+
   bool hasNext() {
     return cur!=end;
   }
 };
+
 /**
  * Your CombinationIterator object will be instantiated and called as such:
  * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
  * string param_1 = obj->next();
  * bool param_2 = obj->hasNext();
  */
-// 01234
-// acfhx
 //.  j
-// acf
-// ach
+// adfhx
+// 01234
 
-// acx
-//  i
-// afh
+// dfh
+// dfx
+//. i
 
-// afx
-// ahx
-// cfh
+// dhx
+// fhx
