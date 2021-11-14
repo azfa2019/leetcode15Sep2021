c88136367506d8663563f3b58826a555ef502bc1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-14 09:36:29 -0800

    Time: 32 ms (80.21%), Space: 29 MB (37.60%) - LeetHub

--- serialize-and-deserialize-bst/serialize-and-deserialize-bst.cpp
+++ serialize-and-deserialize-bst/serialize-and-deserialize-bst.cpp
@@ -8,60 +8,26 @@
  * };
  */
 class Codec {
-  public:
+public:
 
-  // Encodes a tree to a single string.
-  string serialize(TreeNode* root) {
-    if(root==nullptr) return "";
-    string str;
-    queue<TreeNode*>q;
-    q.push(root);
-    while(!q.empty()){
-      int count=q.size();
-      while(count--){
-        TreeNode* cur=q.front();
-        q.pop();
-        if(cur==nullptr) str+="#";
-        else {
-          str+=to_string(cur->val)+"!";
-          q.push(cur->left);
-          q.push(cur->right);
-        }
-      }
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+      if(root==nullptr) return "#";
+      return to_string(root->val)+" "+serialize(root->left)+" "+serialize(root->right);
     }
-    return str;
-  }
 
-  // Decodes your encoded data to tree.
-  TreeNode* deserialize(string data) {
-    if(data.empty()) return nullptr;
-    int i=0,j=0;
-    while(j<data.size() && data[++j]!='!');
-    TreeNode* root=new TreeNode(stoi(data.substr(i,j-i)));
-    queue<TreeNode*>q;
-    q.push(root);
-
-    while(i<data.size()){
-      TreeNode* node=q.front();
-      q.pop();
-      if(node==nullptr) continue;
-      i=++j;
-      if(i>=data.size()) break;
-      if(data[i]=='#') node->left=nullptr;
-      else{
-        while(j<data.size() && data[++j]!='!');
-        node->left=new TreeNode(stoi(data.substr(i,j-i)));
-      }
-      q.push(node->left);
-      i=++j;
-      if(i>=data.size()) break;
-      if(data[i]=='#') node->right=nullptr;
-      else{
-        while(j<data.size() && data[++j]!='!');
-        node->right=new TreeNode(stoi(data.substr(i,j-i)));
-      }
-      q.push(node->right);
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+      stringstream ss(data);
+      return buildTree(ss);
     }
+  TreeNode* buildTree(stringstream& ss){
+    string tmp;
+    ss>>tmp;
+    if(tmp=="#") return nullptr;
+    TreeNode* root=new TreeNode(stoi(tmp));
+    root->left=buildTree(ss);
+    root->right=buildTree(ss);
     return root;
   }
 };
