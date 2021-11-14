3619e4610f7f8b98c3317c036fd050294b04e873
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-14 09:00:32 -0800

    Time: 104 ms (27.78%), Space: 7.6 MB (40.00%) - LeetHub

--- valid-permutations-for-di-sequence/valid-permutations-for-di-sequence.cpp
+++ valid-permutations-for-di-sequence/valid-permutations-for-di-sequence.cpp
@@ -1,32 +1,30 @@
-typedef long long ll;
 class Solution {
-  ll dp[201][201];
-  ll M=1e9+7;
-  public:
-  int numPermsDISequence(string s) {
-    int n=s.size();
-    s="#"+s;
-    dp[0][0]=1;
-    for(int i=1;i<=n;i++){
-      for(int j=0;j<=i;j++){
-        if(s[i]=='I'){
-          for(int k=0;k<=j-1;k++){
-            dp[i][j]+=dp[i-1][k];
-            dp[i][j]%=M;
-          }
-        }else{
-          for(int k=j;k<=i-1;k++){
-            dp[i][j]+=dp[i-1][k];
-            dp[i][j]%=M;
+public:
+    int numPermsDISequence(string s) {
+      int n=s.size();
+      vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+      dp[0][0]=1;
+      int M=1e9+7;
+      for(int i=1;i<=n;i++){
+        for(int j=0;j<=i;j++){
+          if(s[i-1]=='D'){
+            for(int k=0;k<=j-1;k++){
+              dp[i][j]+=dp[i-1][k];
+              dp[i][j]%=M;
+            }
+          }else{
+            for(int k=j;k<=i-1;k++){
+              dp[i][j]+=dp[i-1][k];
+              dp[i][j]%=M;
+            }
           }
         }
       }
+      int ans=0;
+      for(int j=0;j<=n;j++){
+        ans+=dp[n][j];
+        ans%=M;
+      }
+      return ans;
     }
-    ll ans=0;
-    for(int i=0;i<=n;i++) {
-      ans+=dp[n][i];
-      ans%=M;
-    }
-    return ans;
-  }
 };
