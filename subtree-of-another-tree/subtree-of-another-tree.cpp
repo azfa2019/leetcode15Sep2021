4123ab27639dc38666f6f1a77ff671c6e0481d61
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-14 23:05:37 -0800

    Time: 24 ms (62.63%), Space: 28.9 MB (55.80%) - LeetHub

--- subtree-of-another-tree/subtree-of-another-tree.cpp
+++ subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -10,15 +10,13 @@
  * };
  */
 class Solution {
-  public:
-  bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-    if(root==nullptr||subRoot==nullptr) return root==subRoot;
-    if(helper(root,subRoot)) return true;
-    else return isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
-  }
-  bool helper(TreeNode* n1, TreeNode* n2){
-    if(n1==nullptr ||n2==nullptr) return n1==n2;
-    if(n1->val!=n2->val) return false;
-    else return helper(n1->left,n2->left)&&helper(n1->right,n2->right); 
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+      if(root==nullptr || subRoot==nullptr) return root==subRoot;
+      return sameTree(root,subRoot)||isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
+    }
+  bool sameTree(TreeNode* n1,TreeNode* n2){
+    if(n1==nullptr || n2==nullptr) return n1==n2;
+    return (n1->val==n2->val && sameTree(n1->left,n2->left) && sameTree(n1->right,n2->right));
   }
 };
