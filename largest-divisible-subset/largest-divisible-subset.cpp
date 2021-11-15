e7b4550a5c41cf2e2583f322f0e2bdbada6acfee
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-14 16:52:14 -0800

    Time: 36 ms (85.59%), Space: 8.8 MB (60.66%) - LeetHub

--- largest-divisible-subset/largest-divisible-subset.cpp
+++ largest-divisible-subset/largest-divisible-subset.cpp
@@ -6,11 +6,9 @@ class Solution {
     vector<int>dp(n,1);
     vector<int>prev(n,-1);
     dp[0]=1;
-    
-    vector<int>ret;
     for(int i=1;i<n;i++){
       for(int j=0;j<=i-1;j++){
-        if(nums[i]%nums[j]==0) {
+        if(nums[i]%nums[j]==0){
           if(dp[i]<dp[j]+1){
             dp[i]=dp[j]+1;
             prev[i]=j;
@@ -18,22 +16,18 @@ class Solution {
         }
       }
     }
-    int maxIdx=-1,maxLen=-1;
+    int len=-1,idx=-1;
     for(int i=0;i<n;i++){
-      if(dp[i]>maxLen){
-        maxLen=dp[i];
-        maxIdx=i;
+      if(len<dp[i]){
+        len=dp[i];
+        idx=i;
       }
     }
     vector<int>ans;
-    int pos=maxIdx;
-    while(pos!=-1){
-      ans.push_back(nums[pos]);
-      pos=prev[pos];
+    while(ans.size()!=len){
+      ans.push_back(nums[idx]);
+      idx=prev[idx];
     }
     return ans;
   }
 };
-//dp[i]
-//dp[i-1]
-//dp[i]=dp[j] +1 for j=0, 1 ..i-1
