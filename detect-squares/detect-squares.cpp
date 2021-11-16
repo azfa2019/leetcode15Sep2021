78e390e489ce25841f4b3f1d6a1995be16732d47
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-16 07:23:48 -0800

    Time: 344 ms (73.18%), Space: 143.8 MB (34.70%) - LeetHub

--- detect-squares/detect-squares.cpp
+++ detect-squares/detect-squares.cpp
@@ -3,8 +3,9 @@ class DetectSquares {
   public:
   DetectSquares() {
     for(int i=0;i<=1000;i++)
-      for(int j=0;j<=1000;j++)
-        counts[i][j]=0;
+    for(int j=0;j<=1000;j++)
+      counts[i][j]=0;
+
   }
 
   void add(vector<int> point) {
@@ -13,19 +14,20 @@ class DetectSquares {
   }
 
   int count(vector<int> point) {
-    int x=point[0],y=point[1];
+    int u=point[0],v=point[1];
     int ans=0;
-    for(int u=0;u<=1000;u++){
-      int d=abs(x-u);
-      if(d==0) continue;
-      int v=y+d;
-      if(v>=0&& v<=1000)
-        ans+=counts[u][v]*counts[u][y]*counts[x][v];
-      v=y-d;
-      if(v>=0&& v<=1000)
-        ans+=counts[u][v]*counts[u][y]*counts[x][v];
-    }
+      for(int x=0;x<=1000;x++){
+        int d=abs(x-u);
+        if(d==0) continue;
+        int y=v+d;
+        if(y<=1000 && y>=0)
+          ans+=counts[u][y]*counts[x][y]*counts[x][v];
+        y=v-d;
+        if(y<=1000 && y>=0)
+          ans+=counts[u][y]*counts[x][y]*counts[x][v];
+      }
     return ans;
+
   }
 };
 
