413fc548af3a56b44b8c5ac8f7aaba44f3b73799
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-16 23:13:24 -0800

    Time: 60 ms (90.58%), Space: 20 MB (66.27%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -5,24 +5,25 @@ class Solution {
   vector<vector<int>> threeSum(vector<int>& nums) {
     int n=nums.size();
     sort(nums.begin(),nums.end());
-    vector<vector<int>> ans;
+    vector<vector<int>>ans;
     for(int i=0;i<n-2;i++){
       if(i>0 && nums[i]==nums[i-1]) continue;
-      int left=i+1,right=n-1;
-      int target=0-nums[i];
-      while(left<right){
-        if(nums[left]+nums[right]<target) left++;
-        else if(nums[left]+nums[right]>target) right--;
-        else {
-          ans.push_back({nums[i],nums[left],nums[right]});
-          left++;
-          right--;
-          while(left<right && nums[left]==nums[left-1]) left++;
-          while(left<right && nums[right]==nums[right+1]) right--;
-        }
-      }
+      findpair(nums,i,ans);
     }
     return ans;
+  }
+  void findpair(vector<int>&nums,int i,vector<vector<int>>&ans){
+    int left=i+1,right=nums.size()-1;
+    while(left<right){
+      int sum=nums[left]+nums[right]+nums[i];
+      if(sum==0){
+        ans.push_back({nums[i],nums[left],nums[right]});
+        left++,right--;
+        while(left<right && nums[left]==nums[left-1]) left++;
+        while(left<right && nums[right]==nums[right+1]) right--;
+      }else if(sum<0) left++;
+      else right--;
+    }
   }
 };
 //class Solution {
