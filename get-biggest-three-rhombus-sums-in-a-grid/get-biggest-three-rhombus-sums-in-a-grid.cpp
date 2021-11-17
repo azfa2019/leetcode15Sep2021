ac5d2a846ecee45a2d52ef8a0de9f4c86357b525
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-17 10:35:31 -0800

    Time: 288 ms (31.74%), Space: 57 MB (52.69%) - LeetHub

--- get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,46 +1,57 @@
 class Solution {
-  public:
-  vector<int> getBiggestThree(vector<vector<int>>& grid) {
-    int m=grid.size(),n=grid[0].size();
-    unordered_set<int>set0;
-    for(int i=0;i<m;i++){
-      for(int j=0;j<n;j++){
-        set0.insert(grid[i][j]);
-        int r=min(i,j);
-        r=min(m-1-i,r);
-        r=min(n-1-j,r);
-        for(int k=1;k<=r;k++){
-          int sum=0;
-          int i0=i-k;
-          int j0=j;
-          for(int s=1;s<=k;s++){
-            i0++;
-            j0--;
-            sum+=grid[i0][j0];
+  int presum1[100][100]; // "\"
+  int presum2[100][100]; // "/"
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+      int m=grid.size(),n=grid[0].size();
+      for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+          presum1[i][j]=((i-1>=0 && j-1>=0)?presum1[i-1][j-1]:0)+grid[i][j];
+        
+      for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+          presum2[i][j]=((i-1>=0 && j+1<n)?presum2[i-1][j+1]:0)+grid[i][j];
+      
+      unordered_set<int>set0;
+      for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+          set0.insert(grid[i][j]);
+          int rmax=min(i,j);
+          rmax=min(rmax,m-1-i);
+          rmax=min(rmax,n-1-j);
+          for(int r=1;r<=rmax;r++){
+            int sum=0;
+            int i0,j0,i1,j1;
+            i0=i-r;
+            j0=j;
+            i1=i;
+            j1=j+r;
+            sum+=presum1[i1][j1]-((i0-1>=0&&j0-1>=0)?presum1[i0-1][j0-1]:0);
+            
+            i0=i;
+            j0=j-r;
+            i1=i+r;
+            j1=j;
+            sum+=presum1[i1][j1]-((i0-1>=0&&j0-1>=0)?presum1[i0-1][j0-1]:0);
+            
+            i0=i-r;
+            j0=j;
+            i1=i;
+            j1=j-r;
+            sum+=presum2[i1-1][j1+1]-presum2[i0][j0];
+            
+            i0=i;
+            j0=j+r;
+            i1=i+r;
+            j1=j;
+            sum+=presum2[i1-1][j1+1]-presum2[i0][j0];
+            set0.insert(sum);
           }
-          for(int s=1;s<=k;s++){
-            i0++;
-            j0++;
-            sum+=grid[i0][j0];
-          }
-          for(int s=1;s<=k;s++){
-            i0--;
-            j0++;
-            sum+=grid[i0][j0];
-          }
-          for(int s=1;s<=k;s++){
-            i0--;
-            j0--;
-            sum+=grid[i0][j0];
-          }
-          set0.insert(sum);
         }
       }
+      vector<int>ans(set0.begin(),set0.end());
+      sort(ans.rbegin(),ans.rend());
+      while(ans.size()>3) ans.pop_back();
+      return ans;
     }
-    vector<int>ans(set0.begin(),set0.end());
-    sort(ans.rbegin(),ans.rend());
-    while(ans.size()>3) ans.pop_back();
-    return ans;
-  }
 };
-//2500* 50
