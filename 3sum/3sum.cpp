b42c15443f38df40adbd92ae5750c398cdb082d4
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-16 23:34:26 -0800

    Time: 68 ms (80.51%), Space: 20.1 MB (66.27%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,24 +1,23 @@
-//deal with duplicates: i, left and right
-// if() continue or while() left++
 class Solution {
   public:
   vector<vector<int>> threeSum(vector<int>& nums) {
     int n=nums.size();
-    sort(nums.begin(),nums.end());
     vector<vector<int>>ans;
+    sort(nums.begin(),nums.end());
     for(int i=0;i<n-2;i++){
-      if(i>0 && nums[i]==nums[i-1]) continue;
+      while(i>0 && i<n && nums[i]==nums[i-1]) i++;
       findpair(nums,i,ans);
     }
     return ans;
   }
-  void findpair(vector<int>&nums,int i,vector<vector<int>>&ans){
+  void findpair(vector<int>& nums,int i,vector<vector<int>>&ans){
     int left=i+1,right=nums.size()-1;
     while(left<right){
       int sum=nums[left]+nums[right]+nums[i];
       if(sum==0){
         ans.push_back({nums[i],nums[left],nums[right]});
-        left++,right--;
+        left++;
+        right--;
         while(left<right && nums[left]==nums[left-1]) left++;
         while(left<right && nums[right]==nums[right+1]) right--;
       }else if(sum<0) left++;
@@ -26,59 +25,3 @@ class Solution {
     }
   }
 };
-//class Solution {
-//    public:
-//    vector<vector<int>> threeSum(vector<int>& nums) {//140
-//        int n=nums.size();
-//        if(n<3) return {};
-//        sort(nums.begin(),nums.end());
-//        vector<vector<int>> ans;
-//        for(int i=0;i<n-2;i++){
-//            if(i>0 && nums[i]==nums[i-1]) continue;
-//            findPair(nums,i,ans);
-//        }
-//        return ans;
-//    }
-//    void findPair(vector<int>&nums,int first,vector<vector<int>>&ans){
-//        int left=first+1,right=nums.size()-1;
-//        while(left<right){
-//            int sum=nums[first]+nums[left]+nums[right];
-//            if(sum==0) {
-//                ans.push_back({nums[first],nums[left],nums[right]});
-//                left++;
-//                right--;
-//                while(left<right && nums[left]==nums[left-1]) left++;
-//                while(left<right && nums[right]==nums[right+1]) right--;
-//            } else if(sum<0) left++;
-//            else right--;
-//        }
-//    }
-//};
-
-//deal with duplicates: i, left and right
-// if() continue or while() left++
-//class Solution {
-//  public:
-//  vector<vector<int>> threeSum(vector<int>& nums) {//140
-//    int n=nums.size();
-//    vector<vector<int>>res;
-//    sort(nums.begin(),nums.end());
-//    for(int i=0;i<n-2;i++){
-//      while(i>0 && i<n && nums[i]==nums[i-1]) i++;
-//      int left=i+1,right=n-1;
-//      while(left<right){
-//        int sum=nums[i]+nums[left]+nums[right];
-//        if(sum==0) {
-//          res.push_back({nums[i],nums[left],nums[right]});
-//          left++;
-//          right--;
-//          while(left<right && nums[left]==nums[left-1]) left++;
-//          while(left<right && nums[right]==nums[right+1]) right--;
-//        } else if(sum<0) left++;
-//        else if(sum>0) right--;
-//      }
-//    }
-//    return res;
-//  }
-//};
-//
