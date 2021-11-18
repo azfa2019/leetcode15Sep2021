85d894b9302e629a0c42b55a6941201493703c70
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-18 07:26:18 -0800

    Time: 836 ms (41.34%), Space: 301.2 MB (39.11%) - LeetHub

---
+++ minimum-absolute-difference-queries/minimum-absolute-difference-queries.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> minDifference(vector<int>& nums, vector<vector<int>>& queries) {
+     int n=nums.size();
+      vector<vector<int>>presum(101, vector<int>(n,0));
+      for(int k=1;k<=100;k++){
+        for(int i=0;i<n;i++){
+          presum[k][i]=(i==0?0:presum[k][i-1])+(nums[i]==k);
+        }
+      }
+      vector<int>ans;
+      for(auto q:queries){
+        int left=q[0],right=q[1];
+        vector<int>arr;
+        for(int k=1;k<=100;k++){
+          int count=presum[k][right]-(left-1>=0?presum[k][left-1]:0);
+          if(count>0){
+            arr.push_back(k);
+          }
+          if(arr.size()>=2 && arr.back()-arr[arr.size()-2]==1) break;
+        }
+        if(arr.size()<=1) ans.push_back(-1);
+        else{
+          int gap=INT_MAX/2;
+          for(int i=0;i<arr.size()-1;i++){
+            gap=min(gap,abs(arr[i]-arr[i+1]));
+          }
+          ans.push_back(gap);
+        }
+      }
+      return ans;
+    }
+};
+// sort, (nlogn, n)*2*10^4
