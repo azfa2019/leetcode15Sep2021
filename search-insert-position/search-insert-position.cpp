f08c427a1edd46254cee61fe9df4dd1d87c56f30
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-18 16:05:28 -0800

    Time: 4 ms (80.44%), Space: 9.7 MB (70.18%) - LeetHub

---
+++ search-insert-position/search-insert-position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int n=nums.size();
+      if(target>nums[n-1]) return n;
+      if(target<nums[0]) return 0;
+      int left=0,right=n-1;
+      while(left<right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]>=target) right=mid;
+        else left=mid+1;
+      }
+      return left;
+    }
+};
+
+// 1 2 4 7, mid < target
+// 3
+// insert at 2, mid+1
+
+//1 3 5 6, target=2, mid>target
+//insert at 1, mid
+
+//1 3 5 6, target=3, mid=target
+//insert at 1, mid
