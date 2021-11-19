9a013d2b7637071b7d82ea60e250528449b09c2b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-19 11:19:59 -0800

    Time: 52 ms (90.19%), Space: 48.2 MB (78.53%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -1,15 +1,13 @@
 class Solution {
   public:
   bool canJump(vector<int>& nums) {
-    int n=nums.size();
-    if(n==1) return true;
     int maxReach=nums[0];
-    for(int i=1;i<n;i++){
-      if(i>maxReach) return false;
-      maxReach=max(maxReach,i+nums[i]);
-      if(i+nums[i]>=n-1) return true;
-      //2 1 0 3 1
+    for(int i=0;i<nums.size();i++){
+      if(maxReach<i) return false;
+      int curReach=nums[i]+i;
+      maxReach=max(maxReach,curReach);
     }
-    return false;
+    return true;
   }
 };
+// 3 3 1 0 4
