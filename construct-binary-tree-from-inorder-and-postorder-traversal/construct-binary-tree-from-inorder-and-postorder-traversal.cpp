a504962fd483c9a7b65c190b8a499cb02e930d25
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-20 19:06:33 -0800

    Time: 20 ms (61.58%), Space: 26.5 MB (38.15%) - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -10,21 +10,21 @@
  * };
  */
 class Solution {
-  unordered_map<int,int>inPos;
   vector<int> postorder;
+  unordered_map<int,int>map0;
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-      for(int i=0;i<inorder.size();i++) inPos[inorder[i]]=i;
       this->postorder=postorder;
+      for(int i=0;i<inorder.size();i++) map0[inorder[i]]=i;
       return helper(0,inorder.size()-1,0,postorder.size()-1);
     }
   TreeNode* helper(int inLeft,int inRight,int postLeft,int postRight){
     if(inLeft>inRight || postLeft>postRight) return nullptr;
     int rootVal=postorder[postRight];
-    int rootPos=inPos[rootVal];
-    TreeNode* node=new TreeNode(rootVal);
-    node->left=helper(inLeft,rootPos-1,postLeft,postLeft+rootPos-inLeft-1);
-    node->right=helper(rootPos+1,inRight,postLeft+rootPos-inLeft,postRight-1);
-    return node;
+    int rootPos=map0[rootVal];
+    TreeNode* root=new TreeNode(rootVal);
+    root->left=helper(inLeft,rootPos-1,postLeft,postLeft+rootPos-1-inLeft);
+    root->right=helper(rootPos+1,inRight,postLeft+rootPos-inLeft,postRight-1);
+    return root;
   }
 };
