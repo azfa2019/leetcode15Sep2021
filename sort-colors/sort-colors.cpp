0a1f2ec84b91ebfbf9e2201080a084f039bfc3df
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-22 12:11:08 -0800

    Time: 5 ms (44.76%), Space: 8.3 MB (66.91%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,21 +1,31 @@
 class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-      // 2 0 2 1 1 0
-      // 0 0 2 1 1 2
-      //left: 0 pointer
-      //i: 1 pointer
-      //right: 2 pointer
-      int i=0,left=0,right=nums.size()-1;
-      while(i<=right){
-        if(nums[i]==0) swap(left,i,nums),left++,i++;
-        else if(nums[i]==1) i++;
-        else swap(i,right,nums),right--;
+  public:
+  void sortColors(vector<int>& nums) {
+    // 2 0 2 1 1 0
+    // 0 0 2 1 1 2
+    //left: 0 pointer
+    //i: 1 pointer
+    //right: 2 pointer
+
+    int n=nums.size();
+    int left=0,right=n-1,i=0;
+    while(i<=right){
+      if(nums[i]==1) i++;
+      else if(nums[i]==0) {
+        swap(nums[i],nums[left]);
+        i++,left++;
+      }
+      else {
+        swap(nums[i],nums[right]);
+        right--;
       }
     }
-  void swap(int i,int j,vector<int>&nums){
-    int tmp=nums[i];
-    nums[i]=nums[j];
-    nums[j]=tmp;
   }
 };
+// 2 0 2 1 1 0
+//     i
+// 0 0 2 1 1 2
+//.    l   r
+//     i
+// 0 0 1 1 2 2
+//.    l r
