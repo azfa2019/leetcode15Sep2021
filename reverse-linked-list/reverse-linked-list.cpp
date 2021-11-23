bd6826941155ba7368cab8a8783b9ee1ad68d1e2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-23 07:18:22 -0800

    Time: 7 ms (65.78%), Space: 8.3 MB (42.26%) - LeetHub

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -9,23 +9,20 @@
  * };
  */
 class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-      if(head==nullptr || head->next==nullptr) return head;
-      ListNode* pre=nullptr;
-      //pre->next=head;
-      while(head!=nullptr){
-        ListNode* tmp=head->next;
-        head->next=pre;
-        pre=head;
-        head=tmp;
-      }
-      return pre;
+  public:
+  ListNode* reverseList(ListNode* head) {
+    if(head==nullptr || head->next==nullptr) return head;
+    ListNode* pre=nullptr;
+    while(head!=nullptr){
+      ListNode* tmp=head->next;
+      head->next=pre;
+      pre=head;
+      head=tmp;
     }
+    return pre;
+  }
 };
-// pre (null) <-head  2(tmp) ->3 ->4
-// (null) <-head(pre)  2(head) ->3 
-// (null) <-head(pre)  2(head) ->3 ->null
-// (null) <-head<-  2(pre) ->3(head) ->null
-// (null) <-head<-  2 <-3(pre) null(head)
-//
+
+//n<1<2<3<4<5 
+//        p h t
+//          p ht
