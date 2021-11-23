75bfba9e668c77bd0351b6bcc4f75dd02bd89266
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-22 22:48:28 -0800

    Time: 35 ms (14.37%), Space: 13.3 MB (42.67%) - LeetHub

--- word-break/word-break.cpp
+++ word-break/word-break.cpp
@@ -3,16 +3,12 @@ class Solution {
   bool wordBreak(string s, vector<string>& wordDict) {
     int n=s.size();
     s="#"+s;
-    unordered_set<string>set(wordDict.begin(),wordDict.end());
     vector<bool>dp(n+1,false);
-    //dp[i]: ending with s[i]
     dp[0]=true;
+    unordered_set<string> set(wordDict.begin(),wordDict.end());
     for(int i=1;i<=n;i++){
       for(int j=1;j<=i;j++){
-        if(dp[j-1]==true && set.find(s.substr(j,i-j+1))!=set.end()) {
-          dp[i]=true;
-          break;
-        }
+        if(dp[j-1]==true && set.find(s.substr(j,i-j+1))!=set.end()) dp[i]=true;
       }
     }
     return dp[n];
