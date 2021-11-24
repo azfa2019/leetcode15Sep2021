03ad92ae55963f82740bc14b6aed014ea2002b19
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-23 22:24:33 -0800

    Time: 0 ms (100.00%), Space: 5.8 MB (79.44%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
       int ans=0;
-      while(n!=0){
-        n=(n&(n-1));
-        ans++;
+      for(int i=0;i<32;i++){
+        if(((n>>i)&1)==1) ans++;
       }
       return ans;
     }
