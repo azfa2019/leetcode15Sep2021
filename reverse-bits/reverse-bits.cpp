40d27e6c9c6d15a5910968278b0f93c56ebdb62c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-23 20:01:01 -0800

    Time: 3 ms (54.22%), Space: 5.9 MB (50.43%) - LeetHub

--- reverse-bits/reverse-bits.cpp
+++ reverse-bits/reverse-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
-public:
-    uint32_t reverseBits(uint32_t n) {
-      uint32_t ans=0;
-      //ans=ans|(1<<(31-0));
-      //cout<<ans<<endl;
-      //for(int i=0;i<32;i++)
-      //if(n&(1<<i)!=0) cout<<"hi"<<endl;
-      for(int i=0;i<32;i++){
-        //cout<<"i="<<i<<endl;
-        //cout<<(n&(1<<i)!=0)<<n<<endl;
-        if((n&(1<<i))!=0) ans=ans|(1<<(31-i));
-        //cout<<"ans="<<ans<<endl;
-        //else ans|=(0<<(31-i));
-      }
-      return ans;
+  public:
+  uint32_t reverseBits(uint32_t n) {
+    uint32_t ans=0;
+    for(int i=0;i<32;i++){
+      if(((n>>i) & 1)==1) ans|= (1<<(31-i));
     }
+    return ans;
+  }
 };
