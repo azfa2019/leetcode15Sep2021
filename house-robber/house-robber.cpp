87d92d6fcb0bdca7e1ecf95d01c8390083295548
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-23 23:05:59 -0800

    Time: 0 ms (100.00%), Space: 7.9 MB (21.79%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
       int n=nums.size();
-      vector<int>dp(n+1,INT_MIN);
-      //dp[i]: max money ending with nums[i]
       nums.insert(nums.begin(),0);
-      dp[0]=0;
+      int ans=0;
+      vector<int>dp(n+1,0);
       dp[1]=nums[1];
-      for(int i=2;i<=n;i++)
-        dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+      for(int i=2;i<=n;i++){
+        dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
+        ans=max(ans,dp[i]);
+      }
       return dp[n];
     }
 };
