e2e1678be780861d510f8078e83955d954d7c825
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-23 23:44:19 -0800

    Time: 46 ms (27.91%), Space: 12.2 MB (83.09%) - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,27 +1,30 @@
 class Solution {
-  public:
   int m,n;
+  //vector<vector<char>> grid;
   int dir[5]={1,0,-1,0,1};
-  int numIslands(vector<vector<char>>& grid) {
-    m=grid.size(),n=grid[0].size();
-    int count=0;
-    for(int r=0;r<m;r++)
-      for(int c=0;c<n;c++){
-        if(grid[r][c]=='1') {
-          dfs(r,c,grid);
-          count++;
+public:
+    int numIslands(vector<vector<char>>& grid) {
+      this->m=grid.size();
+      this->n=grid[0].size();
+      //this->grid=grid;
+      int count=0;
+      for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+          if(grid[i][j]=='1') {
+            dfs(i,j,grid);
+            count++;
+          }
         }
       }
-    return count;
-
-  }
-  void dfs(int row,int col,vector<vector<char>>& grid){
-    if(row<0||row>m-1||col<0||col>n-1||grid[row][col]=='0') return;
-    grid[row][col]='0';
-    for(int i=0;i<4;i++){
-      int rn=row+dir[i];
-      int cn=col+dir[i+1];
-      dfs(rn,cn,grid);
+      return count;
+    }
+  void dfs(int i,int j,vector<vector<char>>& grid){
+    if(i<0||i>m-1||j<0||j>n-1||grid[i][j]!='1') return;
+    grid[i][j]='0';
+    for(int d=0;d<4;d++){
+      int r=i+dir[d];
+      int c=j+dir[d+1];
+      dfs(r,c,grid);
     }
   }
 };
