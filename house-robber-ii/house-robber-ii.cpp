f4c9ca4c6255466322f5aa14e099d3944422d823
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-25 14:16:31 -0800

    Time: 3 ms (47.08%), Space: 7.9 MB (56.96%) - LeetHub

--- house-robber-ii/house-robber-ii.cpp
+++ house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
-public:
-    int rob(vector<int>& nums) {
-      //dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
-      int n=nums.size();
-      if(n==1) return nums[0];
-      vector<int>dp(n,0);
-      dp[0]=nums[0];
-      dp[1]=max(nums[0],nums[1]);
-      if(n==2) return dp[1];
-      for(int i=2;i<=n-2;i++){
-        dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
-      }
-        
-      vector<int>dpp(n,0);
-      dpp[1]=nums[1];
-      dpp[2]=max(nums[1],nums[2]);
-      for(int i=3;i<=n-1;i++){
-        dpp[i]=max(dpp[i-2]+nums[i],dpp[i-1]);
-      }
-      return max(dp[n-2],dpp[n-1]);
+  public:
+  int rob(vector<int>& nums) {
+    int n=nums.size();
+    if(n==1) return nums[0];
+    nums.insert(nums.begin(),0);
+    vector<int>dp(n+1,0);
+    dp[1]=nums[1];
+    for(int i=2;i<=n-1;i++){
+      dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
     }
+    vector<int>dp2(n+1,0);
+    for(int i=2;i<=n;i++){
+      dp2[i]=max(dp2[i-1],dp2[i-2]+nums[i]);
+    }
+    return max(dp[n-1],dp2[n]);
+  }
 };
