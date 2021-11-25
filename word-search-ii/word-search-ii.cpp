afa961c4fb9a5a6937c1e3ae5802ac3c57390452
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-25 13:02:29 -0800

    Time: 570 ms (41.70%), Space: 12.5 MB (22.97%) - LeetHub

--- word-search-ii/word-search-ii.cpp
+++ word-search-ii/word-search-ii.cpp
@@ -1,25 +1,25 @@
-class Trie{
-  public:
-  Trie* children[26]={};
-  string word="";
-  Trie(){}
-};
 class Solution {
+  class Trie{
+    public:
+    Trie* children[26]={};
+    string word="";
+    Trie(){};
+  };
   int m,n;
   int dir[5]={1,0,-1,0,1};
   public:
   vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-    m=board.size(),n=board[0].size();
     Trie* root=new Trie();
     for(string w:words){
       Trie* node=root;
       for(char c:w){
-        if(node->children[c-'a']==nullptr) node->children[c-'a'] =new Trie();
+        if(node->children[c-'a']==nullptr) node->children[c-'a']=new Trie();
         node=node->children[c-'a'];
       }
       node->word=w;
     }
     vector<string>ans;
+    this->m=board.size(),n=board[0].size();
     for(int i=0;i<m;i++){
       for(int j=0;j<n;j++){
         dfs(i,j,root,board,ans);
@@ -27,17 +27,17 @@ class Solution {
     }
     return ans;
   }
-  void dfs(int i,int j,Trie* node,vector<vector<char>>& board,vector<string>&ans){
-    if(i<0||j<0||i>m-1||j>n-1||board[i][j]=='#') return;
+  void dfs(int i, int j, Trie* root,vector<vector<char>>& board,vector<string>&ans){
+    if(i<0||i>m-1||j<0||j>n-1||board[i][j]=='#') return;
     char c=board[i][j];
-    if(node->children[c-'a']==nullptr) return;
-    node=node->children[c-'a'];
-    if(node->word!="") ans.push_back(node->word),node->word="";
+    if(root->children[c-'a']==nullptr) return;
+    root=root->children[c-'a'];
+    if(root->word!="") ans.push_back(root->word),root->word="";
     board[i][j]='#';
-    for(int k=0;k<4;k++){
-      int r=i+dir[k];
-      int c=j+dir[k+1];
-      dfs(r,c,node,board,ans);
+    for(int d=0;d<4;d++){
+      int r=i+dir[d];
+      int c=j+dir[d+1];
+      dfs(r,c,root,board,ans);
     }
     board[i][j]=c;
   }
