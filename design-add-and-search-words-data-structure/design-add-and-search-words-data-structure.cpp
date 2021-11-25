349218b226b4a934402b057160db3ee6c488ddd1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-25 12:18:12 -0800

    Time: 91 ms (59.57%), Space: 43.1 MB (78.51%) - LeetHub

---
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,45 @@
+class WordDictionary {
+  class Trie{
+    public:
+    Trie* children[26]={};
+    bool isword=false;
+    Trie(){};
+  };
+  Trie* root;
+public:
+    WordDictionary() {
+      root=new Trie();
+    }
+    void addWord(string word) {
+      Trie* node=root;
+      for(char c:word){
+        if(node->children[c-'a']==nullptr) node->children[c-'a']=new Trie();
+        node=node->children[c-'a'];
+      }
+      node->isword=true;
+    }
+    bool search(string word) {
+      Trie* node=root;
+      return find(node,0,word);
+    }
+  bool find(Trie* node,int idx,string& word){
+    if(node==nullptr) return false;
+    if(idx==word.size()) return (node->isword)==true;
+    if(word[idx]=='.'){
+      for(auto next:node->children){
+        if(find(next,idx+1,word)==true) return true;
+      }
+    }else{
+      auto next=node->children[word[idx]-'a'];
+      return find(next,idx+1,word);
+    }
+    return false;
+  }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
