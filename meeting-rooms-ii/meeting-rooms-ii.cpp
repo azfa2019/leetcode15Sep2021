8a37fdcfca9e12a5617f40d7ee97498bc7761b16
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-26 14:10:07 -0800

    Time: 24 ms (37.25%), Space: 12.6 MB (38.08%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.cpp
+++ meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,21 +1,27 @@
 
 class Solution {
-  struct compare{
-    bool operator()(vector<int>&a,vector<int>&b){
-      return a[1]>b[1];
-    }
-  };
   public:
   int minMeetingRooms(vector<vector<int>>& intervals) {
-    sort(intervals.begin(),intervals.end());
-    priority_queue<vector<int>,vector<vector<int>>,compare>pq;
+    int n=intervals.size();
+    vector<int>start,end;
     for(auto e:intervals){
-      if(pq.empty() || e[0]<pq.top()[1]) pq.push(e);
-      else {
-        pq.pop();
-        pq.push(e);
+      start.push_back(e[0]);
+      end.push_back(e[1]);
+    }
+    sort(start.begin(),start.end());
+    sort(end.begin(),end.end());
+    int i=0,j=0,ans=0;
+    int count=0;
+    while(i<n && j<n){
+      if(start[i]<end[j]) {
+        count++;
+        ans=max(ans,count);
+        i++;
+      }else{
+        count--;
+        j++;
       }
     }
-    return pq.size();
+    return ans;
   }
 };
