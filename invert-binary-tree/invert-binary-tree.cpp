384549c6b0dad07e88f59a045f45f5c45776f506
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-25 16:30:16 -0800

    Time: 0 ms (100.00%), Space: 9.5 MB (99.99%) - LeetHub

--- invert-binary-tree/invert-binary-tree.cpp
+++ invert-binary-tree/invert-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-      if(root==nullptr) return root;
+      if(root==nullptr) return nullptr;
       TreeNode* left=invertTree(root->left);
       TreeNode* right=invertTree(root->right);
       root->left=right;
