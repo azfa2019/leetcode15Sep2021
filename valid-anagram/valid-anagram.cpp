11fa83b40c5a05a5965dd30f38f66dcb3466f3c6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-26 11:28:08 -0800

    Time: 4 ms (97.59%), Space: 7.3 MB (44.27%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,12 +1,14 @@
 class Solution {
   public:
   bool isAnagram(string s, string t) {
-    int m=s.size(),n=t.size();
-    if(m!=n) return false;
     vector<int>count(26,0);
-    for(char c:s) count[c-'a']++;
-    for(char c:t) count[c-'a']--;
-    for(int i:count) if(i!=0) return false;
+    for(char c:s){
+      count[c-'a']++;
+    }
+    for(char c:t){
+      count[c-'a']--;
+    }
+    for(auto e:count) if(e!=0) return false;
     return true;
   }
 };
