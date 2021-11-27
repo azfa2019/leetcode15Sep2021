c8cfea2c0692b655852de74eca6cbcd9c10048a6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-11-26 16:04:30 -0800

    Time: 16 ms (96.90%), Space: 24 MB (55.79%) - LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    int n=nums.size();
+    vector<int>ans(n);
+    int tmp=1;
+    ans[0]=1;
+    for(int i=1;i<n;i++){
+      tmp*=nums[i-1];
+      ans[i]=tmp;
+    }
+    tmp=1;
+    for(int i=n-2;i>=0;i--){
+      tmp*=nums[i+1];
+      ans[i]*=tmp;
+    }
+    return ans;
+  }
+};
+// 1 2   3 4
+//   i   i tmp
+// 1 2    3 4
+//.  i-1  i
+//.  b
+//ans[1]=1*1=1
+//ans[2]=1*2=2
+//ans[3]=nums[2]*ans[2]=3*2=6
