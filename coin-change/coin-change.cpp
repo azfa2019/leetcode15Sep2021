211ff42fcf44cdfb8c8bfa2e26dba24db6ab25d7
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-01 12:05:43 -0800

    Time: 84 ms (56.49%), Space: 14.5 MB (27.39%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -4,10 +4,12 @@ class Solution {
     int n=coins.size();
     vector<int>dp(amount+1,INT_MAX/2);
     dp[0]=0;
-    for(int i=0;i<=amount;i++)
+    for(int i=0;i<=amount;i++){
       for(int j=0;j<n;j++){
-        if(i-coins[j]>=0) dp[i]=min(dp[i],dp[i-coins[j]]+1);
+        if(i-coins[j]>=0)
+          dp[i]=min(dp[i],dp[i-coins[j]]+1);
       }
+    }
     return dp[amount]==INT_MAX/2?-1:dp[amount];
   }
 };
