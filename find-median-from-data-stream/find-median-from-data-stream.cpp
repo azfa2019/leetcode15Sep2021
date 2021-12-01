f6184eb3413494c9131b5bf15db127a487b6ab79
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-01 08:55:44 -0800

    Time: 288 ms (87.43%), Space: 117.2 MB (22.02%) - LeetHub

--- find-median-from-data-stream/find-median-from-data-stream.cpp
+++ find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,36 +1,34 @@
 class MedianFinder {
-  priority_queue<int,vector<int>,greater<>>minHeap;
   priority_queue<int>maxHeap;
-
-  public:
-  MedianFinder() {
-
-  }
-
-  void addNum(int num) {
-    if(maxHeap.size()==0||num<maxHeap.top() ){
-      maxHeap.push(num); 
-      if(maxHeap.size()>minHeap.size()+1){
-        int tmp=maxHeap.top();
-        maxHeap.pop();
-        minHeap.push(tmp);
+  priority_queue<int,vector<int>,greater<>>minHeap;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+      if(maxHeap.empty()) {
+        maxHeap.push(num);
+        return;
       }
-    } else{
-      minHeap.push(num);
+      if(num>maxHeap.top()) minHeap.push(num);
+      else maxHeap.push(num);
       if(minHeap.size()>maxHeap.size()) {
         int tmp=minHeap.top();
         minHeap.pop();
         maxHeap.push(tmp);
       }
+      if(maxHeap.size()>minHeap.size()+1){
+        int tmp=maxHeap.top();
+        maxHeap.pop();
+        minHeap.push(tmp);
+      }
     }
-  }
-
-  double findMedian() {
-    if(maxHeap.size()==minHeap.size()) 
-      return ((double)minHeap.top()+maxHeap.top())/2.0;
-    else 
-      return (double)maxHeap.top();
-  }
+    
+    double findMedian() {
+      if(minHeap.size()==maxHeap.size()) return double(minHeap.top()+maxHeap.top())/2.0;
+      return maxHeap.top();
+    }
 };
 
 /**
