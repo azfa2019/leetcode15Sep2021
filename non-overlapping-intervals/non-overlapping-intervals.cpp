6757aebb636271459ef8e83118bbed744dc58d3b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-01 23:22:55 -0800

    Time: 352 ms (96.23%), Space: 89.8 MB (81.27%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.cpp
+++ non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,21 +1,26 @@
 class Solution {
-  struct compare{
-    bool operator()(vector<int>&a,vector<int>&b){
-      return a[1]<b[1];
+    struct compare{
+        bool operator()(vector<int>&a,vector<int>&b){
+            return a[1]<b[1];
+        }
+    };
+    public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end(),compare());
+        int end=INT_MIN;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(intervals[i][0]<end) continue;
+            else {
+                end=intervals[i][1];
+                count++;
+            }
+        }
+        return n-count;
     }
-  };
-  public:
-  int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    sort(intervals.begin(),intervals.end(),compare());
-    int i=0;
-    int count=0;
-    while(i<intervals.size()){
-      int j=i+1;
-      count++;
-      while(j<intervals.size() && intervals[j][0]<intervals[i][1]) j++;
-      i=j;
-    }
-
-    return intervals.size()-count; 
-  }
 };
+// 1 2
+//.  2 3
+// 1.  3
+//.    3 4
