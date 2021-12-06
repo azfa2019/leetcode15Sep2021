a30729deb38d999c7c7ee67204e8604c140b423e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-05 17:08:17 -0800

    Time: 0 ms (100.00%), Space: 7.4 MB (53.85%) - LeetHub

---
+++ minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int n=position.size();
+        int ans=INT_MAX;
+        for(int i=1;i<=100;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                if((position[j]-i)%2==0) continue;
+                sum+=1;
+            }
+            ans=min(sum,ans);
+        }
+        return ans;
+    }
+};
+// 2 2 2 3 3 5
