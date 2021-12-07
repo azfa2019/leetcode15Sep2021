06d55d0c32dfe0f14e773ff70f56219fe49c9ec9
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-07 10:29:32 -0800

    Time: 74 ms (5.03%), Space: 27.4 MB (99.94%) - LeetHub

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -1,13 +1,20 @@
 class Solution {
   public:
   int search(vector<int>& nums, int target) {
-    int n=nums.size();
-    int left=0,right=n-1;
-    while(left<right){
-      int mid=left+(right-left+1)/2;
-      if(nums[mid]>target) right=mid-1;
-      else left=mid;
-    }
-    return nums[left]==target?left:-1;
+      int n=nums.size();
+      int l=0,r=n-1;
+      while(l<r){
+          int m=l+((r-l)>>1);
+          if(target>nums[m]) l=m+1;
+          else r=m;
+      }
+      return nums[l]==target?l:-1;
   }
 };
+//.     left. right. mid nums[mid]>target? 
+//5 9   0.    1      1   9, no
+//      1
+
+//.     left. right. mid nums[mid]>target? 
+//5 9   0.    1      0   5, no
+//      0
