cc016c27178ceb3025998874315dc7f9198bc53c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-07 14:06:16 -0800

    Time: 9 ms (9.38%), Space: 11 MB (74.44%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,28 +1,36 @@
 class Solution {
-  public:
-  int search(vector<int>& nums, int target) {
-    int n=nums.size();
-    int left=0,right=n-1;
-    while(left<right){
-      int mid=left+(right-left)/2;
-      if(nums[mid]>nums[left]){ //left is monotonic
-        if(nums[mid]>=target&&target>=nums[left]) right=mid;
-        else left=mid+1;
-      }else{//right side is monotonic
-        if(nums[mid+1]<=target&&target<=nums[right]) left=mid+1;
-        else right=mid;
-      }
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int left=0,right=n-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[left]){
+                if(nums[mid]>=target && target>=nums[left]) right=mid;
+                else left=mid+1;
+            }else{
+                if(nums[mid+1]<=target && target<=nums[right]) left=mid+1;
+                else right=mid;
+            }
+        }
+        return nums[left]==target?left:-1;
     }
-    cout<<left<<endl;
-    return nums[left]==target?left:-1;
-  }
 };
-
-//4,5,6,7,0,1,2, m>right && nums[mid]>target, right=mid-1;
-// else left=mid
-// compare: target & left/right, or mid and left/right?
-// if target>nums[left] && mid>target, right=mid-1
-//else left=mid
-//6,7,0,1,2,4,5, 7 is target, mid=1
-//compare target and nums[left] no go
-//mid > left: left monotonic, 
+//left=0,right=1
+// mid=0
+//
+// 5,6,7,8,9,0,1
+//.  t   m
+//nums[m]>t, r=m-1
+// else l=m
+// 0,1,2,4,5,6,7
+//.  t   m
+//nums[m]>t, r=m-1
+// else l=m
+// 8,9,0,1,2,3,4
+//   t   m
+// need to decide if left is mono or right is mono
+// 8,9,0,1,2,3,4
+//       m   t
+// nums[mid]<t, left=mid+1;
+// else right=mid
