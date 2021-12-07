ca1a572d82c88c92624ed5ad2f976f08d11bda53
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-07 07:22:17 -0800

    Time: 5 ms (7.49%), Space: 6.1 MB (96.06%) - LeetHub

--- valid-parentheses/valid-parentheses.cpp
+++ valid-parentheses/valid-parentheses.cpp
@@ -1,20 +1,17 @@
 class Solution {
-  public:
-  bool isValid(string s) {
-    stack<char>stack0;
-    for(char c:s){
-      if(c=='('||c=='['||c=='{') stack0.push(c);
-      if(c==')') {
-        if(stack0.empty() || stack0.top()!='(') return false;
-        stack0.pop();
-      }else if(c==']') {
-        if(stack0.empty() ||stack0.top()!='[') return false;
-        stack0.pop();
-      }else if(c=='}') {
-        if(stack0.empty() ||stack0.top()!='{') return false;
-        stack0.pop();
-      }
+    public:
+    bool isValid(string s) {
+        stack<char>stk;
+        for(char c:s){
+            if(c=='(' || c=='[' || c=='{') stk.push(c);
+            else {
+                if (stk.empty()) return false;
+                if(c==')' && stk.top()!='(') return false;
+                if(c==']' && stk.top()!='[') return false;
+                if(c=='}' && stk.top()!='{') return false;
+                stk.pop();
+            }
+        }
+        return stk.empty();
     }
-    return stack0.size()==0;
-  }
 };
