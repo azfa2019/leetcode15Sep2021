b3dabfaa5d5f533b67f1be84838b9765b847a59a
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-06 22:10:45 -0800

    Time: 124 ms (38.08%), Space: 20.1 MB (65.88%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
-  public:
-  vector<vector<int>> threeSum(vector<int>& nums) {
-    int n=nums.size();
-    vector<vector<int>>ans;
-    sort(nums.begin(),nums.end());
-    for(int i=0;i<n-2;i++){
-      while(i>0 && i<n && nums[i]==nums[i-1]) i++;
-      findpair(nums,i,ans);
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left=i+1,right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum<0) left++;
+                else if(sum>0) right--;
+                else {
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    left++,right--;
+                    while(left<right && nums[left]==nums[left-1]) left++;
+                    while(left<right && nums[right]==nums[right+1]) right--;
+                }
+            }
+        }
+        return ans;
     }
-    return ans;
-  }
-  void findpair(vector<int>& nums,int i,vector<vector<int>>&ans){
-    int left=i+1,right=nums.size()-1;
-    while(left<right){
-      int sum=nums[left]+nums[right]+nums[i];
-      if(sum==0){
-        ans.push_back({nums[i],nums[left],nums[right]});
-        left++;
-        right--;
-        while(left<right && nums[left]==nums[left-1]) left++;
-        while(left<right && nums[right]==nums[right+1]) right--;
-      }else if(sum<0) left++;
-      else right--;
-    }
-  }
 };
