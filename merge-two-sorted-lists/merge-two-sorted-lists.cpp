a95709e0ac493dbd770a95458e888cc1165c9107
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-07 07:55:19 -0800

    Time: 15 ms (11.89%), Space: 14.8 MB (87.57%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,27 +11,27 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-      ListNode* cur=new ListNode(-1);
-      ListNode* dummy=cur;
-      auto p1=l1,p2=l2;
-      while(p1!=nullptr && p2!=nullptr){
-        if(p1->val<p2->val){
-          cur->next=p1;
-          p1=p1->next;
-        }else{
-          cur->next=p2;
-          p2=p2->next;
+        ListNode* cur=new ListNode(-1);
+        ListNode* pre=cur;
+        while(l1!=nullptr && l2!=nullptr){
+            if(l1->val<l2->val) {
+                cur->next=l1;
+                l1=l1->next;
+            }else{
+                cur->next=l2;
+                l2=l2->next;
+            }
+            cur=cur->next;
         }
-        cur=cur->next;
-      }
-      if(p2==nullptr) cur->next=p1;
-      else cur->next=p2;
-      
-      return dummy->next;
+        if(l1==nullptr) cur->next=l2;
+        else cur->next=l1;
+        return pre->next;
     }
 };
-//         p1
-// 1   2   4
-// 1   3   4
+//     c  l1
+// 1 > 2   4
+// ^
+//>1   3   4
+//    l2
 //         cur p2
-// 1 1 2 3 4 
+// 1 2 
