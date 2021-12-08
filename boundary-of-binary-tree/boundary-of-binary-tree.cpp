5ad84a52cf1c399c523c10d552114ceb972aae53
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-08 09:56:01 -0800

    Time: 12 ms (58.77%), Space: 21 MB (60.55%) - LeetHub

--- boundary-of-binary-tree/boundary-of-binary-tree.cpp
+++ boundary-of-binary-tree/boundary-of-binary-tree.cpp
@@ -10,34 +10,39 @@
  * };
  */
 class Solution {
+    vector<int>ans;
 public:
-vector<int> boundaryOfBinaryTree(TreeNode* root) {
-        if (!root) return {};
-        vector<int> res;
-        if (root->left || root->right) res.push_back(root->val);
-        leftBoundary(root->left, res);
-        leaves(root, res);
-        rightBoundary(root->right, res);
-        return res;
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if(root==nullptr) return {};
+        if(root->left!=nullptr || root->right!=nullptr) ans.push_back(root->val);
+        lhelper(root->left);
+        leaf(root);
+        rhelper(root->right);
+        return ans;
     }
-    void leftBoundary(TreeNode* node, vector<int>& res) {
-        if (!node || (!node->left && !node->right)) return;
-        res.push_back(node->val);
-        if (!node->left) leftBoundary(node->right, res);
-        else leftBoundary(node->left, res);
+    void lhelper(TreeNode* node){
+        if(node==nullptr) return;
+        if(node->left==nullptr && node->right==nullptr) return;
+        ans.push_back(node->val);
+        if(node->left!=nullptr) lhelper(node->left);
+        else lhelper(node->right);
     }
-    void rightBoundary(TreeNode* node, vector<int>& res) {
-        if (!node || (!node->left && !node->right)) return;
-        if (!node->right) rightBoundary(node->left, res);
-        else rightBoundary(node->right, res);
-        res.push_back(node->val);
+    void rhelper(TreeNode* node){
+        if(node==nullptr) return;
+        if(node->left==nullptr && node->right==nullptr) return;
+        if(node->right==nullptr) rhelper(node->left);
+        else rhelper(node->right);
+        ans.push_back(node->val);
     }
-    void leaves(TreeNode* node, vector<int>& res) {
-        if (!node) return;
-        if (!node->left && !node->right) {
-            res.push_back(node->val);
-        }
-        leaves(node->left, res);
-        leaves(node->right, res);
+    void leaf(TreeNode* node){
+        if(node==nullptr) return;
+        if(node->left==nullptr && node->right==nullptr) ans.push_back(node->val);
+        leaf(node->left);
+        leaf(node->right);
     }
 };
+//   1
+//    4
+//. 3  
+// 2 
+//5
