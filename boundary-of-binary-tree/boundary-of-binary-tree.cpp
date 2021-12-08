a5799fb20998814cba59667c83a00e6c29ddfcf5
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-08 09:47:59 -0800

    Time: 15 ms (29.38%), Space: 21 MB (60.55%) - LeetHub

---
+++ boundary-of-binary-tree/boundary-of-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if (!root) return {};
+        vector<int> res;
+        if (root->left || root->right) res.push_back(root->val);
+        leftBoundary(root->left, res);
+        leaves(root, res);
+        rightBoundary(root->right, res);
+        return res;
+    }
+    void leftBoundary(TreeNode* node, vector<int>& res) {
+        if (!node || (!node->left && !node->right)) return;
+        res.push_back(node->val);
+        if (!node->left) leftBoundary(node->right, res);
+        else leftBoundary(node->left, res);
+    }
+    void rightBoundary(TreeNode* node, vector<int>& res) {
+        if (!node || (!node->left && !node->right)) return;
+        if (!node->right) rightBoundary(node->left, res);
+        else rightBoundary(node->right, res);
+        res.push_back(node->val);
+    }
+    void leaves(TreeNode* node, vector<int>& res) {
+        if (!node) return;
+        if (!node->left && !node->right) {
+            res.push_back(node->val);
+        }
+        leaves(node->left, res);
+        leaves(node->right, res);
+    }
+};
