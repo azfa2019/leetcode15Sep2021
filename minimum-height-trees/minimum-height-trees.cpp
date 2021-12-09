c20a3c4c40f5cd586e795ff9b0ea870536676a74
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-09 09:53:27 -0800

    Time: 100 ms (32.65%), Space: 35.4 MB (31.20%) - LeetHub

--- minimum-height-trees/minimum-height-trees.cpp
+++ minimum-height-trees/minimum-height-trees.cpp
@@ -1,37 +1,37 @@
 class Solution {
-public:
+    public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         if(edges.size()==0) return {0};
         if(edges.size()==1) return {edges[0]};
         unordered_map<int,vector<int>>graph;
         unordered_map<int,int>inDegree;
-        vector<int>res;
-        queue<int>leaf;
         for(int i=0;i<n;i++) inDegree[i]=0;
-        for(auto e:edges){
+        queue<int>q;
+        for(auto e:edges) {
             graph[e[0]].push_back(e[1]);
             graph[e[1]].push_back(e[0]);
             inDegree[e[0]]++;
             inDegree[e[1]]++;
         }
-        for(auto e:inDegree) if(e.second==1) leaf.push(e.first);
+        for(auto e:inDegree) 
+            if(e.second==1) q.push(e.first);
         int numNodes=n;
         while(numNodes>2){
-            int leafSize=leaf.size();
+            int leafSize=q.size();
             numNodes-=leafSize;
-            for(int i=0;i<leafSize;i++){
-                int cur=leaf.front();
-                leaf.pop();
-                for(int child:graph[cur]){
-                    inDegree[child]--;
-                    if(inDegree[child]==1) leaf.push(child);
+            while(leafSize--){
+                int cur=q.front();q.pop();
+                for(int connect:graph[cur]){
+                    inDegree[connect]--;
+                    if(inDegree[connect]==1) q.push(connect);
                 }
             }
         }
-        while(!leaf.empty()) {
-            res.push_back(leaf.front());
-            leaf.pop();
+        vector<int>ans;
+        while(!q.empty()){
+            ans.push_back(q.front());
+            q.pop();
         }
-        return res;
+        return ans;
     }
 };
