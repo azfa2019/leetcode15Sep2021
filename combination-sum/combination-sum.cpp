13713310e0c18597e110b8f51ebda6f6aa280d60
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-09 14:27:33 -0800

    Time: 4 ms (93.23%), Space: 11 MB (60.43%) - LeetHub

--- combination-sum/combination-sum.cpp
+++ combination-sum/combination-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
-  int n;
-  vector<int> candidates;
-  vector<vector<int>>ans;
-  public:
-  vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-    this->n=candidates.size();
-    this->candidates=candidates;
-    vector<int>cur;
-    helper(0,cur,target);
-    return ans;
-  }
-  void helper(int idx,vector<int>&cur,int remain){
-    if(idx==n || remain<0) return;
-    if(remain==0) {
-      ans.push_back(cur);
-      return;
+    vector<vector<int>>ans;
+    int n;
+    public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        this->n=candidates.size();
+        vector<int>cur;
+        dfs(0,cur,candidates,target);
+        return ans;
     }
-    for(int i=idx;i<n;i++){
-      cur.push_back(candidates[i]);
-      helper(i,cur,remain-candidates[i]);
-      cur.pop_back();
+    void dfs(int idx,vector<int>&cur,vector<int>& candidates,int remain){
+        if(remain<0) return;
+        if(remain==0) ans.push_back(cur);
+        for(int i=idx;i<n;i++){
+            cur.push_back(candidates[i]);
+            dfs(i,cur,candidates,remain-candidates[i]); 
+            cur.pop_back();
+        }
     }
-  }
 };
