1a20e474b34f9f54b1beadb9350bce551247fd18
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-09 00:18:32 -0800

    Time: 8 ms (69.19%), Space: 13.7 MB (62.79%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,36 @@
 class Solution {
-  public:
-  vector<int> searchRange(vector<int>& nums, int target) {
-    int n=nums.size();
-    if(n==0) return {-1,-1};
-    if(target<nums[0]||target>nums[n-1]) return {-1,-1};
-    int left=0,right=n-1;
-    while(left<right){
-      int mid=left+(right-left)/2;
-      if(nums[mid]>=target) right=mid;
-      else left=mid+1;
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        if(n==0) return {-1,-1};
+        int left=0,right=n-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<target) left=mid+1;
+            else right=mid;
+        }
+        if(nums[left]!=target) return {-1,-1};
+        int left0=left;
+        left=0,right=n-1;
+        while(left<right){
+            int mid=left+(right-left+1)/2;
+            if(nums[mid]<=target) left=mid;
+            else right=mid-1;
+        }
+        return {left0,left};
     }
-    int l=nums[left]==target?left:-1;
-    if(l==-1) return {-1,-1};
-    
-    left=0,right=n-1;
-    while(left<right){
-      int mid=left+(right-left+1)/2;
-      if(nums[mid]<=target) left=mid;
-      else right=mid-1;
-    }
-    int r=left;
-    return {l,r};
-  }
 };
-//class Solution {
-//public:
-//    vector<int> searchRange(vector<int>& nums, int target) {//30
-//        //idx=mid, then expand: start=mid+1, or end=mid-1;
-//        vector<int>res{-1,-1};
-//        res[0]=findRange(nums,target,false);
-//        if(res[0]!=-1) res[1]=findRange(nums,target,true);
-//        return res;
-//    }
-//    int findRange(vector<int>& nums,int target,bool findMax){
-//        int start=0,end=nums.size()-1;
-//        int idx=-1;
-//        while(start<=end){
-//            int mid=start+(end-start)/2;
-//            if(target<nums[mid]) end=mid-1;
-//            else if(target>nums[mid]) start=mid+1;
-//            else {
-//                idx=mid;
-//                if(findMax) start=mid+1;
-//                else end=mid-1;
-//            }
-//        }
-//        return idx;
-//    }
-//};
+//5,5,5,8,8,10 nums[mid]=target, left=mid
+//. t m 
+//5,5,6,8,8,10 nums[mid]>target, right=mid-1
+//. t m 
+//5,5,6,8,8,10 nums[mid]<target, left=mid
+//.   m t 
+//5,5,7,8,8,10
+//. t m 
+//5,7,7,8,8,10
+//.   m t
+//5,7,8,8,8,10
+//.   m t
+//5,7,8,8,8,10
+//. t m
