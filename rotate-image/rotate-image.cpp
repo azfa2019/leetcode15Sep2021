98ee8cf9330c4569ef962635c931afb0e36b5247
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-10 00:09:44 -0800

    Time: 4 ms (54.32%), Space: 7.1 MB (71.57%) - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -1,19 +1,19 @@
 class Solution {
-  public:
-  void rotate(vector<vector<int>>& matrix) {
-    int n=matrix.size();
-    for(int l=0;l<n/2;l++){
-      int s=l+1,e=n-1-l;
-      int tmp;
-      for(int i=s;i<=e;i++){
-        tmp=matrix[l][i];
-        matrix[l][i]=matrix[n-1-i][l];
-        matrix[n-1-i][l]=matrix[n-1-l][n-1-i];
-        matrix[n-1-l][n-1-i]=matrix[i][n-1-l];
-        matrix[i][n-1-l]=tmp;
-      }
+    public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int l=0;l<n/2;l++){
+            int cs=l,ce=n-1-l;
+            int rs=l,re=n-1-l;
+            for(int i=1;i<=n-1-2*l;i++){
+                int tmp=matrix[rs][cs+i];
+                matrix[rs][cs+i]=matrix[re-i][cs];
+                matrix[re-i][cs]=matrix[re][ce-i];
+                matrix[re][ce-i]=matrix[rs+i][ce];
+                matrix[rs+i][ce]=tmp;
+            }
+        }
     }
-  }
 };
 // x 15 13 2 5 x  l=1, 
 //1 4 8
