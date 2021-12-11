c1384c579800ff68be37e21d93f633355af4443a
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-11 13:56:13 -0800

    Time: 112 ms (50.73%), Space: 70.4 MB (6.63%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        return divide(nums,0,nums.size()-1);
-    }
-    int divide(vector<int>& nums,int l,int r){
-        if(l==r) return nums[l];
-        int mid=l+(r-l)/2;
-        int ls=divide(nums,l,mid);
-        int rs=divide(nums,mid+1,r);
-        int lsum=0,lsumMax=INT_MIN;
-        for(int i=mid;i>=l;i--){
-            lsum+=nums[i];
-            lsumMax=max(lsumMax,lsum);
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            dp[i]=(dp[i-1]>0?dp[i-1]:0)+nums[i];
+            ans=max(ans,dp[i]);
         }
-        int rsum=0,rsumMax=INT_MIN;
-        for(int i=mid+1;i<=r;i++){
-            rsum+=nums[i];
-            rsumMax=max(rsumMax,rsum);
-        }
-        int xsum=lsumMax+rsumMax;
-        return max(max(ls,rs),xsum);
+        return ans;
     }
 };
