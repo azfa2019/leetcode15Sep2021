f6e34999d749af0b26909c69a81b7674afd3a7f3
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-12 11:55:07 -0800

    Time: 0 ms (100.00%), Space: 6.9 MB (29.13%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -1,21 +1,40 @@
 class Solution {
-  public:
-  vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    int m=matrix.size(),n=matrix[0].size();
-    int rs=0,cs=0,re=m-1,ce=n-1;
-    vector<int>ans;
-    while(rs<re&&cs<ce){
-      for(int j=cs;j<ce;j++) ans.push_back(matrix[rs][j]);
-      for(int i=rs;i<re;i++) ans.push_back(matrix[i][ce]);
-      //if(re>rs)
-        for(int j=ce;j>cs;j--) ans.push_back(matrix[re][j]);
-      //if(ce>cs)
-        for(int i=re;i>rs;i--) ans.push_back(matrix[i][cs]);
-      rs++,re--;
-      cs++,ce--;
+    public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        int layer=min(m/2,n/2);
+        vector<int>ans;
+        int rs=0,cs=0,re=m-1,ce=n-1;
+        while(rs<re && cs<ce){
+            for(int j=cs;j<ce;j++) 
+                ans.push_back(matrix[rs][j]);
+            for(int i=rs;i<re;i++)
+                ans.push_back(matrix[i][ce]);
+            for(int j=ce;j>cs;j--) 
+                ans.push_back(matrix[re][j]);
+            for(int i=re;i>rs;i--)
+                ans.push_back(matrix[i][cs]);
+            rs++,re--;
+            cs++,ce--;
+        }
+        if(rs==re){
+            for(int j=cs;j<=ce;j++) ans.push_back(matrix[rs][j]);
+        } else if(cs==ce){
+            for(int i=rs;i<=re;i++) ans.push_back(matrix[i][cs]);
+        } 
+        return ans;
     }
-    if(rs==re) for(int i=cs;i<=ce;i++) ans.push_back(matrix[rs][i]);
-    else if(cs==ce) for(int i=rs;i<=re;i++) ans.push_back(matrix[i][cs]); 
-    return ans;
-  }
 };
+// if m<n, last are rows
+//m=4,n=5
+// xxxxx
+// xxxxx
+// xxxxx
+// xxxxx
+
+//if m>n, last are columns
+// xxxx
+// xxxx
+// xxxx
+// xxxx
+// xxxx
