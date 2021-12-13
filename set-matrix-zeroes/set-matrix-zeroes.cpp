2379cb1670e0ee13ae4d0f8b5bfcb424e92ce898
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-13 13:40:48 -0800

    Time: 16 ms (49.51%), Space: 13.3 MB (20.03%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,23 +1,26 @@
 class Solution {
-  public:
-  void setZeroes(vector<vector<int>>& matrix) {
-    int m=matrix.size(),n=matrix[0].size();
-    int row0=-1,col0=-1;
-    for(int i=0;i<m;i++)
-      for(int j=0;j<n;j++){
-        if(matrix[i][j]==0) {
-          if(i==0) row0=0;
-          if(j==0) col0=0;
-          matrix[0][j]=0;
-          matrix[i][0]=0;
+    public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        int firstRow=1,firstCol=1;
+        for(int j=0;j<n;j++) if(matrix[0][j]==0) firstRow=0;
+        for(int i=0;i<m;i++) if(matrix[i][0]==0) firstCol=0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0) matrix[0][j]=0,matrix[i][0]=0;
+            }
         }
-      }
-    for(int i=1;i<m;i++) 
-      for(int j=1;j<n;j++){
-        if(matrix[i][0]==0) matrix[i][j]=0;
-        if(matrix[0][j]==0) matrix[i][j]=0;
-      }
-    if(row0==0) for(int j=0;j<n;j++) matrix[0][j]=0;
-    if(col0==0) for(int i=0;i<m;i++) matrix[i][0]=0;
-  }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0)
+                    matrix[i][j]=0;
+                if(matrix[i][0]==0)
+                    matrix[i][j]=0;
+            }
+        }
+        if(firstRow==0) 
+            for(int j=0;j<n;j++) matrix[0][j]=0;
+        if(firstCol==0) 
+            for(int i=0;i<m;i++) matrix[i][0]=0;
+    }
 };
