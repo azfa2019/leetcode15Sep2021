7f23c1961b850003a20188f0b1480eeddbdf6984
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-13 14:33:31 -0800

    Time: 252 ms (71.89%), Space: 7.9 MB (25.60%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,27 +1,29 @@
 class Solution {
-  public:
-  int m,n;
-  vector<int>dir{1,0,-1,0,1};
-  bool exist(vector<vector<char>>& board, string word) {
-    m=board.size(),n=board[0].size();
-    for(int i=0;i<m;i++)
-      for(int j=0;j<n;j++)
-        if(board[i][j]==word[0]) 
-          if (dfs(0,i,j,board,word)) return true;
-    return false;
-  }
-  bool dfs(int idx,int r,int c,vector<vector<char>>& board, string& word){
-    if(idx==word.size()) return true;
-    if(r<0||r>m-1||c<0||c>n-1||board[r][c]=='#'||word[idx]!=board[r][c]) return false;
-    char ch=board[r][c];
-    board[r][c]='#';
-    for(int i=0;i<4;i++){
-      int rn=r+dir[i];
-      int cn=c+dir[i+1];
-      //if(rn<0||rn>m-1||cn<0||cn>n-1||board[rn][cn]=='#') continue;
-      if (dfs(idx+1,rn,cn,board,word)) return true;
-      }
-    board[r][c]=ch;
-    return false;
-  }
+    int m,n;
+    int dir[5]{1,0,-1,0,1};
+    public:
+    bool exist(vector<vector<char>>& board, string word) {
+        m=board.size(),n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0]) 
+                    if (dfs(board,i,j,0,word)) return true;
+            }
+        }
+        return false;
+    }
+    bool dfs(vector<vector<char>>& board,int i,int j, int idx, string& word){
+        if(idx==word.size()) return true;
+        if(i<0||j<0||i>m-1||j>n-1||board[i][j]=='#') return false;
+        if(word[idx]!=board[i][j]) return false;
+        char c=board[i][j];
+        board[i][j]='#';
+        for(int d=0;d<4;d++){
+            int r=i+dir[d];
+            int c=j+dir[d+1];
+            if(dfs(board,r,c,idx+1,word)) return true;
+        }
+        board[i][j]=c;
+        return false;
+    }
 };
