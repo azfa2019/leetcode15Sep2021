7089b4325f4e825fdd355dbe9e9a51f3072d573e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-13 14:05:08 -0800

    Time: 24 ms (45.47%), Space: 16.4 MB (18.37%) - LeetHub

--- minimum-window-substring/minimum-window-substring.cpp
+++ minimum-window-substring/minimum-window-substring.cpp
@@ -1,32 +1,32 @@
 class Solution {
-  public:
-  string minWindow(string s, string t) {
-    int n=s.size();
-    unordered_map<char,int>map0;
-    for(char c:t) map0[c]++;
-    int left=0;
-    int minLen=INT_MAX;
-    string ans;
-    int count=0;
-    for(int right=0;right<n;right++){
-      char r=s[right];
-      if(map0.find(r)!=map0.end()) {
-        map0[r]--;
-        if(map0[r]==0) count++;
-      }
-      while(count==map0.size()){
-        if(minLen>right-left+1){
-          minLen=right-left+1;
-          ans=s.substr(left,minLen);
+public:
+    string minWindow(string s, string t) {
+        int n=s.size();
+        unordered_map<char,int>mp;
+        for(char c:t) mp[c]++;
+        int left=0;
+        int count=0;
+        string ans="";
+        int minLen=INT_MAX;
+        for(int right=0;right<n;right++){
+            char r=s[right];
+            if(mp.find(r)!=mp.end()) {
+                mp[r]--;
+                if(mp[r]==0) count++;
+            }
+            while(count==mp.size()){
+                if(minLen>right-left+1){
+                    minLen=right-left+1;
+                    ans=s.substr(left,minLen);
+                }
+                char l=s[left];
+                left++;
+                if(mp.find(l)!=mp.end()){
+                    if(mp[l]==0) count--;
+                    mp[l]++;
+                }
+            }
         }
-        char l=s[left];
-        left++;
-        if(map0.find(l)!=map0.end()){
-          if(map0[l]==0) count--;
-          map0[l]++;
-        }
-      }
+        return ans;
     }
-    return ans;
-  }
 };
