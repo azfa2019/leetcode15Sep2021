84c0fdb7ec5b39604d5975965ddd709cd17e99d3
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-12 23:57:02 -0800

    Time: 8 ms (98.83%), Space: 17.2 MB (41.25%) - LeetHub

--- insert-interval/insert-interval.cpp
+++ insert-interval/insert-interval.cpp
@@ -1,25 +1,19 @@
 class Solution {
   public:
   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-    int n=intervals.size();
-    vector<vector<int>>ans;
-    int i=0;
-    while(i<n && intervals[i][1]<newInterval[0]) {
-      ans.push_back(intervals[i]);
-      i++;
-    }
-    
-    int left=newInterval[0];
-    int right=newInterval[1];
-      
-    while(i<n && intervals[i][1]>=newInterval[0] && intervals[i][0]<=newInterval[1]){
-      left=min(left,intervals[i][0]);
-      right=max(right,intervals[i][1]);
-      i++;
-    }
-    ans.push_back({left,right});
-    while(i<n) ans.push_back(intervals[i++]);
-    return ans;
+      int n=intervals.size();
+      if(n==0) return {newInterval};
+      int i=0;
+      vector<vector<int>>ans;
+      while(i<n && intervals[i][1]<newInterval[0]) ans.push_back(intervals[i++]);
+      while(i<n&& intervals[i][1]>=newInterval[0] && intervals[i][0]<=newInterval[1] ) {
+          newInterval[0]=min(newInterval[0],intervals[i][0]);
+          newInterval[1]=max(newInterval[1],intervals[i][1]);
+          i++;
+      }
+      ans.push_back(newInterval);
+      while(i<n) ans.push_back(intervals[i++]);
+      return ans;
   }
 };
 // 1,3       6,9
@@ -29,3 +23,5 @@ class Solution {
 
 // 1,13  6,9
 //  2   5
+// 1,2  3, 5  6, 7  8,  10
+//        4         8
