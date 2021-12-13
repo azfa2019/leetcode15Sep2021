7ac10288b353408cd391ce6448d0e90070eb3b51
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-12 19:01:17 -0800

    Time: 16 ms (89.51%), Space: 14.2 MB (81.69%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,12 +1,20 @@
 class Solution {
-  public:
-  vector<vector<int>> merge(vector<vector<int>>& intervals) {
-    vector<vector<int>> ans;
-    sort(intervals.begin(),intervals.end());
-    for(auto e:intervals){
-      if(ans.empty() || ans.back()[1]<e[0]) ans.push_back(e);
-      else ans.back()[1]=max(ans.back()[1],e[1]);
+    public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<n;){
+            ans.push_back(intervals[i]);
+            while(i<n && ans.back()[1]>=intervals[i][0]) {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                i++;
+            }
+        }
+        return ans;
     }
-    return ans;
-  }
 };
+// 1. 3
+//   2.     6
+//              8   10
+//                      15. 18
