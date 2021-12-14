fc6a5e5e19976d20d0ab44a31ea0132626becb4d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-14 14:29:08 -0800

    Time: 128 ms (37.11%), Space: 101.1 MB (6.08%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,18 @@
 class Solution {
-  public:
-  int maxProfit(vector<int>& prices) {
-    int n=prices.size();
-    vector<int>dp0(n,0); //no stock
-    vector<int>dp1(n,0); //with stock
-    dp0[0]=0;
-    dp1[0]=-prices[0];
-    for(int i=1;i<n;i++){
-      dp0[i]=max(dp0[i-1],dp1[i-1]+prices[i]);
-      dp1[i]=max(dp1[i-1],-prices[i]);
+    public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int>dp0(n,0); //no stock
+        vector<int>dp1(n,0); //with stock
+        dp1[0]=-prices[0];
+        dp0[0]=0;
+        for(int i=1;i<n;i++){
+            dp1[i]=max(-prices[i],dp1[i-1]);
+            dp0[i]=max(dp0[i-1],dp1[i-1]+prices[i]);
+        }
+        return dp0[n-1];
     }
-    return dp0[n-1];
-  }
 };
+// states: 
+// stock:with stock, without stock
+// no stock: never had stock, or sold stock
