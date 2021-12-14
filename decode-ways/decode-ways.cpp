6875a5169ed6770072443da045ca6792158f0451
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-13 17:49:44 -0800

    Time: 0 ms (100.00%), Space: 6.2 MB (42.56%) - LeetHub

--- decode-ways/decode-ways.cpp
+++ decode-ways/decode-ways.cpp
@@ -1,32 +1,24 @@
 class Solution {
-  public:
-  int numDecodings(string s) {
-    int n=s.size();
-    s="#"+s;
-    vector<int>dp(n+1,0);
-    dp[0]=1;
-    string first=s.substr(1,1);
-    if(valid(first)) dp[1]=1;
-    for(int i=2;i<=n;i++){
-      string s1=s.substr(i,1);
-      if(valid(s1)) dp[i]+=dp[i-1];
-      string s2=s.substr(i-1,2);
-      // 1110     6
-      //.   i-1   i
-      if(valid(s2)) dp[i]+=dp[i-2];
+    public:
+    int numDecodings(string s) {
+        int n=s.size();
+        vector<int>dp(n+1,0);
+        s="#"+s;
+        dp[0]=1;
+        dp[1]=valid(s.substr(1,1))?1:0;
+        for(int i=2;i<=n;i++){
+            string cur1=s.substr(i,1);
+            string cur2=s.substr(i-1,2);
+            dp[i]=(valid(cur2)?dp[i-2]:0)+(valid(cur1)?dp[i-1]:0);
+        }
+        return dp[n];
     }
-    return dp[n];
-  }
-  bool valid(string s){
-    int num=stoi(s);
-    if(s.size()==1) {
-      if(num<1 || num>9) return false;;
+    bool valid(string s){
+        if(s[0]=='0') return false;
+        if(s.size()==1 && (s<"1"||s>"9")) return false;
+        if(s.size()==2 &&(s<"10"||s>"26")) return false;
+        return true;
     }
-    if(s.size()==2) {
-      if(s[0]=='0') return false;
-      if(num<1 || num>26) return false;
-    }
-    return true;
-  }
 };
-//dp[i]=dp[i-1]+dp[i-2]
+// 2   2    6
+// i-2 i-1  i
