92baa4dea579928eb76c98ec14283f73500d470b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-15 16:04:23 -0800

    Time: 8 ms (63.44%), Space: 8.7 MB (77.29%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,29 +1,22 @@
 class Solution {
     public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>stck;
         int n=nums2.size();
-        stack<int>stack;
-        unordered_map<int,int>map;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            if(stack.empty() || nums2[i]<=stack.top()){
-                stack.push(nums2[i]);
-            }else{
-                while(!stack.empty() && stack.top()<nums2[i]){
-                    map[stack.top()]=nums2[i];
-                    stack.pop();
-                }
-                stack.push(nums2[i]);
+            while(!stck.empty() && nums2[i]>=stck.top()) {
+                mp[stck.top()]=nums2[i];
+                stck.pop();
             }
-        }
-        while(!stack.empty()) {
-            map[stack.top()]=-1;
-            stack.pop();
+            stck.push(nums2[i]);
         }
         vector<int>ans;
-        for(int i=0;i<nums1.size();i++)
-            ans.push_back(map[nums1[i]]);
+        for(int num:nums1){
+            if(mp.find(num)==mp.end()) ans.push_back(-1);
+            else ans.push_back(mp[num]);
+        }
         return ans;
     }
 };
-// 1 2 3 4 2
-// 4 2 1 3
+// 4 1 2
