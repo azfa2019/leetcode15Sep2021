a33d939b77754b703001c5531fde62c85f854694
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-17 23:57:00 -0800

    Time: 8 ms (96.19%), Space: 8.1 MB (56.67%) - LeetHub

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -7,14 +7,16 @@
  * };
  */
 class Solution {
-  public:
-  bool hasCycle(ListNode *head) {
-    ListNode* fast=head, *slow=head;
-    while(fast!=nullptr && fast->next!=nullptr){
-      fast=fast->next->next;
-      slow=slow->next;
-      if(slow==fast) return true;
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast && (fast->next) ){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) return true;
+        }
+        return false;
     }
-    return false;
-  }
 };
