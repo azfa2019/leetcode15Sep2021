d6eeaedd1dcefe608afbfcd4629f84ebd312538f
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-18 23:26:52 -0800

    Time: 4 ms (59.49%), Space: 10.3 MB (21.71%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,35 +1,23 @@
 class Solution {
-  public:
-  int findMin(vector<int>& nums) {
-    int n=nums.size();
-    int left=0,right=n-1;
-    while(left<right){
-      int mid=left+(right-left)/2;
-      if(nums[mid]<nums[right]) right=mid;
-      else left=mid+1;
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int left=0,right=n-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<nums[right]) right=mid;
+            else left=mid+1;
+        }
+        return nums[left];
     }
-    return nums[left];
-  }
 };
-// 3 4 5 1 2
-//     m
-//  5 1 2 3 4
-//.     m
+// 3,4,5,1,2
+// rotate at right, nums[mid]>nums[right]
+// left=mid+1, else right=mid
+// nums[mid]<nums[right] right=mid
+// 6 8 0 1 3 4 5
+// rotate at left, nums[mid]<nums[right]
+// right=mid, else left=mid+1
 // 1 2 3 4 5
-//.    m
-//class Solution {
-//public:
-//    int findMin(vector<int>& nums) {//00
-//        //the pivot has this featuree: the pivot is smaller than the previous number
-//        int n=nums.size();
-//        int start=0,end=n-1;
-//        while(start<=end){
-//            int mid=start+(end-start)/2;
-//            if(mid>start && nums[mid]<nums[mid-1]) return nums[mid];
-//            if(mid<end && nums[mid+1]<nums[mid]) return nums[mid+1];
-//            if(nums[mid]>nums[start]) start=mid+1;//left side is sorted, the pivot is on the right
-//            else end=mid-1;
-//        }
-//        return nums[0];
-//    }
-//};
+// no rotate, nums[mid]<nums[right]
+// right=mid else left=mid+1
