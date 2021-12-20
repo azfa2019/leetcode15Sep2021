8f1db40407528b07e7e47f0885f13870c736f083
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-20 13:19:38 -0800

    Time: 16 ms (98.79%), Space: 24 MB (13.29%) - LeetHub

--- longest-well-performing-interval/longest-well-performing-interval.cpp
+++ longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int longestWPI(vector<int>& hours) {
+        int n=hours.size();
         vector<int>presum{0};
         int sum=0;
-        for(int h:hours) {
-            sum+=(h>8?1:-1);
+        for(int h:hours){
+            if(h>8) sum+=1;
+            else sum+=-1;
             presum.push_back(sum);
         }
-        stack<int>stack;
+        
+        stack<int>stck;
         for(int i=0;i<presum.size();i++){
-            if(stack.empty() || presum[i]<presum[stack.top()]) stack.push(i);
+            if(stck.empty() || presum[stck.top()]>presum[i])
+                stck.push(i);
         }
         int ans=0;
-        for(int i=presum.size()-1;i>=0;i--){
-            while(!stack.empty() && presum[i]>presum[stack.top()]){
-                ans=max(ans,i-stack.top());
-                stack.pop();
+        for(int j=presum.size()-1;j>=0;j--){
+            while(!stck.empty() && presum[j]>presum[stck.top()]) {
+                ans=max(ans,j-stck.top());
+                stck.pop();
             }
         }
         return ans;
