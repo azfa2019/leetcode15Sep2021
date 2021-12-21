feda31255a398dfff738e4c72f3c7438c10028a2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-21 12:26:22 -0800

    Time: 152 ms (87.01%), Space: 84.1 MB (57.40%) - LeetHub

--- number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
+++ number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -1,30 +1,24 @@
 class Solution {
-public:
+    public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
-        stack<int>stck;
         int n=heights.size();
-        vector<int>prevBig(n,-1);
-        vector<int>nextBig(n,-1);
-        for(int i=0;i<n;i++){
-            while(!stck.empty() && heights[stck.top()]<=heights[i]){
-                nextBig[stck.top()]=i;
-                stck.pop();
-            }
-            stck.push(i);
-        }
-        while(!stck.empty()) stck.pop();
-        for(int i=n-1;i>=0;i--){
-            while(!stck.empty() && heights[stck.top()]<=heights[i]){
-                prevBig[stck.top()]=i;
-                stck.pop();
-            }
-            stck.push(i);
-        }
+        stack<int>stck;
         vector<int>ans(n,0);
-        for(int i=0;i<n;i++)
-            if(prevBig[i]!=-1) ans[prevBig[i]]++;
-        for(int i=0;i<n;i++)
-            if(nextBig[i]>i) ans[i]++;
+        for(int i=n-1;i>=0;i--){
+            int count=0;
+            if(stck.empty() || heights[i]<heights[stck.top()]){
+                if(!stck.empty())  ans[i]=1;
+                stck.push(i);
+            }else{
+                while(!stck.empty() && heights[i]>heights[stck.top()]){
+                    count++;
+                    stck.pop();
+                }
+                if(!stck.empty()) count++;
+                ans[i]=count;
+                stck.push(i);
+            }
+        }
         return ans;
     }
 };
