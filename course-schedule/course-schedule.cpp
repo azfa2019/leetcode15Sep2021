7ece805baf41f41271ff971338cac4e4ec7bd346
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-20 20:25:23 -0800

    Time: 28 ms (30.90%), Space: 15.2 MB (19.33%) - LeetHub

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,56 +1,28 @@
 class Solution {
-  public:
-  bool canFinish(int numCourses, vector<vector<int>>& pre) {
-    vector<vector<int>>g(numCourses);
-    for(auto p:pre) g[p[1]].push_back(p[0]);
-    vector<int>flags(numCourses,0);
-    for(int i=0;i<numCourses;i++){
-      if(dfs(i,g,flags)==false) return false;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>>g;
+        unordered_map<int,int>indegree;
+        for(int i=0;i<numCourses;i++) {
+            indegree[i]=0;
+        }
+        for(auto p:prerequisites){
+            g[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+        queue<int>source;
+        for(auto e:indegree){
+            if(e.second==0) source.push(e.first);
+        }
+        vector<int>order;
+        while(!source.empty()){
+            int cur=source.front();source.pop();
+            order.push_back(cur);
+            for(int next:g[cur]){
+                indegree[next]--;
+                if(indegree[next]==0) source.push(next);
+            }
+        }
+        return order.size()==numCourses;
     }
-    return true;
-  }
-  bool dfs(int i,vector<vector<int>>&g,vector<int>&flags){
-    if(flags[i]==1) return false;
-    if(flags[i]==-1) return true;
-    flags[i]=1;
-    for(int next:g[i]){
-      if(dfs(next,g,flags)==false) return false;
-    }
-    flags[i]=-1;
-    return true;
-  }
 };
-//class Solution {
-//  public:
-//  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//    //topological sort
-//    //inDegree: map
-//    //source: vector
-//    //res: vector
-//    //graph: map
-//    //initialize the graph and inDegree
-//    unordered_map<int,int>inDegree;
-//    unordered_map<int,vector<int>>graph;
-//    vector<int>res;
-//    queue<int>source;
-//    
-//    for(int i=0;i<numCourses;i++) inDegree[i]=0;
-//    for(int i=0;i<prerequisites.size();i++) {
-//      int k=prerequisites[i][0];
-//      int v=prerequisites[i][1];
-//      graph[k].push_back(v);
-//      inDegree[v]++;
-//    }
-//    for(auto e:inDegree) if(e.second==0) source.push(e.first);
-//    while(!source.empty()){
-//      int cur=source.front();
-//      source.pop();
-//      res.push_back(cur);
-//      for(int child:graph[cur]){
-//        inDegree[child]--;
-//        if(inDegree[child]==0) source.push(child);
-//      }
-//    }
-//    return res.size()==numCourses;
-//  }
-//};
