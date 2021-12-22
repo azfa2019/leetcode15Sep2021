6ffad20303d77277f337a29e003967ed372dcb70
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-21 18:15:22 -0800

    Time: 8 ms (29.61%), Space: 7.3 MB (66.78%) - LeetHub

---
+++ string-transforms-into-another-string/string-transforms-into-another-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConvert(string str1, string str2) {
+        if(str1==str2) return true;
+        unordered_map<char,char>mp;
+        unordered_set<char>st;
+        int n=str1.size();
+        for(int i=0;i<n;i++){
+            char c1=str1[i];
+            char c2=str2[i];
+            st.insert(c2);
+            if(mp.find(c1)==mp.end()){
+                mp[c1]=c2;
+            }else{
+                if(mp[c1]!=c2) return false;
+            }
+        }
+        if(mp.size()==26 && st.size()==26) return false;
+        return true;
+    }
+};
