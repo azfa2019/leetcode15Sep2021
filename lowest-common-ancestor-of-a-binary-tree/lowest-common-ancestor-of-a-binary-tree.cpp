8bc9c2bd30362fbfb58a0ff67b0a1ffd754b2877
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-22 20:26:07 -0800

    Time: 16 ms (70.50%), Space: 14.3 MB (57.36%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,33 +8,13 @@
  * };
  */
 class Solution {
-  public:
-  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-      unordered_map<TreeNode*,TreeNode*>mp;
-      queue<TreeNode*>q0;
-      mp[root]=nullptr;
-      q0.push(root);
-      while(!q0.empty()){
-          auto cur=q0.front();q0.pop();
-          if(cur->left) {
-              q0.push(cur->left);
-              mp[cur->left]=cur;
-          }
-          if(cur->right) {
-              q0.push(cur->right);
-              mp[cur->right]=cur;
-          }
-          if(mp.find(p)!=mp.end() && mp.find(q)!=mp.end()) break;
-      }
-      unordered_set<TreeNode*>st;
-      while(p!=nullptr){
-          st.insert(p);
-          p=mp[p];
-      }
-      while(q!=nullptr){
-          if(st.find(q)!=st.end()) return q;
-          q=mp[q];
-      }
-      return nullptr;
-  }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==nullptr) return nullptr;
+        if(root==p || root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left, p, q);
+        TreeNode* right=lowestCommonAncestor(root->right, p, q);
+        if(left!=nullptr && right!=nullptr) return root;
+        return left?left:right;
+    }
 };
