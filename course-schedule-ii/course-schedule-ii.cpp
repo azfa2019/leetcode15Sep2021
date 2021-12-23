2de5dc2f7494afdd341f6a15ff1157494f5662a1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-22 18:43:18 -0800

    Time: 28 ms (29.88%), Space: 15.4 MB (15.18%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    vector<int>ans;
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=prerequisites.size();
+        if(n==0) {
+            vector<int>ans;
+            for(int i=0;i<numCourses;i++) ans.push_back(i);
+            return ans;
+        }
+        unordered_map<int,vector<int>>g;
+        for(auto p:prerequisites){
+            g[p[1]].push_back(p[0]);
+        }
+        vector<int>flag(numCourses);
+        stack<int>stck;
+        for(int i=0;i<numCourses;i++){
+            if(dfs(i,g,flag)==false) return {};
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    bool dfs(int i,unordered_map<int,vector<int>>&g,vector<int>&flag){
+        if(flag[i]==1) return false;
+        if(flag[i]==-1) return true;
+        flag[i]=1;
+        for(int next:g[i]){
+            if(dfs(next,g,flag)==false) return false;
+        }
+        flag[i]=-1;
+        ans.push_back(i);
+        return true;
+    }
+};
