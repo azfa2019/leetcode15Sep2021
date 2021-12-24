741af307163c7ba349d89fad5ef2ac5e61cc7b57
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-24 13:57:13 -0800

    Time: 64 ms (75.19%), Space: 14.5 MB (48.16%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,22 +1,14 @@
 class Solution {
-    vector<int>dp;
-    vector<int>coins;
-public:
+    public:
     int coinChange(vector<int>& coins, int amount) {
-        this->coins=coins;
-        this->dp=vector<int>(amount+1,0);
-        return helper(amount);
-    }
-    int helper(int amount){
-        if(amount==0) return 0;
-        if(amount<0) return -1;
-        if(dp[amount]!=0) return dp[amount];
-        int ans=INT_MAX;
-        for(int c:coins){
-            int tmp=helper(amount-c);
-            if(tmp!=-1) ans=min(ans,tmp+1);
+        vector<int>dp(amount+1,INT_MAX/2);
+        dp[0]=0;
+        for(int i=0;i<=amount;i++){
+            for(int c:coins){
+                if(i-c>=0)
+                    dp[i]=min(dp[i-c]+1,dp[i]);
+            }
         }
-        dp[amount]=(ans==INT_MAX?-1:ans);
-        return dp[amount];
+        return dp[amount]==INT_MAX/2?-1:dp[amount];
     }
 };
