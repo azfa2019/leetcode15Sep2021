4d265bf2e5e3653712cf9c5def870a2ce22f4f7a
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-24 14:56:22 -0800

    Time: 20 ms (69.57%), Space: 14.9 MB (19.20%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,37 +1,23 @@
-class UF{
-    public:
-    vector<int>parent;
-    vector<int>rank;
-    UF(int n){
-        parent=vector<int>(n,0);
-        rank=vector<int>(n,0);
-        for(int i=0;i<n;i++) parent[i]=i;
-    }
-    int findParent(int i){
-        if(i!=parent[i]) parent[i]=findParent(parent[i]);
-        return parent[i];
-    }
-    bool unionNodes(int x,int y){
-        int px=findParent(x);
-        int py=findParent(y);
-        if(px==py) return false;
-        if(rank[px]<rank[py]) parent[px]=py;
-        else if(rank[px]>rank[py]) parent[py]=px;
-        else parent[px]=py,rank[py]++;
-        return true;
-    }
-};
 class Solution {
-public:
+    public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        UF uf=UF(n);
-        for(auto e:edges){
-            uf.unionNodes(e[0],e[1]);
+        vector<int>visited(n,0);
+        int count=0;
+        vector<vector<int>>g(n);
+        for(auto e:edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
         }
-        unordered_set<int>st;
         for(int i=0;i<n;i++){
-            st.insert(uf.findParent(i));
+            if(visited[i]==0) count++;
+            dfs(i,g,visited);
         }
-        return st.size();
+        return count;
+    }
+    void dfs(int i,vector<vector<int>>&g,vector<int>&visited){
+        if(visited[i]==1) return;
+        visited[i]=1;
+        for(auto next:g[i]) 
+            dfs(next,g,visited);
     }
 };
