5ffc4f542b52d280a6a5b380c40eb703f01e6552
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-24 11:48:39 -0800

    Time: 52 ms (30.15%), Space: 81.2 MB (10.63%) - LeetHub

--- encode-and-decode-strings/encode-and-decode-strings.cpp
+++ encode-and-decode-strings/encode-and-decode-strings.cpp
@@ -1,28 +1,31 @@
 class Codec {
-  public:
+    public:
 
-  // Encodes a list of strings to a single string.
-  string encode(vector<string>& strs) {
-    string ans;
-    for(auto s:strs){
-      ans+=to_string(s.size())+"@"+s;
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string ans;
+        for(auto s:strs){
+            auto len=to_string(s.size());
+            ans+=len+"#"+s;
+        }
+        return ans;
     }
-    return ans;
-  }
 
-  // Decodes a single string to a list of strings.
-  vector<string> decode(string s) {
-    vector<string>ans;
-    while(s!=""){
-      int pos=s.find("@");
-      int len=stoi(s.substr(0,pos));
-      ans.push_back(s.substr(pos+1,len));
-      s=s.substr(pos+1+len);
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string>ans;
+        while(s!=""){
+            int pos=s.find("#");
+            int len=stoi(s.substr(0,pos));
+            ans.push_back(s.substr(pos+1,len));
+            s=s.substr(pos+len+1);
+        }
+        return ans;
     }
-    return ans;
-  }
 };
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
 // codec.decode(codec.encode(strs));
+// # a b c d
+// p 
