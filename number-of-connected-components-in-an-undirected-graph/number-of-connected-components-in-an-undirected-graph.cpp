550611e935550740ad9bd8218ef4aaa110bf24cb
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-24 14:43:22 -0800

    Time: 16 ms (89.32%), Space: 12.7 MB (60.25%) - LeetHub

---
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,37 @@
+class UF{
+    public:
+    vector<int>parent;
+    vector<int>rank;
+    UF(int n){
+        parent=vector<int>(n,0);
+        rank=vector<int>(n,0);
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+    int findParent(int i){
+        if(i!=parent[i]) parent[i]=findParent(parent[i]);
+        return parent[i];
+    }
+    bool unionNodes(int x,int y){
+        int px=findParent(x);
+        int py=findParent(y);
+        if(px==py) return false;
+        if(rank[px]<rank[py]) parent[px]=py;
+        else if(rank[px]>rank[py]) parent[py]=px;
+        else parent[px]=py,rank[py]++;
+        return true;
+    }
+};
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        UF uf=UF(n);
+        for(auto e:edges){
+            uf.unionNodes(e[0],e[1]);
+        }
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(uf.findParent(i));
+        }
+        return st.size();
+    }
+};
