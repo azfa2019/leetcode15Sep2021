959e7d758df64cbabb498a7d84f86633b9a17f89
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-23 19:54:19 -0800

    Time: 24 ms (34.36%), Space: 15.5 MB (10.32%) - LeetHub

--- graph-valid-tree/graph-valid-tree.cpp
+++ graph-valid-tree/graph-valid-tree.cpp
@@ -1,37 +1,26 @@
 class Solution {
-    class UF{
-        public:
-        vector<int>parent;
-        vector<int>rank;
-        UF(int n){
-            parent=vector<int>(n,0);
-            rank=vector<int>(n,0);
-            for(int i=0;i<parent.size();i++) parent[i]=i;
-        }
-        bool unionNodes(int n1,int n2){
-            int p1=findroot(n1);
-            int p2=findroot(n2);
-            if(p1==p2) return false;
-            if(rank[p1]<rank[p2]) parent[p1]=p2;
-            else if(rank[p1]>rank[p2]) parent[p2]=p1;
-            else parent[p2]=p1,rank[p1]++;
-            return true;
-        }
-        int findroot(int n){
-            if(n!=parent[n]) parent[n]=findroot(parent[n]);
-            return parent[n];
-        }
-    };
     public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        UF uf=UF(n);
-        for(auto e:edges){
-            if(uf.unionNodes(e[0],e[1])==false) return false;
+        vector<int>visited(n,0);
+        unordered_map<int,vector<int>>g;
+        for(auto e:edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
         }
-        int root=uf.findroot(0);
-        for(int i=0;i<n;i++){
-            if(root!=uf.findroot(i)) return false;
+        if(dfs(0,visited,g,-1)==false) return false;
+        for(int i=0;i<n;i++)
+            if(visited[i]!=-1) return false;
+        return true;
+    }
+    bool dfs(int i,vector<int>&visited, unordered_map<int,vector<int>>&g,int pre){
+        if(visited[i]==1) return false;
+        if(visited[i]==-1) return true;
+        visited[i]=1;
+        for(int next:g[i]){
+            if(next!=pre)
+                if(dfs(next,visited,g,i)==false) return false;
         }
+        visited[i]=-1;
         return true;
     }
 };
