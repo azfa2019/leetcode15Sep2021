e1fa60f1136445a106e2201f609bb589024a9bbb
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-24 21:50:03 -0800

    Time: 0 ms (100.00%), Space: 6 MB (24.49%) - LeetHub

--- sum-of-two-integers/sum-of-two-integers.cpp
+++ sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
+        int a0,b0;
         while(b!=0){
-            int carry=((unsigned int)(a&b))<<1;
-            a^=b;
-            b=carry;
+            a0=a;
+            b0=b;
+            a=(a0^b0);
+            b=((unsigned int)(a0&b0)<<1);
         }
         return a;
     }
