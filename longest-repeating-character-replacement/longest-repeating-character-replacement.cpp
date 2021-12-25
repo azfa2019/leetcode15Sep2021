26f80a87f9ef0e985a655d5776f1cc1ca39a4946
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-25 11:28:50 -0800

    Time: 12 ms (58.91%), Space: 7 MB (82.76%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int ans=1;
-        int n=s.size();
-        int maxRepeat=1;
-        int left=0;
         unordered_map<char,int>mp;
-        for(int right=0;right<n;right++){
+        int left=0,right=0;
+        int n=s.size();
+        int maxRep=0,ans=0;
+        for( right=0;right<n;right++){
             char r=s[right];
             mp[r]++;
-            maxRepeat=max(maxRepeat,mp[r]);
-            while(right-left+1>maxRepeat+k) {
-                char l=s[left];
+            maxRep=max(maxRep,mp[r]);
+            while(right-left+1>maxRep+k){
+                char l=s[left++];
                 mp[l]--;
-                left++;
             }
             ans=max(ans,right-left+1);
         }
         return ans;
     }
 };
-//AABABBA
