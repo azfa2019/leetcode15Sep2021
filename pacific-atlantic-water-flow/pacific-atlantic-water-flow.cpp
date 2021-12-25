f09548bf326baae551d58891f12be1f936312513
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-24 22:12:28 -0800

    Time: 36 ms (86.56%), Space: 19.1 MB (47.79%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -3,17 +3,16 @@ class Solution {
     int dir[5]={1,0,-1,0,1};
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        this->m=heights.size();
-        this->n=heights[0].size();
-        vector<vector<int>>pac(m,vector<int>(n,0));
+        this->m=heights.size(),this->n=heights[0].size();
         vector<vector<int>>atl(m,vector<int>(n,0));
+        vector<vector<int>>pac(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
-            dfs(i,0,pac,heights);
-            dfs(i,n-1,atl,heights);
+            dfs(pac,i,0,heights);
+            dfs(atl,i,n-1,heights);
         }
         for(int j=0;j<n;j++){
-            dfs(0,j,pac,heights);
-            dfs(m-1,j,atl,heights);
+            dfs(pac,0,j,heights);
+            dfs(atl,m-1,j,heights);
         }
         vector<vector<int>>ans;
         for(int i=0;i<m;i++){
@@ -23,14 +22,14 @@ public:
         }
         return ans;
     }
-    void dfs(int i,int j,vector<vector<int>>&sea,vector<vector<int>>& heights){
+    void dfs(vector<vector<int>>& sea,int i,int j,vector<vector<int>>& heights){
         if(i<0||j<0||i>m-1||j>n-1||sea[i][j]==1) return;
         sea[i][j]=1;
         for(int d=0;d<4;d++){
             int r=i+dir[d];
             int c=j+dir[d+1];
             if(r<0||c<0||r>m-1||c>n-1) continue;
-            if(heights[r][c]>=heights[i][j]) dfs(r,c,sea,heights);
+            if(heights[r][c]>=heights[i][j]) dfs(sea,r,c,heights);
         }
     }
 };
