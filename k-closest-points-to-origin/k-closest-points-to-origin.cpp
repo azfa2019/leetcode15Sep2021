d4ff27edbe6272fe2bb07f4fd04ebd7c849ba6ed
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-25 17:15:29 -0800

    Time: 312 ms (18.15%), Space: 66.2 MB (25.98%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,22 +1,21 @@
 class Solution {
-  struct compare{
-    bool operator()(pair<int,int>&a,pair<int,int>&b){
-      return a.first*a.first+a.second*a.second<b.first*b.first+b.second*b.second;
+    struct compare{
+        bool operator()(vector<int>&p1,vector<int>&p2){
+            return p1[0]*p1[0]+p1[1]*p1[1] < p2[0]*p2[0]+p2[1]*p2[1];
+        }
+    };
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>,vector<vector<int>>,compare>pq;
+        for(auto e:points){
+            pq.push(e);
+            if(pq.size()>k) pq.pop();
+        }
+        vector<vector<int>>ans;
+        while(!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
     }
-  };
-  public:
-  vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-    priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
-    for(auto e:points){
-      pq.push({e[0],e[1]});
-      if(pq.size()>k) pq.pop();
-    }
-    vector<vector<int>>ans;
-    while(!pq.empty()) {
-      ans.push_back({pq.top().first,pq.top().second});
-      pq.pop();
-    }
-    return ans;
-
-  }
 };
