f02cce199f462c45a65e4cb8a5afb5efe4ec9438
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-27 13:31:16 -0800

    Time: 12 ms (93.05%), Space: 9.1 MB (60.92%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,37 @@
 class Solution {
-    public:
+public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
-        for(int i=0;i<n;i++) dp[i][i]=true;
-        int maxLen=0;
-        string ans;
-        for(int j=0;j<n;j++){
-            for(int i=0;i<=j;i++){
-                if(s[i]==s[j]){
-                    if(i+1==j) dp[i][j]=true;
-                    else if(i+1<j) dp[i][j]=dp[i+1][j-1];
-                }
-                if(dp[i][j]==true && j-i+1>maxLen){
-                    maxLen=j-i+1;
-                    ans=s.substr(i,maxLen);
-                }
+        string t="#";
+        for(char c:s){
+            t.push_back(c);
+            t.push_back('#');
+        }
+        int n=t.size();
+        int maxC=-1,maxR=-1;
+        vector<int>p(n,0);
+        int maxLen=0,center=0;
+        for(int i=0;i<n;i++){
+            int r;
+            if(maxR>=i){
+                int j=2*maxC-i;
+                r=min(p[j],maxR-i);
+                while(i-r>=0 && i+r<n && t[i-r]==t[i+r]) r++;
+            }else{
+                r=0;
+                while(i-r>=0 && i+r<n && t[i-r]==t[i+r]) r++;
+            }
+            p[i]=r-1;
+            if(maxR<i+p[i]){
+                maxR=i+p[i];
+                maxC=i;
+            }
+            if(maxLen<p[i]) {
+                maxLen=p[i];
+                center=i;
             }
         }
-        return ans;
+        return s.substr(center/2-maxLen/2,maxLen);
     }
 };
-// babad
-// i=n-1, dp[n][j]
-// i=n-2, j=n-2, j=n-1, dp[n-1][n-1]
-// i+1, j-1
-// i=0
-//i=n-1
+// [x [x x x] x x x [x x x] x] x x x x x x x x x x x x x x x x x 
+//       j      mC     i    mR
