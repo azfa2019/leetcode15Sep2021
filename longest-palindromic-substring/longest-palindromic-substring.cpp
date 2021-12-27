57cad5d072a9f1bd8fabc0df57f5337fb7304935
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-27 11:12:42 -0800

    Time: 436 ms (19.38%), Space: 47.6 MB (29.87%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -4,21 +4,26 @@ class Solution {
         int n=s.size();
         vector<vector<bool>>dp(n,vector<bool>(n,false));
         for(int i=0;i<n;i++) dp[i][i]=true;
-        int maxLen=1;
-        int start=0;
-        for(int len=2;len<=n;len++){
-            for(int i=0;i+len-1<n;i++){
-                int j=i+len-1;
+        int maxLen=0;
+        string ans;
+        for(int j=0;j<n;j++){
+            for(int i=0;i<=j;i++){
                 if(s[i]==s[j]){
                     if(i+1==j) dp[i][j]=true;
                     else if(i+1<j) dp[i][j]=dp[i+1][j-1];
                 }
-                if(dp[i][j]==true && j-i+1>maxLen) {
+                if(dp[i][j]==true && j-i+1>maxLen){
                     maxLen=j-i+1;
-                    start=i;
+                    ans=s.substr(i,maxLen);
                 }
             }
         }
-        return s.substr(start,maxLen);
+        return ans;
     }
 };
+// babad
+// i=n-1, dp[n][j]
+// i=n-2, j=n-2, j=n-1, dp[n-1][n-1]
+// i+1, j-1
+// i=0
+//i=n-1
