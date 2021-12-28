e3974fe5b44c10c87a8fbd11b2c1ed4f6c5c643d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-28 13:29:33 -0800

    Time: 0 ms (100.00%), Space: 7.1 MB (30.47%) - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -1,21 +1,17 @@
 class Solution {
-    public:
+public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        for(int l=0;l<n/2;l++){
-            int cs=l,ce=n-1-l;
-            int rs=l,re=n-1-l;
-            for(int i=1;i<=n-1-2*l;i++){
-                int tmp=matrix[rs][cs+i];
-                matrix[rs][cs+i]=matrix[re-i][cs];
-                matrix[re-i][cs]=matrix[re][ce-i];
-                matrix[re][ce-i]=matrix[rs+i][ce];
-                matrix[rs+i][ce]=tmp;
+        int start=0,end=n-1;
+        while(start<=end){
+            for(int j=start,i=end;j<=end-1,i>=start+1;j++,i--){
+                int tmp=matrix[start][j];
+                matrix[start][j]=matrix[i][start];
+                matrix[i][start]=matrix[end][i];
+                matrix[end][i]=matrix[j][end];
+                matrix[j][end]=tmp;
             }
+            start++,end--;
         }
     }
 };
-// x 15 13 2 5 x  l=1, 
-//1 4 8
-//6 5 3
-//7 2 9
