18c6ac936c99e3fe68517f7216b34e03217fab97
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-29 23:14:58 -0800

    Time: 0 ms (100.00%), Space: 6.6 MB (21.73%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long ans=1;
-        for(int i=1;i<n;i++){
-            ans=ans*(m-1+i)/i;
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++) dp[i][0]=1;
+        for(int i=0;i<n;i++) dp[0][i]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
         }
-        return ans;
+        return dp[m-1][n-1];
+        
     }
 };
-// m+n-2
-// m-1
-// (m+n-2)!/(m-1)!/(n-1)!
-// (m+n-2)!=(m-1+n-1)(m-2+n-1) ...(1+n-1){(n-1)(n-2)...1}
-// (m+n-2)! /(m-1)!(n-1)!
-// (m+n-2) (m+n-3) m (m-1)!/ (m-1)! (n-1)!
-// ---------n-1------
