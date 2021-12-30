82385d975d3e878d322fb504beff199aa4ff48d2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-30 15:49:57 -0800

    Time: 7 ms (43.33%), Space: 12.5 MB (61.76%) - LeetHub

---
+++ binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(!root) return {};
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            vector<int>level;
+            int levelSize=q.size();
+            while(levelSize--){
+                auto cur=q.front();q.pop();
+                level.push_back(cur->val);
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+            }
+            ans.push_back(level);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
