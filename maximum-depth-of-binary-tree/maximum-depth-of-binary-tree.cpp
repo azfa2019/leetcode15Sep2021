57cdd9dffae07b112e8b28ae9abfdb0ee4765867
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-30 14:16:01 -0800

    Time: 21 ms (5.14%), Space: 18.9 MB (13.98%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -10,17 +10,9 @@
  * };
  */
 class Solution {
-    int ans=0;
 public:
     int maxDepth(TreeNode* root) {
-        dfs(root);
-        return ans;
-    }
-    int dfs(TreeNode* root){
         if(!root) return 0;
-        int left=dfs(root->left);
-        int right=dfs(root->right);
-        ans=max(ans,max(left,right)+1);
-        return max(left,right)+1;
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
