d63b2515c66153fa46513f920c27e2786cbb0072
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-30 11:15:59 -0800

    Time: 51 ms (30.54%), Space: 32.7 MB (32.52%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -10,31 +10,31 @@
  * };
  */
 class Solution {
-  public:
-  TreeNode* deleteNode(TreeNode* root, int key) {
-    if(root==nullptr) return nullptr;
-    if(key>root->val) root->right=deleteNode(root->right,key);
-    else if(key<root->val) root->left=deleteNode(root->left,key);
-    else{
-      if(root->left==nullptr && root->right==nullptr) return nullptr;
-      else if(root->right!=nullptr) {
-        root->val=successor(root);
-        root->right=deleteNode(root->right,root->val);
-      }else if(root->left!=nullptr) {
-        root->val=predecessor(root);
-        root->left=deleteNode(root->left,root->val);
-      }
-    } 
-    return root;
-  }
-  int predecessor(TreeNode* root){
-    root=root->left;
-    while(root->right!=nullptr) root=root->right;
-    return root->val;
-  }
-  int successor(TreeNode* root){
-    root=root->right;
-    while(root->left!=nullptr) root=root->left;
-    return root->val;
-  }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==nullptr) return nullptr;
+        if(key<root->val) root->left=deleteNode(root->left,key);
+        else if(key>root->val) root->right=deleteNode(root->right,key);
+        else{
+            if(root->left==nullptr && root->right==nullptr) return nullptr;
+            else if(root->right!=nullptr) {
+                root->val=successor(root);
+                root->right=deleteNode(root->right,root->val);
+            }else if(root->left!=nullptr){
+                root->val=predecessor(root);
+                root->left=deleteNode(root->left,root->val);
+            }
+        }
+        return root;
+    }
+    int successor(TreeNode* node){
+        node=node->right;
+        while(node->left) node=node->left;
+        return node->val;
+    }
+    int predecessor(TreeNode* node){
+        node=node->left;
+        while(node->right) node=node->right;
+        return node->val;
+    }
 };
