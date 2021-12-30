567387ed9b507b6943504076d40dca38f4b7f480
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-30 00:32:00 -0800

    Time: 427 ms (44.76%), Space: 8 MB (37.61%) - LeetHub

---
+++ word-search/word-search.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    string word;
+    int m,n;
+    int dir[5]={1,0,-1,0,1};
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        this-> m=board.size(),this->n=board[0].size();
+        this->word=word;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0] && dfs(0,i,j,board)) return true;
+            }
+        }
+        return false;
+    }
+    bool dfs(int idx,int i,int j,vector<vector<char>>& board){
+        if(idx==word.size()) return true;
+        if(i<0 || j<0||i>m-1 || j>n-1 || board[i][j]=='#') return false;
+        if(board[i][j]!=word[idx]) return false;
+        char c=board[i][j];
+        board[i][j]='#';
+        for(int d=0;d<4;d++){
+            int r=i+dir[d];
+            int c=j+dir[d+1];
+            if(dfs(idx+1,r,c,board)) return true;
+        }
+        board[i][j]=c;
+        return false;
+    }
+};
