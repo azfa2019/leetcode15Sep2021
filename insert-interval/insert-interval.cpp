04189696221a3b9721b0222d5920547fe1c53bc6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-29 16:30:11 -0800

    Time: 16 ms (66.78%), Space: 17.1 MB (40.74%) - LeetHub

--- insert-interval/insert-interval.cpp
+++ insert-interval/insert-interval.cpp
@@ -1,43 +1,27 @@
 class Solution {
-    public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n=intervals.size();
-        vector<vector<int>>ans;
-        int i=0;
-        while(i<n && intervals[i][1]<newInterval[0]) {
-            ans.push_back(intervals[i]);  
-            i++;
-        } 
-        while(i<n && ((intervals[i][1]<=newInterval[1] && intervals[i][1]>=newInterval[0]) ||
-                      (intervals[i][0]<=newInterval[1] && intervals[i][0]>=newInterval[0]) ||
-                      (intervals[i][0]<=newInterval[1] && intervals[i][1]>=newInterval[1]) ))
-              {
-                  newInterval[0]=min(newInterval[0],intervals[i][0]);
-                  newInterval[1]=max(newInterval[1],intervals[i][1]);
-                  i++;
-              }
-              ans.push_back(newInterval);
-              for(;i<n;i++) ans.push_back(intervals[i]);
-              return ans;
-              }
-              };
-              //xxxx
-              // xxxx 
-              //   xxxxxxxx
-              // 
-              //xxxxxx
-              // xxx 
-              //     xxxxxxxx
-              //
-              // xxxx 
-              //   xxxxxxxx
-              //
-              //     xxxx 
-              //   xxxxxxxx
-              //
-              //         xxxx 
-              //   xxxxxxxx
-              // 
-              // 
-              // 
-              // 
+  public:
+  vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+      int n=intervals.size();
+      if(n==0) return {newInterval};
+      int i=0;
+      vector<vector<int>>ans;
+      while(i<n && intervals[i][1]<newInterval[0]) ans.push_back(intervals[i++]);
+      while(i<n&& intervals[i][1]>=newInterval[0] && intervals[i][0]<=newInterval[1] ) {
+          newInterval[0]=min(newInterval[0],intervals[i][0]);
+          newInterval[1]=max(newInterval[1],intervals[i][1]);
+          i++;
+      }
+      ans.push_back(newInterval);
+      while(i<n) ans.push_back(intervals[i++]);
+      return ans;
+  }
+};
+// 1,3       6,9
+// 1,3  4, 5  6,9
+// 1,  3, 4,5   6,9 10,15
+//    3          7
+
+// 1,13  6,9
+//  2   5
+// 1,2  3, 5  6, 7  8,  10
+//        4         8
