cb4bc360a594ebddb23a5d66181a4cd0f05f774b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-31 12:51:25 -0800

    Time: 24 ms (12.52%), Space: 30.2 MB (14.14%) - LeetHub

--- largest-bst-subtree/largest-bst-subtree.cpp
+++ largest-bst-subtree/largest-bst-subtree.cpp
@@ -10,22 +10,24 @@
  * };
  */
 class Solution {
-  int ans;
-  public:
-  int largestBSTSubtree(TreeNode* root) {
-    ans=0;
-    dfs(root);
-    return ans;
-  }
-  vector<int>dfs(TreeNode* root){
-    if(root==nullptr) return {INT_MAX,INT_MIN,0};
-    auto l=dfs(root->left);
-    auto r=dfs(root->right);
-    if(l[2]==-1 || r[2]==-1) return {INT_MIN,INT_MAX,-1};
-    if(l[1]<root->val && r[0]>root->val){
-      int size=l[2]+r[2]+1;
-      ans=max(ans,size);
-      return {l[2]==0?root->val:l[0],r[1]==INT_MIN?root->val:r[1],size};
-    }else return {INT_MIN,INT_MAX,-1};
-  }
+    int ans;
+public:
+    int largestBSTSubtree(TreeNode* root) {
+        ans=0;
+        dfs(root);
+        return ans;
+    }
+    vector<int>dfs(TreeNode* root){
+        if(!root) return {INT_MAX,INT_MIN,0};
+        auto l=dfs(root->left);
+        auto r=dfs(root->right);
+        if(l[2]==-1 || r[2]==-1) return {INT_MIN,INT_MAX,-1};
+        if(root->val>l[1] && root->val<r[0]){
+            int count=l[2]+r[2]+1;
+            ans=max(ans,count);
+            int low=(l[0]==INT_MAX?root->val:l[0]);
+            int high=(r[1]==INT_MIN?root->val:r[1]);
+            return {low,high,count};
+        }else return {INT_MIN,INT_MAX,-1};
+    }
 };
