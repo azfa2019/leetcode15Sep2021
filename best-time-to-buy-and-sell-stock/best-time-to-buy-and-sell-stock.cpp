d3f41a728ef182c260d40db3dd7dd74cc775c2c1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2021-12-31 13:22:59 -0800

    Time: 124 ms (39.35%), Space: 101.1 MB (6.11%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans=0,minP=prices[0];
-        for(int p:prices){
-            minP=min(p,minP);
-            ans=max(ans,p-minP);
+        int n=prices.size();
+        vector<int> hold(n,0),noHold(n,0);
+        hold[0]=-prices[0];
+        noHold[0]=0;
+        for(int i=1;i<n;i++){
+            hold[i]=max(hold[i-1],-prices[i]);
+            noHold[i]=max(hold[i-1]+prices[i],noHold[i-1]);
         }
-        return ans;
+        return noHold[n-1];
     }
 };
