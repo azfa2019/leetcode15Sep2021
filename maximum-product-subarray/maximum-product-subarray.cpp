14a61f8d9a4cdd23b829035d2a2c45e8e6e1e94d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-02 00:37:33 -0800

    Time: 17 ms (5.70%), Space: 13.8 MB (15.63%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.cpp
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int ans=nums[0];
         int n=nums.size();
-        vector<int>dpmin(n,0);
-        vector<int>dpmax(n,0);
-        dpmin[0]=nums[0];
-        dpmax[0]=nums[0];
-
-        for(int i=1;i<n;i++){
-            dpmax[i]=max(dpmin[i-1]*nums[i],max(nums[i],dpmax[i-1]*nums[i]));
-            dpmin[i]=min(dpmin[i-1]*nums[i],min(nums[i],dpmax[i-1]*nums[i]));
-            ans=max(ans,dpmax[i]);
+        int minP=1,maxP=1;
+        int minPTmp,maxPTmp;
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            minPTmp=minP,maxPTmp=maxP;
+            minP=min(min(minPTmp*nums[i],maxPTmp*nums[i]),nums[i]);
+            maxP=max(max(minPTmp*nums[i],maxPTmp*nums[i]),nums[i]);
+            ans=max(ans,maxP);
         }
         return ans;
     }
