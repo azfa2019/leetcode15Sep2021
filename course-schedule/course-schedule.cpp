31861834d5a8bab7690fc47d5047c80413c929d2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-03 00:36:22 -0800

    Time: 20 ms (75.59%), Space: 14.6 MB (32.16%) - LeetHub

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size()==0) return true;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
         vector<vector<int>>g(numCourses);
-        for(auto p:prerequisites) g[p[1]].push_back(p[0]);
+        for(auto p:pre) g[p[1]].push_back(p[0]);
         vector<int>visited(numCourses,0);
         for(int i=0;i<numCourses;i++)
-            if (dfs(i,visited,g)==false) return false;
+            if(dfs(i,g,visited)==false) return false;
         return true;
     }
-    bool dfs(int idx,vector<int>& visited,vector<vector<int>>&g){
+    bool dfs(int idx,vector<vector<int>>&g,vector<int>&visited){
         if(visited[idx]==1) return false;
         if(visited[idx]==-1) return true;
         visited[idx]=1;
-        for(auto next:g[idx]){
-            if (dfs(next,visited,g)==false) return false;
-        }
+        for(int next:g[idx])
+            if(dfs(next,g,visited)==false) return false;
         visited[idx]=-1;
         return true;
     }
