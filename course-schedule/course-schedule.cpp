dafa0118673aa09d2a1e6b1ca9f551548e67315e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-03 00:21:56 -0800

    Time: 35 ms (17.19%), Space: 14.5 MB (32.16%) - LeetHub

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,24 +1,22 @@
 class Solution {
-public:
+    public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        if(prerequisites.size()==0) return true;
         vector<vector<int>>g(numCourses);
-        vector<int>indegree(numCourses,0);
-        for(auto p:prerequisites){
-            g[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
+        for(auto p:prerequisites) g[p[1]].push_back(p[0]);
+        vector<int>visited(numCourses,0);
+        for(int i=0;i<numCourses;i++)
+            if (dfs(i,visited,g)==false) return false;
+        return true;
+    }
+    bool dfs(int idx,vector<int>& visited,vector<vector<int>>&g){
+        if(visited[idx]==1) return false;
+        if(visited[idx]==-1) return true;
+        visited[idx]=1;
+        for(auto next:g[idx]){
+            if (dfs(next,visited,g)==false) return false;
         }
-        queue<int>sources;
-        for(int i=0;i<numCourses;i++) 
-            if(indegree[i]==0) sources.push(i);
-        vector<int>ans;
-        while(!sources.empty()){
-            int cur=sources.front();sources.pop();
-            ans.push_back(cur);
-            for(auto next:g[cur]){
-                indegree[next]--;
-                if(indegree[next]==0) sources.push(next);
-            }
-        }
-        return ans.size()==numCourses;
+        visited[idx]=-1;
+        return true;
     }
 };
