c98f281ebb5dddce5c84c68cf2be98d1861bac40
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-04 21:03:21 -0800

    Time: 808 ms (5.17%), Space: 86.9 MB (25.86%) - LeetHub

--- 737-sentence-similarity-ii/737-sentence-similarity-ii.cpp
+++ 737-sentence-similarity-ii/737-sentence-similarity-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
     class UF{
         public:
-        vector<int>parent;
-        vector<int>rank;
-        UF(int n){
-            parent=vector<int>(n,0);
-            rank=vector<int>(n,0);
-            for(int i=0;i<n;i++) parent[i]=i;
+        unordered_map<string,string>parent;
+        unordered_map<string,int>rank;
+        UF(vector<string>words){
+            for(int i=0;i<words.size();i++){
+                parent[words[i]]=words[i];
+                rank[words[i]]=0;   
+            } 
         }
-        bool unionNodes(int n1,int n2){
-            int p1=findParent(n1);
-            int p2=findParent(n2);
+        bool unionNodes(string& s1,string& s2){
+            string p1=findParent(s1);
+            string p2=findParent(s2);
             if(p1==p2) return false;
             if(rank[p1]<rank[p2]) parent[p1]=p2;
             else if(rank[p1]>rank[p2]) parent[p2]=p1;
             else parent[p1]=p2,rank[p2]++;
             return true;
         }
-        int findParent(int n){
-            if(n!=parent[n]) parent[n]=findParent(parent[n]);
-            return parent[n];
+        string findParent(string& s){
+            if(s!=parent[s]) parent[s]=findParent(parent[s]);
+            return parent[s];
         }
     };
     public:
@@ -35,14 +36,12 @@ class Solution {
             words.insert(p[0]);
             words.insert(p[1]);
         }
-        unordered_map<string,int>mp;
-        int count=0;
-        for(auto s:words) mp[s]=count++;
-        UF uf=UF(words.size());
+        vector<string>all(words.begin(),words.end());
+        UF uf=UF(all);
         for(auto p:similarPairs)
-            uf.unionNodes(mp[p[0]],mp[p[1]]);
+            uf.unionNodes(p[0],p[1]);
         for(int i=0;i<n;i++){
-            if(uf.findParent(mp[sentence1[i]])!=uf.findParent(mp[sentence2[i]])) return false;
+            if(uf.findParent(sentence1[i])!=uf.findParent(sentence2[i])) return false;
         }
         return true;
     }
