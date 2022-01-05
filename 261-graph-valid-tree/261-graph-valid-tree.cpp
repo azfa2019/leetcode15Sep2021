27a7213de31bd1fa95d96bce2bbc4794b4a1e7fb
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-05 10:31:14 -0800

    Time: 69 ms (5.26%), Space: 17.3 MB (5.21%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        vector<bool>visited(n,false);
-        vector<vector<int>>g(n);
+        vector<unordered_set<int>>g(n);
         for(auto e:edges){
-            g[e[0]].push_back(e[1]);
-            g[e[1]].push_back(e[0]);
+            g[e[0]].insert(e[1]);
+            g[e[1]].insert(e[0]);
         }
-        if(!dfs(0,visited,g,-1)) return false;
-        for(auto e:visited) if(!e) return false;
-        return true;
-    }
-    bool dfs(int idx,vector<bool>&visited, vector<vector<int>>&g,int pre){
-        if(visited[idx]) return false;
-        visited[idx]=true;
-        for(int next:g[idx]){
-            if(pre!=next){
-                if(!dfs(next,visited,g,idx)) return false;
+        queue<int>q{{0}};
+        unordered_set<int>st{{0}};
+        while(q.size()){
+            int cur=q.front();q.pop();
+            for(int next:g[cur]){
+                if(st.find(next)!=st.end()) return false;
+                st.insert(next);
+                q.push(next);
+                g[next].erase(cur);
             }
         }
-        return true;
+        return st.size()==n;
     }
 };
