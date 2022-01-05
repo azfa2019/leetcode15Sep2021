09849b664e376a8b00c4666dae8e893e71360c0a
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-05 10:21:58 -0800

    Time: 24 ms (36.66%), Space: 14.8 MB (16.83%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,25 +1,24 @@
 class Solution {
-    public:
+public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        vector<int>visited(n,0);
+        vector<bool>visited(n,false);
         vector<vector<int>>g(n);
-        for(auto e:edges) {
+        for(auto e:edges){
             g[e[0]].push_back(e[1]);
             g[e[1]].push_back(e[0]);
         }
-        if(dfs(0,visited,g,-1)==false) return false;
-        for(int i=0;i<n;i++) if(visited[i]==0) return false;
+        if(!dfs(0,visited,g,-1)) return false;
+        for(auto e:visited) if(!e) return false;
         return true;
     }
-    bool dfs(int idx,vector<int>&visited, vector<vector<int>>&g,int pre){
-        if(visited[idx]==1) return false;
-        if(visited[idx]==-1) return true;
-        visited[idx]=1;
-        for(auto next:g[idx]){
-            if(next!=pre)
-                if(dfs(next,visited,g,idx)==false) return false;
+    bool dfs(int idx,vector<bool>&visited, vector<vector<int>>&g,int pre){
+        if(visited[idx]) return false;
+        visited[idx]=true;
+        for(int next:g[idx]){
+            if(pre!=next){
+                if(!dfs(next,visited,g,idx)) return false;
+            }
         }
-        visited[idx]=-1;
         return true;
     }
 };
