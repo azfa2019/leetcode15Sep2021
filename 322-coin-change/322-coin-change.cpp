f7bda7eec96d927bfd951d7972dc6f45637b4941
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-05 22:51:15 -0800

    Time: 158 ms (17.94%), Space: 14.3 MB (66.17%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,7 +3,6 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,INT_MAX/2);
         dp[0]=0;
-
         for(int i=0;i<=amount;i++){
             for(int coin:coins){
                 if(i-coin>=0)
