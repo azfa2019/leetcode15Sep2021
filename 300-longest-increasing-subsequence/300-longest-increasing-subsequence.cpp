8aee0aff2da26f16ee67d90525c3bb9b2e594de2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-05 21:01:42 -0800

    Time: 553 ms (5.32%), Space: 10.6 MB (13.12%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -7,8 +7,8 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]) dp[i]=max(dp[i],dp[j]+1);
-                ans=max(ans,dp[i]);
             }
+            ans=max(ans,dp[i]);
         }
         return ans;
     }
