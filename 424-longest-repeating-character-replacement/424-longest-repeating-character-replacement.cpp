8a89fcfb95aa0438df4523b149bc104f03528e58
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-06 21:27:01 -0800

    Time: 24 ms (20.97%), Space: 7.3 MB (8.95%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,17 +1,27 @@
 class Solution {
+    string s;
+    int k;
+    vector<int>count;
     public:
     int characterReplacement(string s, int k) {
-        int ans=0,n=s.size();
-        vector<int>count(26,0);
-        int left=0;
-        for(int right=0;right<n;right++){
-            count[s[right]-'A']++;
-            while(right-left+1-*max_element(count.begin(),count.end())>k){
-                count[s[left]-'A']--;
-                left++;
-            }
-            ans=max(ans,right-left+1);
+        int ans=0;
+        int j=0;
+        this->s=s,this->k=k;
+        count=vector<int>(26,0);
+        for(int i=0;i<s.size();i++){
+            while(j<s.size() && checkOk(j,j-i+1)) j++;
+            ans=max(ans,j-i);
+            count[s[i]-'A']--;
         }
         return ans;
+    }
+    bool checkOk(int j, int total){
+        count[s[j]-'A']++;
+        int maxCount=*max_element(count.begin(),count.end());
+        if(total-maxCount<=k) return true;
+        else{
+            count[s[j]-'A']--;
+            return false;
+        }
     }
 };
