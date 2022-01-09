a878a07ccd5f6f37da9e141bc482778a16774372
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-08 17:05:54 -0800

    Time: 372 ms (24.26%), Space: 30.1 MB (34.65%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,33 +1,26 @@
 class Solution {
-    string s;
-    int n;
     public:
     string longestPalindrome(string s) {
-        this->s=s;
-        this->n=s.size();
-        int maxLen=0,start=0,len;
-        for(int i=0;i<n;i++){
-            len=expand(i,i);
-            if(maxLen<len){
-                maxLen=len;
-                start=i-len/2;
-            }
-            // 1 2 3 4 5
-        }
-        for(int i=0;i<n-1;i++)
-            if(s[i]==s[i+1]){
-                len=expand(i,i+1);
-                if(maxLen<len){
-                    maxLen=len;
-                    start=i-len/2+1;
+        int n=s.size();
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        for(int i=0;i<n;i++) dp[i][i]=true;
+        int maxLen=0,start=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i]==s[j])
+                    dp[i][j]=(i+1<j-1?dp[i+1][j-1]:true);
+                if(j-i+1>maxLen && dp[i][j]){
+                    maxLen=j-i+1;
+                    start=i;
                 }
-                // 1 2 3 4 
             }
+        }
         return s.substr(start,maxLen);
     }
-    int expand(int i,int j){
-        while(i>=0 && j<n && s[i]==s[j]) 
-            i--,j++;
-        return j-1-(i+1)+1;
-    }
 };
+// a a
+// i i+1 dp[i+1][i]
+// 
+// 
+// 
+// 
