b17dd7d51a4e1ec86f40514d1b438ba418e3a650
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-09 20:22:11 -0800

    Time: 4 ms (61.32%), Space: 6.2 MB (84.88%) - LeetHub

---
+++ 67-add-binary/67-add-binary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+    string addBinary(string a, string b) {
+        string ans;
+        int carry=0;
+        for(int i=a.size()-1,j=b.size()-1;i>=0 || j>=0;i--,j--){
+            int sum=carry;
+            sum+=i>=0?a[i]-'0':0;
+            sum+=j>=0?b[j]-'0':0;
+            ans.push_back(sum%2+'0');
+            carry=sum/2;
+        }
+        if(carry==1) ans.push_back('1');
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
