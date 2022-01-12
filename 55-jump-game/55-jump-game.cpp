ad04054d2235e31235802677a7c644c447c95222
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-11 20:07:26 -0800

    Time: 144 ms (22.55%), Space: 48.4 MB (21.48%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int pos=n-1;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]+i>=pos) pos=i;
+        }
+        return pos==0;
+    }
+};
