66b295b63141ebe66c705c770ce37402d2c7cfeb
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-12 09:17:25 -0800

    Time: 78 ms (10.27%), Space: 19 MB (19.12%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,10 +3,13 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>>ans;
-        for(auto e:intervals){
-            if(ans.empty()|| e[0]>ans.back()[1]) ans.push_back(e);
-            else ans.back()[1]=max(ans.back()[1],e[1]);
+        vector<vector<int>>ans{intervals[0]};
+        for(int i=1;i<n;i++){
+            while(i<n && intervals[i][0]<=ans.back()[1]) {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                i++;
+            }
+           if(i<n) ans.push_back(intervals[i]);
         }
         return ans;
     }
