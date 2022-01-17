27d501e129e4fee99bfae6c03c278abbf4d6d0b3
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-17 15:39:37 -0800

    Time: 3 ms (51.15%), Space: 5.9 MB (47.10%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,9 +1,12 @@
 class Solution {
+    int lowbit(uint32_t n){
+        return n&(-n);
+    }
 public:
     int hammingWeight(uint32_t n) {
         uint32_t count=0;
         while(n){
-            n=n&(n-1);
+            n=n-lowbit(n);
             count++;
         }
         return count;
