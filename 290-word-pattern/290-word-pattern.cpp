b39471147cd725136fb1e36a33e8f7131986de07
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-16 18:15:54 -0800

    Time: 3 ms (20.00%), Space: 6.4 MB (50.50%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string>map;
+        unordered_map<string,char>rmap;
+        stringstream ss(s);
+        string cur;
+        for(char c:pattern){
+            if(!(ss>>cur)||
+              (map.find(c)!=map.end() && map[c]!=cur) ||
+              (rmap.find(cur)!=rmap.end() && rmap[cur]!=c) ) 
+                return false;
+            map[c]=cur;
+            rmap[cur]=c;
+        }
+        
+        return ss.eof()?true:false;
+    }
+};
