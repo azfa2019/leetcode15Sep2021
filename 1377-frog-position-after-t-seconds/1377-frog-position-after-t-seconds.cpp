b9949da3749b43e82d4a47f9ae868e555873aa2e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-17 13:09:37 -0800

    Time: 20 ms (86.04%), Space: 16.2 MB (28.17%) - LeetHub

--- 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.cpp
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
         vector<vector<int>>g(n+1);
-        for(auto e:edges){
-            g[e[0]].push_back(e[1]);
-            g[e[1]].push_back(e[0]);
-        }
-        vector<double>p(n+1);
-        p[1]=1.;
+        for(auto e:edges)
+            g[e[0]].push_back(e[1]),g[e[1]].push_back(e[0]);
         vector<int>seen(n+1);
         seen[1]=1;
+        vector<double>p(n+1);
+        p[1]=1.0;
         queue<int>q{{1}};
         while(t--){
             int size=q.size();
@@ -19,7 +17,7 @@ public:
                 for(int next:g[cur]) 
                     if(!seen[next]) count++;
                 for(int next:g[cur]){
-                    if(!seen[next]){
+                    if(!seen[next]){                                         
                         q.push(next);
                         p[next]=p[cur]/count;
                         seen[next]=1;
