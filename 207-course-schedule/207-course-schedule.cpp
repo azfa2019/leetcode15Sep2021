567ec24d78e076d95b24d1f5952d807cb6893e7b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-17 21:19:58 -0800

    Time: 24 ms (58.80%), Space: 14.5 MB (33.16%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,20 +1,27 @@
 class Solution {
+    vector<int>visited;
+    vector<vector<int>>g;
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        vector<vector<int>>g(numCourses);
-        for(auto p:pre) g[p[1]].push_back(p[0]);
-        vector<int>visited(numCourses,0);
-        for(int i=0;i<numCourses;i++)
-            if(!dfs(i,g,visited)) return false;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        visited=vector<int>(numCourses,0);
+        g=vector<vector<int>>(numCourses);
+        for(auto p:prerequisites){
+            g[p[1]].push_back(p[0]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(visited[i]==0) 
+                if(!dfs(i,visited)) return false;
+        }
         return true;
     }
-    bool dfs(int idx,vector<vector<int>>&g,vector<int>&visited){
-        if(visited[idx]==1) return false;
-        if(visited[idx]==-1) return true;
-        visited[idx]=1;
-        for(int next:g[idx])
-            if(dfs(next,g,visited)==false) return false;
-        visited[idx]=-1;
+    bool dfs(int cur,vector<int>&visited){
+        if(visited[cur]==1) return false;
+        if(visited[cur]==2) return true;
+        visited[cur]=1;
+        for(int next:g[cur]){
+            if(!dfs(next,visited)) return false;
+        }
+        visited[cur]=2;
         return true;
     }
 };
