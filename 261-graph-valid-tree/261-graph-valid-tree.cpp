def58a3f6457373df91fb9d4f840461cf342db01
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 15:44:53 -0800

    Time: 55 ms (6.00%), Space: 12.3 MB (53.80%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,22 +1,37 @@
 class Solution {
+    class UF{
+  public:
+  vector<int>parent;
+  vector<int>rank;
+  UF(int n){
+    parent=vector<int>(n,0);
+    rank=vector<int>(n,0);
+    for(int i=0;i<n;i++)
+      parent[i]=i;
+  }
+  int findParent(int i){
+    if(parent[i]!=i) parent[i]=findParent(parent[i]);
+    return parent[i];
+  }
+  bool unionNodes(int x,int y){
+    x=findParent(x);
+    y=findParent(y);
+    if(x==y) return false;
+    if(rank[x]<rank[y]) parent[x]=y;
+    else if(rank[x]>rank[y]) parent[y]=x;
+    else parent[y]=x,rank[x]++;
+    return true;
+  }
+};
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        vector<unordered_set<int>>g(n);
-        for(auto e:edges){
-            g[e[0]].insert(e[1]);
-            g[e[1]].insert(e[0]);
+        UF uf=UF(n);
+        for(auto item:edges){
+            if(uf.unionNodes(item[0],item[1])==false) return false;
         }
-        queue<int>q{{0}};
-        unordered_set<int>st{{0}};
-        while(q.size()){
-            int cur=q.front();q.pop();
-            for(int next:g[cur]){
-                if(st.find(next)!=st.end()) return false;
-                st.insert(next);
-                q.push(next);
-                g[next].erase(cur);
-            }
-        }
-        return st.size()==n;
+        unordered_set<int>st;
+        for(int i=0;i<n;i++) st.insert(uf.findParent(i));
+        return st.size()==1;
+        
     }
 };
