2cae12fdbe8222d4cfb75dba6bb9315926d2c8b1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 11:33:48 -0800

    Time: 30 ms (33.64%), Space: 12.6 MB (34.30%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        vector<pair<int,int>>all;
+        vector<int>start,end;
         for(auto item:intervals){
-            all.push_back({item[0],1});
-            all.push_back({item[1],-1});
+            start.push_back(item[0]);
+            end.push_back(item[1]);
         }
+        int i=0,j=0;
         int ans=0,count=0;
-        sort(all.begin(),all.end());
-        for(auto item:all){
-            count+=item.second;
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
+        while(i<start.size() && j<end.size()){
+            if(start[i]<end[j]) count++,i++;
+            else count--,j++;
             ans=max(ans,count);
         }
         return ans;
