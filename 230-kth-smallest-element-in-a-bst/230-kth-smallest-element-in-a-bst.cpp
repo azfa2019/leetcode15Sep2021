e0c4fb27c414e26ca89e264fc6c2476defd2f12e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-18 23:07:44 -0800

    Time: 33 ms (17.91%), Space: 24.3 MB (32.12%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -10,15 +10,19 @@
  * };
  */
 class Solution {
-public:
+    public:
     int kthSmallest(TreeNode* root, int k) {
-        int l=count(root->left);
-        if(k-1<l) return kthSmallest(root->left,k);
-        else if(l==k-1) return root->val;
-        else return kthSmallest(root->right,k-l-1);
-    }
-    int count(TreeNode* node){
-        if(!node) return 0;
-        return count(node->left)+count(node->right)+1;
+        stack<TreeNode*>stck;
+        while(true){
+            while(root){
+                stck.push(root);
+                root=root->left;
+            }
+            root=stck.top();stck.pop();
+            k--;
+            if(k==0) return root->val;
+            root=root->right;
+        }
+        return -1;
     }
 };
