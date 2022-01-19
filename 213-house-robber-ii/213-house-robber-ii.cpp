c60fd9190e5f68d0db3dd29b80e0f33f9f63c2d9
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-18 22:28:37 -0800

    Time: 0 ms (100.00%), Space: 7.9 MB (37.82%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -3,23 +3,16 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
         nums.insert(nums.begin(),0);
         vector<int>dp(n+1,0);
         dp[1]=nums[1];
-        for(int i=2;i<=n-1;i++){
+        for(int i=2;i<=n-1;i++)
             dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-        }
         int op1=dp[n-1];
-        //vector<int>dp2(n+1,0);
-        dp[1]=0;
-        dp[2]=nums[2];
-        for(int i=3;i<=n;i++){
+        dp[1]=0,dp[2]=nums[2];
+        for(int i=3;i<=n;i++)
             dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-        }
         int op2=dp[n];
         return max(op1,op2);
     }
 };
-// 1 ~ n-1
-// 2 ~ n
