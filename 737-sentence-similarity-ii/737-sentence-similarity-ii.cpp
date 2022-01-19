d824bb1ca3cbcbb3e2d2722808047d48ba7d6993
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 15:18:58 -0800

    Time: 450 ms (26.53%), Space: 93 MB (21.29%) - LeetHub

--- 737-sentence-similarity-ii/737-sentence-similarity-ii.cpp
+++ 737-sentence-similarity-ii/737-sentence-similarity-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
     class UF{
-  public:
-  vector<int>parent;
-  vector<int>rank;
-  UF(int n){
-    parent=vector<int>(n,0);
-    rank=vector<int>(n,0);
-    for(int i=0;i<n;i++)
-      parent[i]=i;
-  }
-  int findParent(int i){
-    if(parent[i]!=i) parent[i]=findParent(parent[i]);
-    return parent[i];
-  }
-  bool unionNodes(int x,int y){
-    x=findParent(x);
-    y=findParent(y);
-    if(x==y) return false;
-    if(rank[x]<rank[y]) parent[x]=y;
-    else if(rank[x]>rank[y]) parent[y]=x;
-    else parent[y]=x,rank[x]++;
-    return true;
-  }
-};
+        public:
+        unordered_map<string,string>parent;
+        unordered_map<string,int>rank;
+        UF(vector<string>words){
+            for(string w:words) {
+                rank[w]=0;
+                parent[w]=w;
+            }
+        }
+        string findParent(string s){
+            if(parent[s]!=s) parent[s]=findParent(parent[s]);
+            return parent[s];
+        }
+        bool unionNodes(string&x, string& y){
+            x=findParent(x);
+            y=findParent(y);
+            if(x==y) return false;
+            if(rank[x]<rank[y]) parent[x]=y;
+            else if(rank[x]>rank[y]) parent[y]=x;
+            else parent[y]=x,rank[x]++;
+            return true;
+        }
+    };
     public:
     bool areSentencesSimilarTwo(vector<string>& sentence1, vector<string>& sentence2, vector<vector<string>>& similarPairs) {
         unordered_set<string>st;
@@ -32,14 +32,12 @@ class Solution {
         for(auto s:sentence2) st.insert(s);
         for(auto item: similarPairs) st.insert(item[0]),st.insert(item[1]);
         vector<string>words(st.begin(),st.end());
-        unordered_map<string,int>mp;
-        for(int i=0;i<words.size();i++) mp[words[i]]=i;
-        UF uf=UF(words.size());
+        UF uf=UF(words);
         for(auto item:similarPairs){
-            uf.unionNodes(mp[item[0]],mp[item[1]]);
+            uf.unionNodes(item[0],item[1]);
         }
         for(int i=0;i<n;i++){
-            if(uf.findParent(mp[sentence1[i]])!=uf.findParent(mp[sentence2[i]])) return false;
+            if(uf.findParent(sentence1[i])!=uf.findParent(sentence2[i])) return false;
         }
         return true;
     }
