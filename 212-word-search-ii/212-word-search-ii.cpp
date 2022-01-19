7d15b0ecc43ba34f7984c781ff5827ecebc015ed
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-18 21:57:56 -0800

    Time: 696 ms (42.76%), Space: 12.4 MB (85.18%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -11,26 +11,25 @@ class Solution {
     vector<string>ans;
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        m=board.size(),n=board[0].size();
         this->root=new Trie();
+        m=board.size(),n=board[0].size();
         for(string w:words){
-            Trie*node=root;
+            auto node=root;
             for(char c:w){
                 if(!node->children[c-'a']) node->children[c-'a']=new Trie();
                 node=node->children[c-'a'];
             }
             node->word=w;
         }
-        m=board.size(),n=board[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dfs(i,j,root,board);
+                dfs(i,j,board,root);
             }
         }
         return ans;
     }
-    void dfs(int i,int j,Trie* node,vector<vector<char>>& board){
-        if(i<0 ||j<0||i>m-1||j>n-1||board[i][j]=='#') return;
+    void dfs(int i,int j,vector<vector<char>>&board,Trie* node){
+        if(i<0 || j<0 || i>m-1 || j>n-1 || board[i][j]=='#') return;
         char c=board[i][j];
         if(!node->children[c-'a']) return;
         node=node->children[c-'a'];
@@ -38,12 +37,11 @@ public:
             ans.push_back(node->word);
             node->word="";
         }
-        
         board[i][j]='#';
         for(int d=0;d<4;d++){
             int r=i+dir[d];
             int c=j+dir[d+1];
-            dfs(r,c,node,board);
+            dfs(r,c,board,node);
         }
         board[i][j]=c;
     }
