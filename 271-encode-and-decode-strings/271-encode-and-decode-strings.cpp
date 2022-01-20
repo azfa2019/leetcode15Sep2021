572ae06da3ad676919bf2eeb0c9d37402cc0e714
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 20:47:27 -0800

    Time: 100 ms (14.61%), Space: 82.5 MB (10.09%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.cpp
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.cpp
@@ -1,33 +1,30 @@
 class Codec {
-    public:
+public:
 
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
-        string ans="";
+        string ans;
         for(string s:strs){
-            ans+=to_string(s.size())+"#"+s;
+            ans+=to_string(s.size())+"!"+s;
         }
         return ans;
     }
-    //"Hello","World"
-    //5#Hello5#World
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
         vector<string>ans;
-        helper(s,ans);
+        while(s!=""){
+            int pos=s.find("!");
+            int len=stoi(s.substr(0,pos));
+            string cur=s.substr(pos+1,len);
+            ans.push_back(cur);
+            s=s.substr(pos+1+len);
+        }
         return ans;
     }
-    void helper(string& s,vector<string>& ans){
-        while(s!=""){
-            int i=s.find("#",0);
-            int len=stoi(s.substr(0,i));
-            string cur=s.substr(i+1,len);
-            ans.push_back(cur);
-            s=s.substr(i+1+len);
-        }
-    }
 };
+//  w  orld
+// p1     5
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
