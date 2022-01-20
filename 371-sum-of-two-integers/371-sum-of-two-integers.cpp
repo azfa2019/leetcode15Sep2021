b4e4d5a938ecd0f5482a6103a9142b5a9c9ce6ec
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 21:57:30 -0800

    Time: 0 ms (100.00%), Space: 5.9 MB (25.09%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        while(b!=0){
-            uint32_t atmp=a;
-            a=(atmp^b);
-            b=(atmp&b)<<1;
+        while(b){
+            uint32_t aTmp=a,bTmp=b;
+            a=(aTmp^bTmp);
+            b=(aTmp&bTmp)<<1;
         }
         return a;
     }
