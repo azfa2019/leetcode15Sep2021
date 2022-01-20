21d66a2591bf27171a3074846eb2a3ad49b78c9f
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-20 01:50:34 -0800

    Time: 36 ms (69.28%), Space: 25.3 MB (91.17%) - LeetHub

--- 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.cpp
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.cpp
@@ -13,21 +13,51 @@ public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         if(!root) return "#";
-        return to_string(root->val)+" "+serialize(root->left)+" "+serialize(root->right);
+        queue<TreeNode*>q;
+        q.push(root);
+        string ans;
+        while(!q.empty()){
+            auto cur=q.front();q.pop();
+            if(!cur){
+                ans+="#";
+            }else{
+                ans+=to_string(cur->val)+"!";
+                q.push(cur->left);
+                q.push(cur->right);
+            }
+        }
+        return ans;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        stringstream ss(data);
-        return helper(ss);
-    }
-    TreeNode* helper(stringstream& ss){
-        string tmp;
-        ss>>tmp;
-        if(tmp=="#") return nullptr;
+        if(data=="#") return nullptr;
+        queue<TreeNode*>q;
+        int i=0,j=0;
+        j=data.find("!",i);
+        auto tmp=data.substr(i,j-i);
         TreeNode* node=new TreeNode(stoi(tmp));
-        node->left=helper(ss);
-        node->right=helper(ss);
+        q.push(node);
+        while(q.size()){
+            auto node=q.front();q.pop();
+            if(!node) continue;
+            i=++j;
+            if(i>=data.size()) break;
+            if(data[i]=='#') node->left=nullptr;
+            else{
+                j=data.find("!",i);
+                node->left=new TreeNode(stoi(data.substr(i,j-i)));
+            }
+            q.push(node->left);
+            i=++j;
+            if(i>=data.size()) break;
+            if(data[i]=='#') node->right=nullptr;
+            else{
+                j=data.find("!",i);
+                node->right=new TreeNode(stoi(data.substr(i,j-i)));
+            }
+            q.push(node->right);
+        }
         return node;
     }
 };
