3b4b0d72f0fe5dbf0aa05e67863bee078953dbad
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-20 10:36:09 -0800

    Time: 10 ms (58.59%), Space: 6.6 MB (65.97%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        string tmp=s;
-        s="#";
-        for(char c:tmp){
-            s.push_back(c);
-            s.push_back('#');
+        string t="#";
+        for(char c:s){
+            t.push_back(c);
+            t.push_back('#');
         }
+        int n=t.size();
+        vector<int>p(n);
         int maxC=0,maxR=0;
-        int n=s.size();
-        vector<int>p(n,0);
-        int count=0;
         for(int i=0;i<n;i++){
             int r;
             if(maxR>=i){
                 int j=2*maxC-i;
-                r=min(p[j],maxR-i);
-                while(i-r>=0 && i+r<n && s[i-r]==s[i+r]) r++;
-            }else{
+                r=min(maxR-i,p[j]);
+                while(i-r>=0 && i+r<n && t[i-r]==t[i+r]) r++;
+            }else {
                 r=0;
-                while(i-r>=0 && i+r<n && s[i-r]==s[i+r]) r++;
+                while(i-r>=0 && i+r<n && t[i-r]==t[i+r]) r++;
             }
             p[i]=r-1;
-            if(maxR<i+p[i]){
-                maxR=p[i];
+            if(i+p[i]>maxR){
+                maxR=i+p[i];
                 maxC=i;
             }
         }
-        for(auto e:p)
-            count+=(e+1)/2;
-        return count;
+        int ans=0;
+        for(int num:p)
+            ans+=(num+1)/2;
+        return ans;
     }
 };
-// [x x x x x x x x x x x x x] x x x x x 
-//      j       mC      i   mR 
+// [x [x x x] x x x [x x x] x] x x x x x x x x x x x x x 
+//       j.     mC     i.   mR
 // 
 // 
 // 
