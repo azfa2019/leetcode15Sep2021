1294096276ab00734c7e8dfb8d79666a4834d052
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 21:40:32 -0800

    Time: 44 ms (17.09%), Space: 14.8 MB (18.22%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -3,22 +3,24 @@ public:
     int countComponents(int n, vector<vector<int>>& edges) {
         vector<int>visited(n,0);
         vector<vector<int>>g(n);
-        for(auto e:edges) {
-            g[e[0]].push_back(e[1]);
-            g[e[1]].push_back(e[0]);
+        for(auto item:edges){
+            g[item[0]].push_back(item[1]);
+            g[item[1]].push_back(item[0]);
         }
-        int count=0;
+        int ans=0;
         for(int i=0;i<n;i++){
             if(visited[i]==0){
-                count++;
-                dfs(i,g,visited);
+                ans++;
+                dfs(i,visited,n,g);
             }
         }
-        return count;
+        return ans;
     }
-    void dfs(int idx,vector<vector<int>>&g,vector<int>&visited){
-        if(visited[idx]==1) return;
-        visited[idx]=1;
-        for(int next:g[idx]) dfs(next,g,visited);
+    void dfs(int cur,vector<int>&visited,int n,vector<vector<int>>&g){
+        if(visited[cur]==1) return;
+        visited[cur]=1;
+        for(int next:g[cur]){
+            dfs(next,visited,n,g);
+        }
     }
 };
