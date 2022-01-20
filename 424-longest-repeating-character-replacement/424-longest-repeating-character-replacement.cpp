8e0e6a6487475434a1e48fb5b09c7d1c7034bc72
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-20 00:52:13 -0800

    Time: 12 ms (64.15%), Space: 7 MB (84.20%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -3,8 +3,8 @@ public:
     int characterReplacement(string s, int k) {
         int n=s.size();
         int right=0;
-        vector<int>mp(26);
         int ans=0;
+        vector<int>mp(26);
         for(int left=0;left<n;left++){
             while(right<n && checkOk(right,left,k,mp,s)) right++;
             ans=max(ans,right-left);
@@ -13,11 +13,11 @@ public:
         }
         return ans;
     }
-    bool checkOk(int right,int left,int k,vector<int>&mp,string& s){
+    bool checkOk(int right,int left,int k,vector<int>&mp,string &s){
         char r=s[right];
         mp[r-'A']++;
         int rep=*max_element(mp.begin(),mp.end());
-        if(rep+k>=right-left+1) return true;
+        if(right-left+1<=rep+k) return true;
         mp[r-'A']--;
         return false;
     }
