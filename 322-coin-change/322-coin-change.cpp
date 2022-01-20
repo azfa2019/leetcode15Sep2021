4e63522461648f430a936fe91ed4767b8cb317a6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-19 21:31:33 -0800

    Time: 163 ms (22.81%), Space: 16 MB (24.61%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<int>dp(amount+1,INT_MAX/2);
-        dp[0]=0;
-        for(int i=0;i<=amount;i++){
-            for(int c:coins){
-                if(i-c>=0) dp[i]=min(dp[i],dp[i-c]+1);
-            }
+        vector<int>mem(amount+1,0);
+        dfs(amount,coins,mem);
+        return mem[amount];
+    }
+    int dfs(int amount,vector<int>&coins,vector<int>&mem){
+        if(amount==0) return 0;
+        if(amount<0) return -1;
+        if(mem[amount]!=0) return mem[amount];
+        int ans=INT_MAX;
+        for(int c:coins){
+            int tmp=dfs(amount-c,coins,mem);
+            if(tmp>=0) ans=min(ans,tmp+1);
         }
-        return dp[amount]==INT_MAX/2?-1:dp[amount];
+        mem[amount]=(ans==INT_MAX?-1:ans);
+        return mem[amount];
     }
 };
