4f6e5ac662c9f2bfeedff4f04b77a02172b9581b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-24 20:22:31 -0800

    Time: 4 ms (60.62%), Space: 7.3 MB (43.45%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool check(vector<int>&nums,int mid,int m){
+    int splitArray(vector<int>& nums, int m) {
+        int l=0,r=INT_MAX;
+        while(l<r){
+            int mid=(long long) l+r>>1;
+            if(check(nums,m,mid)) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+    bool check(vector<int>& nums, int m,int mid){
         int sum=0,cnt=0;
         for(int num:nums){
             if(num>mid) return false;
-            if(sum+num>mid) {
+            if(sum+num>mid){
                 sum=num;
                 cnt++;
             }else sum+=num;
         }
         if(sum) cnt++;
         return cnt<=m;
-    }
-    int splitArray(vector<int>& nums, int m) {
-        int l=0,r=INT_MAX;
-        while(l<r){
-            int mid=(long long)l+r>>1;
-            if(check(nums,mid,m)) r=mid;
-            else l=mid+1;
-        }
-        return l;
     }
 };
