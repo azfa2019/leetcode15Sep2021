d4a1c900c011bfdc4ffdae244089935eeeeac708
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-25 11:31:42 -0800

    Time: 115 ms (39.80%), Space: 42.9 MB (11.14%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,12 +1,11 @@
 class Solution {
-        unordered_map<int,int>hash;
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>s(n+1);
-        for(int i=1;i<=n;i++) s[i]=s[i-1]+nums[i-1];
-        hash[0]=1;
         int res=0;
+        for(int i=1;i<=n;i++) s[i]=s[i-1]+nums[i-1];
+        unordered_map<int,int>hash{{0,1}};
         for(int i=1;i<=n;i++){
             res+=hash[s[i]-k];
             hash[s[i]]++;
