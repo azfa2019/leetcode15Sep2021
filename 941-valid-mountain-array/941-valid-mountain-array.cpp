c01e0d56321e83b34df7a258ac9588da8044fc59
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-01-24 16:16:12 -0800

    Time: 65 ms (6.50%), Space: 22.5 MB (48.39%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        if(n<3) return false;
+        int l=0,r=n-1;
+        while(l+1<n && arr[l]<arr[l+1]) l++;
+        while(r-1>=0 && arr[r]<arr[r-1]) r--;
+        if(r==n-1 || l==0) return false;
+        return r==l;
+    }
+};
