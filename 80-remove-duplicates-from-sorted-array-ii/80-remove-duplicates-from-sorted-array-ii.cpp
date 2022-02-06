96a89e6b34eebf13f76d18f365a3b1d1c09ec274
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-05 19:06:08 -0800

    Time: 14 ms (15.12%), Space: 10.7 MB (93.81%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -5,7 +5,7 @@ public:
         if(n<=2) return n;
         int k=2;
         for(int i=2;i<n;i++){
-            if(nums[i]!=nums[k-1] || nums[i]!=nums[k-2]) nums[k++]=nums[i];
+            if(nums[i]!=nums[k-2]) nums[k++]=nums[i];
         }
         return k;
     }
