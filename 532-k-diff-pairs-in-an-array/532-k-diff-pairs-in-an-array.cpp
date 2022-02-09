07785ed80660a888a6cb464693cf8f179434c6dd
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-08 17:58:32 -0800

    Time: 20 ms (73.03%), Space: 13.2 MB (58.82%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -7,8 +7,7 @@ class Solution {
         if(k==0){
             for(auto item:cnt) 
                 if(item.second>=2) res++;
-        }
-        else{
+        } else{
             for(auto item:cnt)
                 if(cnt.count(item.first-k)) res++;
         }
