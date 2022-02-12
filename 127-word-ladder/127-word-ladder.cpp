c89600a848cf5d9764710d855a580bf678501426
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-11 21:09:45 -0800

    Time: 143 ms (68.45%), Space: 16 MB (37.59%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<string>q;
+        q.push(beginWord);
+        unordered_set<string>words;
+        for(auto s:wordList) words.insert(s);
+        unordered_map<string,int>dist;
+        dist[beginWord]=1;
+        while(q.size()){
+            auto cur=q.front();q.pop();
+            auto tmp=cur;
+            for(int i=0;i<tmp.size();i++){
+                cur=tmp;
+                for(int c='a';c<='z';c++){
+                    cur[i]=c;
+                    if(words.count(cur) && !dist.count(cur)){
+                        dist[cur]=dist[tmp]+1;
+                        if(cur==endWord) return dist[cur];
+                        q.push(cur);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
