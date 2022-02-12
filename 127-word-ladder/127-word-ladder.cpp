316d9ae0b62a84d1e6ac7496b0decb2fab1f3c59
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-11 20:58:04 -0800

    Time: 159 ms (64.35%), Space: 15.9 MB (38.66%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -5,22 +5,21 @@ class Solution {
         q.push(beginWord);
         unordered_set<string>words;
         for(auto s:wordList) words.insert(s);
-        unordered_map<string,int>hash;
-        hash[beginWord]=1;
+        unordered_map<string,int>dist;
+        dist[beginWord]=1;
         while(q.size()){
             auto cur=q.front();q.pop();
-            string tmp=cur;
+            auto tmp=cur;
             for(int i=0;i<tmp.size();i++){
                 cur=tmp;
-                for(char c='a';c<='z';c++){
-                    if(cur[i]!=c){
+                for(int c='a';c<='z';c++){
+                    if(c!=tmp[i]) {
                         cur[i]=c;
-                        if(words.count(cur) && !hash.count(cur)){
-                            hash[cur]=hash[tmp]+1;
-                            if(cur==endWord) return hash[cur];
+                        if(words.count(cur) && !dist.count(cur)) {
+                            dist[cur]=dist[tmp]+1;
+                            if(cur==endWord) return dist[cur];
                             q.push(cur);
                         }
-
                     }
                 }
             }
