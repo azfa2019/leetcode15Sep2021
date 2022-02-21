d60fef7f76245de091068aba83821750a226ab85
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-20 23:25:24 -0800

    Time: 39 ms (19.43%), Space: 19.7 MB (9.42%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>hash;
+        for(int num:nums) {
+            hash[num]++;
+            if(hash[num]>n/2) return num;
+        }
+        return -1;
+    }
+};
