fa8806d8a753036a7a41b3b9e858d7607a519029
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-23 18:46:16 -0800

    Time: 231 ms (11.18%), Space: 32.5 MB (43.71%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        auto dummy=new ListNode();
+        dummy->next=head;
+        int n=0;
+        for(auto p=head;p;p=p->next) n++;
+        for(int size=1;size<n;size*=2){
+            auto cur=dummy->next;
+            auto tail=dummy;
+            while(cur){
+                auto left=cur;
+                auto right=cut(left,size);
+                cur=cut(right,size);
+                tail->next=merge(left,right);
+                while(tail->next) tail=tail->next;
+            }
+        }
+        return dummy->next;
+    }
+    ListNode* cut(ListNode* head,int n){
+        auto p=head;
+        while(--n && p) p=p->next;
+        if(!p) return nullptr;
+        auto next=p->next;
+        p->next=nullptr;
+        return next;
+    }
+    ListNode* merge(ListNode* l1,ListNode* l2){
+        auto dummy=new ListNode();
+        auto p=dummy;
+        while(l1 && l2){
+            if(l1->val<l2->val) p=p->next=l1,l1=l1->next;
+            else p=p->next=l2,l2=l2->next;
+        }    
+        p->next=l1?l1:l2;
+        return dummy->next;
+    }
+};
