63a17e1f1591b91ff72abdadf04a8728df5dd5d6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-02-25 16:55:05 -0800

    Time: 32 ms (79.54%), Space: 14.4 MB (63.04%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,29 @@
+typedef pair<int,int> PII;
+#define x first
+#define y second
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<vector<int>>f(1<<n,vector<int>(n,1e8));
+        queue<PII>q;
+        for(int i=0;i<n;i++){
+            f[1<<i][i]=0;
+            q.push({1<<i,i});
+        }
+        while(q.size()){
+            auto t=q.front();q.pop();
+            for(int node:graph[t.y]){
+                int s=t.x|1<<node;
+                if(f[s][node]>f[t.x][t.y]+1){
+                    f[s][node]=f[t.x][t.y]+1;
+                    q.push({s,node});
+                }
+            }
+        }
+        int res=1e8;
+        for(int i=0;i<n;i++)
+            res=min(res,f[(1<<n)-1][i]);
+        return res;
+    }
+};
