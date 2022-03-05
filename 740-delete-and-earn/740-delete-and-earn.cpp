2ddd65f0b791aee7a3e6d0990daf7991f9b69a25
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-04 20:05:45 -0800

    Time: 11 ms (58.71%), Space: 9.2 MB (83.08%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -7,12 +7,12 @@ class Solution {
         memset(cnt,0,sizeof cnt);
         for(int num:nums) cnt[num]++;
         int res=0;
-        int maxNum=*max_element(nums.begin(),nums.end());
-        for(int i=1;i<=maxNum;i++){
+        //int maxNum=*max_element(nums.begin(),nums.end());
+        for(int i=1;i<N;i++){
             f[i][1]=f[i-1][0]+i*cnt[i];
             f[i][0]=max(f[i-1][0],f[i-1][1]);
-            res=max(f[i][0],f[i][1]);
+            //res=max(f[i][0],f[i][1]);
         }
-        return res;
+        return max(f[N-1][0],f[N-1][1]);
     }
 };
