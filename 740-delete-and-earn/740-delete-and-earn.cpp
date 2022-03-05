af52c92d7a1c0459653fee7428b44f8884ac3174
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-04 19:54:43 -0800

    Time: 7 ms (82.55%), Space: 9.2 MB (90.77%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,17 @@
+const int N=10010;
+int f[N][2],cnt[N];
+class Solution {
+    public:
+    int deleteAndEarn(vector<int>& nums) {
+        memset(f,0,sizeof f);
+        memset(cnt,0,sizeof cnt);
+        for(int num:nums) cnt[num]++;
+        int res=0;
+        for(int i=1;i<N;i++){
+            f[i][1]=f[i-1][0]+i*cnt[i];
+            f[i][0]=max(f[i-1][0],f[i-1][1]);
+            res=max(f[i][0],f[i][1]);
+        }
+        return res;
+    }
+};
