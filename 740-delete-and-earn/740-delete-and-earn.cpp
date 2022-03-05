66f9c6c95c1e1ec398417514437a17338a8abe6b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-04 19:52:53 -0800

    Time: 11 ms (58.71%), Space: 9.2 MB (90.77%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -3,14 +3,13 @@ int f[N][2],cnt[N];
 class Solution {
     public:
     int deleteAndEarn(vector<int>& nums) {
-        int n=nums.size();
         memset(f,0,sizeof f);
         memset(cnt,0,sizeof cnt);
-        int res=0;
         for(int num:nums) cnt[num]++;
+        int res=0;
         for(int i=1;i<N;i++){
-            f[i][0]=max(f[i-1][1],f[i-1][0]);
             f[i][1]=f[i-1][0]+i*cnt[i];
+            f[i][0]=max(f[i-1][0],f[i-1][1]);
             res=max(res,max(f[i][0],f[i][1]));
         }
         return res;
