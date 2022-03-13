05abbcbe5198457d1985087323f964f0d1f40621
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-12 16:14:19 -0800

    Time: 2 ms (42.21%), Space: 6.3 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,17 +1,17 @@
 class Solution {
-public:
+    public:
     bool isValid(string s) {
         stack<char>stk;
         for(auto c:s){
             if(c=='('||c=='['||c=='{') stk.push(c);
-            else if(c==')') {
-                if(stk.empty() || stk.top()!='(') return false;
-                stk.pop();
-            }else if(c==']'){
-                if(stk.empty() || stk.top()!='[') return false;
-                stk.pop();
-            }else if(c=='}'){
-                if(stk.empty() || stk.top()!='{') return false;
+            else {
+                if(c==')') {
+                    if(stk.empty() || stk.top()!='(') return false;
+                }else if(c==']'){
+                    if(stk.empty() || stk.top()!='[') return false;
+                }else if(c=='}'){
+                    if(stk.empty() || stk.top()!='{') return false;
+                }
                 stk.pop();
             }
         }
