6d2baacb38151505fb39484dc744a38392fe94fa
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-13 18:26:43 -0700

    Time: 0 ms (100.00%), Space: 9.2 MB (64.27%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string res,name;
+        string res,cur;
         if(path.back()!='/') path=path+'/';
         for(auto c:path){
-            if(c!='/') name+=c;
+            if(c!='/') cur+=c;
             else{
-                if(name==".."){
+                if(cur==".."){
                     while(res.size() && res.back()!='/') res.pop_back();
                     if(res.size()) res.pop_back();
-                }else if(name!="." && name!="") res+='/'+name;
-                name.clear();
+                }else if(cur!="." && cur!="") res+='/'+cur;
+                cur.clear();
             }
         }
         if(res.empty()) res="/";
