384525e5149ae277b8ad8980327dbfebd5848c9f
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-14 18:12:00 -0700

    Time: 43 ms (28.39%), Space: 11.9 MB (37.59%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         stack<int>st;
-        string res;
         int n=s.size();
         vector<bool>invalid(n,0);
         for(int i=0;i<n;i++){
-            if(s[i]=='(') invalid[i]=1,st.push(i);
-            else if(s[i]==')'){
+            if(s[i]=='(') st.push(i),invalid[i]=1;
+            if(s[i]==')'){
                 if(st.empty()) invalid[i]=1;
-                else {
-                    invalid[st.top()]=false;
-                    st.pop();
-                }
+                else invalid[st.top()]=0, st.pop();
             }
         }
+        string res;
         for(int i=0;i<n;i++){
             if(invalid[i]) continue;
-            res+=s[i];
+            else res+=s[i];
         }
         return res;
     }
