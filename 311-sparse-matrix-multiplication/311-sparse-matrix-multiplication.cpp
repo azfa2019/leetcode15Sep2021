bae2d2402749a53e15120b1d0c8ee5102f53e3c6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-21 18:27:06 -0700

    Time: 14 ms (31.54%), Space: 8 MB (100.00%) - LeetHub

---
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+        vector<vector<int>>res(mat1.size(),vector<int>(mat2[0].size()));
+        for(int i=0;i<mat1.size();i++){
+            for(int k=0;k<mat1[0].size();k++){
+                if(mat1[i][k]!=0){
+                    for(int j=0;j<mat2[0].size();j++){
+                        if(mat2[k][j]!=0) res[i][j]+=mat1[i][k]*mat2[k][j];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
