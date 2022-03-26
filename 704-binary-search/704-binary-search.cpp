0a2cffffe381bc344720df3158298e51b7ace334
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-25 22:36:37 -0700

    Time: 69 ms (18.73%), Space: 27.7 MB (13.43%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,9 +3,9 @@ public:
     int search(vector<int>& nums, int target) {
         int l=0,r=nums.size()-1;
         while(l<r){
-            int m=l+r+1>>1;
-            if(nums[m]>target) r=m-1;
-            else l=m;
+            int m=l+r>>1;
+            if(nums[m]<target) l=m+1;
+            else r=m;
         }
         return nums[l]==target?l:-1;
     }
