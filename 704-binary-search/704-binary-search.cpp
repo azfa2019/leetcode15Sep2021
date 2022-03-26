bb790e578a736dbc09198069b48594098855480c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-25 18:21:23 -0700

    Time: 73 ms (14.18%), Space: 27.6 MB (63.85%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            int m=l+r+1>>1;
+            if(nums[m]>target) r=m-1;
+            else l=m;
+        }
+        return target==nums[l]?l:-1;
+    }
+};
