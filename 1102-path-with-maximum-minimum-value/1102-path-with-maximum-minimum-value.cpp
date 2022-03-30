432e272eb37bb04dea6b7a6a1e97c931feba3606
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-03-29 19:52:10 -0700

    Time: 300 ms (83.22%), Space: 31 MB (81.50%) - LeetHub

---
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    struct Unit{
+        int val;
+        int row;
+        int col;
+        bool operator< (const Unit &rhs) const{
+            return val<rhs.val;
+        }
+    };
+    int dir[5]{1,0,-1,0,1};
+public:
+    int maximumMinimumPath(vector<vector<int>>& grid) {
+        priority_queue<Unit>pq;
+        int m=grid.size(),n=grid[0].size();
+        int res=min(grid[0][0],grid[m-1][n-1]);
+        vector<vector<bool>>st(m,vector<bool>(n,false));
+        int i=0,j=0;
+        st[0][0]=true;
+        while(i!=m-1 || j!=n-1){
+            st[i][j]=true;
+            for(int k=0;k<4;k++){
+                int r=i+dir[k];
+                int c=j+dir[k+1];
+                if(r==m-1 && c==n-1) return res;
+                if(r>=0 && r<m && c>=0 && c<n && st[r][c]==false){
+                    st[r][c]=true;
+                    pq.push({grid[r][c],r,c});
+                }
+            }
+            auto next=pq.top();pq.pop();
+            res=min(res,next.val);
+            i=next.row;
+            j=next.col;
+        }
+        return res;
+    }
+};
