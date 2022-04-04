d0c0aa9190b21a5997f13f0fa98e18ab0ad716e9
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-03 18:16:11 -0700

    Time: 576 ms (97.16%), Space: 180.2 MB (15.97%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,25 +11,15 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-       ListNode * fast = head;
-        while (k-1 != 0)    //快指针先走k-1步，到达第k个点
-        {
-            fast = fast -> next;
-            k --;
-        }
-        ListNode* L = fast;
-        ListNode* slow = head;  // 1 2 3 4 5 6 7 k=3  L=3
-        while (fast -> next)    // slow=5 fast=7
-        {
-            fast = fast->next;
-            slow = slow->next;
-        }
-        ListNode* R = slow;     // R=5
-
-        int tmp = R->val;
-        R->val = L->val;
-        L->val = tmp;
+        if(!head) return head;
+        ListNode* h1=head;
+        for(int i=0;i<k-1;i++) h1=h1->next;
+        ListNode* h2=head;
+        ListNode* h=h1;
+        while(h->next) h=h->next,h2=h2->next;
+        int tmp=h1->val;
+        h1->val=h2->val;
+        h2->val=tmp;
         return head;
-
     }
 };
