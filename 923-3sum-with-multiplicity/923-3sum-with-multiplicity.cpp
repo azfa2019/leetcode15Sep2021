bc72c055a88650b3555d2a937dc0f113ecb55eb1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-05 17:34:07 -0700

    Time: 3 ms (99.66%), Space: 10.4 MB (74.83%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,27 @@
+typedef long long LL;
+const int mod=1e9+7;
+class Solution {
+    int cnt[310];
+public:
+    LL C(int a,int b){
+        LL res=1;
+        for(int i=a,j=1;j<=b;i--,j++){
+            res=res*i/j;
+        }
+        return res%mod;
+    }
+    int calc(int a,int b,int c){
+        if(a==b && b==c) return C(cnt[a],3);
+        if(a==b) return C(cnt[a],2)*cnt[c]%mod;
+        if(b==c) return C(cnt[b],2)*cnt[a]%mod;
+        return (LL) cnt[a]*cnt[b]*cnt[c]%mod;
+    }
+    int threeSumMulti(vector<int>& arr, int target) {
+        int res=0;
+        for(auto x:arr) cnt[x]++;
+        for(int i=0;i<=target;i++)
+            for(int j=i;j<=target-i-j;j++)
+                    res=(res+calc(i,j,target-i-j))%mod;
+    return res;
+    }
+};
