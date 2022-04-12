ab145a4b9f10b2dcac702efe7b5d4d5c8c600f3d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-11 18:08:58 -0700

    Time: 3 ms (45.24%), Space: 7 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        if(board.size()==0 || board[0].size()==0) return;
-        //if (board.empty() || board[0].empty()) return;
-        
-        int n=board.size(),m=board[0].size();
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++){
+        if(!board.size() || !board[0].size()) return;
+        int m=board.size(),n=board[0].size();
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++){
                 int live=0;
-                for(int x=max(0,i-1);x<=min(i+1,n-1);x++)
-                    for(int y=max(0,j-1);y<=min(j+1,m-1);y++){
-                        if(x==i && y==j) continue;
-                        if(board[x][y]&1) live++;
+                for(int r=max(0,i-1);r<=min(m-1,i+1);r++)
+                    for(int c=max(0,j-1);c<=min(n-1,j+1);c++){
+                        if(r==i && c==j) continue;
+                        if(board[r][c]&1) live++;
                     }
                 int cur=board[i][j]&1,next;
                 if(cur){
-                    if(live<2 || live>3) next=0;
+                    if(live<2||live>3) next=0;
                     else next=1;
                 }else{
                     if(live==3) next=1;
@@ -23,8 +21,8 @@ public:
                 }
                 board[i][j]|=next<<1;
             }
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
                 board[i][j]>>=1;
     }
 };
