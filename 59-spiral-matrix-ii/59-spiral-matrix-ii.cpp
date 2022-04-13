ebf983ea7beb49faf2e04474c6308e46963379f8
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-12 22:40:41 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (63.66%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>>res(n,vector<int>(n));
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        for(int x=0,y=0,i=0,d=1;i<n*n;i++){
-            res[x][y]=i+1;
+        int dx[4]={0,1,0,-1};
+        int dy[4]={1,0,-1,0};
+        for(int x=0,y=0,d=0,i=1;i<=n*n;i++){
+            res[x][y]=i;
             int a=x+dx[d],b=y+dy[d];
-            if(a<0 || a==n||b<0||b==n||res[a][b]){
+            if(a<0||a>=n||b<0||b>=n||res[a][b]){
                 d=(d+1)%4;
                 a=x+dx[d],b=y+dy[d];
             }
