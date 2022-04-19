4f68cbd2b8b5515559260d328b9aeac5259af449
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-19 09:02:33 -0700

    Time: 38 ms (70.24%), Space: 57.9 MB (70.41%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -15,17 +15,17 @@ public:
         TreeNode *first=nullptr,*second,*last=nullptr;
         while(root){
             if(!root->left){
-                if(last && last->val>root->val) {
+                if(last && last->val>root->val){
                     if(!first) first=last,second=root;
-                    else second=root; 
+                    else second=root;
                 }
                 last=root;
                 root=root->right;
             }else{
                 auto p=root->left;
-                while(p->right && p->right!=root) p=p->right;
+                while(p->right&& p->right!=root) p=p->right;
                 if(!p->right) p->right=root,root=root->left;
-                else{
+                else {
                     p->right=nullptr;
                     if(last && last->val>root->val){
                         if(!first) first=last,second=root;
