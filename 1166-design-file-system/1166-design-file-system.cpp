ccaeaa80d37bbe999aeb5fb4740dfd2bfa1722af
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-21 22:29:40 -0700

    Time: 250 ms (79.87%), Space: 66.4 MB (99.67%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.cpp
@@ -0,0 +1,33 @@
+class FileSystem {
+    unordered_map<string,int>paths;
+public:
+    FileSystem() {
+        
+    }
+    
+    bool createPath(string path, int value) {
+        if(paths.count(path)) return false;
+        string cur;
+        for(int i=0;i<path.size();i++){
+            if(i==0||path[i]!='/') cur+=path[i];
+            else if(i!=0 && path[i]=='/'){
+                if(paths.count(cur)==0) return false;
+                else cur+=path[i];
+            }
+        }
+        paths[path]=value;
+        return true;
+    }
+    
+    int get(string path) {
+        if(!path.empty() && paths.count(path)) return paths[path];
+        return -1;
+    }
+};
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem* obj = new FileSystem();
+ * bool param_1 = obj->createPath(path,value);
+ * int param_2 = obj->get(path);
+ */
