78bf907c22b1fbcf399ec5788465c65ad623fd44
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-26 08:48:42 -0700

    Time: 1622 ms (9.28%), Space: 292.5 MB (5.05%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int manDist(vector<vector<int>>& points,int i,int j){
+        i--,j--;
+        auto pi=points[i],pj=points[j];
+        return abs(pi[0]-pj[0])+abs(pi[1]-pj[1]);
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size();
+        //printf("n=%d\n",n);
+        bool st[n+1];
+        for(int i=0;i<=n;i++) st[i]=false;
+        int dist[n+1];
+        for(int i=0;i<=n;i++) dist[i]=INT_MAX;
+        //cout<<"hi"<<endl;
+//        memset(dist,0x3f,sizeof dist);
+        int res=0;
+        for(int i=0;i<n;i++){
+            //cout<<"hi1"<<endl;
+            int t=-1;
+            for(int j=1;j<=n;j++)
+                if(!st[j] && (t==-1 || dist[t]>dist[j])) t=j;
+            if(i) res+=dist[t];
+            st[t]=true;
+            //cout<<"t="<<t<<endl;
+            for(int j=1;j<=n;j++){
+                dist[j]=min(dist[j],manDist(points,t,j));
+            //cout<<"j="<<j<<","<<dist[j]<<endl;
+            }
+        }
+        return res;
+    }
+};
