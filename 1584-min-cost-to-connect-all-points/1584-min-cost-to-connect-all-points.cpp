ade40bcc577111ec38530abf250dc9e9e127b48b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-26 09:01:21 -0700

    Time: 955 ms (33.59%), Space: 155.8 MB (19.10%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     int manDist(vector<vector<int>>& points,int i,int j){
-        //i--,j--;
         auto pi=points[i],pj=points[j];
         return abs(pi[0]-pj[0])+abs(pi[1]-pj[1]);
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
-        //printf("n=%d\n",n);
         bool st[n+1];
         memset(st,0,sizeof st);
         int dist[n+1];
         memset(dist,0x3f,sizeof dist);
         int g[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+        for(int i=0;i<n;i++)
+            for(int j=0;j<=i;j++)
                 g[i][j]=g[j][i]=manDist(points,i,j);
-//                printf("%d ",g[i][j]);
-            }
-//            puts("");
-        }
-        
         int res=0;
         for(int i=0;i<n;i++){
-            //cout<<"hi1"<<endl;
             int t=-1;
             for(int j=0;j<n;j++)
                 if(!st[j] && (t==-1 || dist[t]>dist[j])) t=j;
             if(i) res+=dist[t];
             st[t]=true;
-            //cout<<"t="<<t<<endl;
             for(int j=0;j<n;j++){
                 dist[j]=min(dist[j],g[t][j]);
-            //cout<<"j="<<j<<","<<dist[j]<<endl;
             }
         }
         return res;
