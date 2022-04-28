3047fe72f8389590d2311d174ff9013c65ad597c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-27 22:49:49 -0700

    Time: 316 ms (52.68%), Space: 44.9 MB (33.58%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,30 +1,27 @@
 class Solution {
-    int dx[4]={-1,0,1,0},dy[4]={0,1,0,-1};   
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m=heights.size(),n=heights[0].size();
-        int l=0,r=1e6,ans=0;
+        int l=0,r=1e6,res=0;
+        int dx[4]={1,0,-1,0},dy[4]={0,-1,0,1};
         while(l<r){
-            int mid=(l+r)/2;
+            int mid=(l+r)>>1;
             queue<pair<int,int>>q;
             q.push({0,0});
-            vector<int>seen(m*n);
-            seen[0]=1;
-            while(!q.empty()){
+            vector<bool> st(m*n);
+            st[0]=1;
+            while(q.size()){
                 auto [x,y]=q.front();
                 q.pop();
                 for(int i=0;i<4;i++){
                     int nx=x+dx[i];
                     int ny=y+dy[i];
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && !seen[nx*n+ny] && abs(heights[x][y]-heights[nx][ny])<=mid){
-                        q.push({nx,ny});
-                        seen[nx*n+ny]=1;
-                    }
+                    if(nx>=0 && nx<m && ny>=0 && ny<n && !st[nx*n+ny] && abs(heights[x][y]-heights[nx][ny])<=mid) q.push({nx,ny}),st[nx*n+ny]=1;
                 }
             }
-                                                               if(seen[m*n-1]) ans=mid,r=mid;
-                                                               else l=mid+1;
+            if(st[m*n-1]) res=mid,r=mid;
+                else l=mid+1;
         }
-                                                               return ans;
+        return res;
     }
 };
