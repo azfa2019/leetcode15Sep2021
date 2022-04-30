78e79af218a810270b57b3185dac4365bdf9817a
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-30 11:24:50 -0700

    Time: 4 ms (51.49%), Space: 8.3 MB (52.62%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -4,22 +4,19 @@ public:
         unordered_set<string>vers;
         unordered_map<string,unordered_map<string,double>>d;
         for(int i=0;i<equations.size();i++){
-            string a=equations[i][0],b=equations[i][1];
+            auto a=equations[i][0],b=equations[i][1];
             auto c=values[i];
-            d[a][b]=c;
-            d[b][a]=1/c;
-            vers.insert(a);
-            vers.insert(b);
+            d[a][b]=c,d[b][a]=1/c;
+            vers.insert(a),vers.insert(b);
         }
         for(auto k:vers)
             for(auto i:vers)
                 for(auto j:vers)
-                    if(d[i][k] && d[j][k]) d[i][j]=d[i][k]*d[k][j];
+                    if(d[i][k]&&d[k][j]) d[i][j]=d[i][k]*d[k][j];
         vector<double>res;
         for(auto q:queries){
             auto a=q[0],b=q[1];
-            if(d[a][b]) res.push_back(d[a][b]);
-            else res.push_back(-1.0);
+            res.push_back(d[a][b]?d[a][b]:-1);
         }
         return res;
     }
