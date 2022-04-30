754e9c43af6bdb813605c5fc0fc27a93a121e5a6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-04-30 12:55:59 -0700

    Time: 62 ms (78.37%), Space: 176 MB (42.79%) - LeetHub

--- 431-encode-n-ary-tree-to-binary-tree/431-encode-n-ary-tree-to-binary-tree.cpp
+++ 431-encode-n-ary-tree-to-binary-tree/431-encode-n-ary-tree-to-binary-tree.cpp
@@ -35,12 +35,11 @@ public:
         if(!root) return nullptr;
         TreeNode* res=new TreeNode(root->val);
         if(root->children.size()) res->left=encode(root->children[0]);
-        TreeNode* cur=res->left;
+        auto cur=res->left;
         for(int i=1;i<root->children.size();i++){
             cur->right=encode(root->children[i]);
             cur=cur->right;
         }
-        //printBT(res);
         return res;
     }
 	
@@ -53,52 +52,8 @@ public:
             res->children.push_back(decode(cur));
             cur=cur->right;
         }
-        //printNT(res);
         return res;
     }
-    void printBT(const std::string& prefix, const TreeNode* node, bool isLeft)
-{
-    if( node != nullptr )
-    {
-        std::cout << prefix;
-
-        std::cout << (isLeft ? "├──" : "└──" );
-
-        // print the value of the node
-        std::cout << node->val << std::endl;
-
-        // enter the next tree level - left and right branch
-        printBT( prefix + (isLeft ? "│   " : "    "), node->left, true);
-        printBT( prefix + (isLeft ? "│   " : "    "), node->right, false);
-    }
-}
-    void printBT(const TreeNode* node)
-{
-    printBT("", node, false);    
-}
-        void printNT(const std::string& prefix, const Node* node, bool notLast)
-{
-    if( node != nullptr )
-    {
-        std::cout << prefix;
-
-        std::cout << (notLast ? "├──" : "└──" );
-
-        // print the value of the node
-        std::cout << node->val << std::endl;
-
-        // enter the next tree level - left and right branch
-        for(int i=0;i< node->children.size();i++){
-            auto child=node->children[i];
-            if (i!=node->children.size()-1) printNT( prefix + (notLast ? "│   " : "    "), child, true);
-            else printNT( prefix + (notLast ? "│   " : "    "), child, false);
-        }
-    }
-}
-    void printNT(const Node* node)
-{
-    printNT("", node, false);    
-}
 };
 
 // Your Codec object will be instantiated and called as such:
