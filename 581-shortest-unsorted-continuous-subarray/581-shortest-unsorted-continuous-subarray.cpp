f35c277834124f1b83fc97f392deedac3b49b0a2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-02 21:31:02 -0700

    Time: 36 ms (73.26%), Space: 26.3 MB (98.60%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -4,13 +4,12 @@ public:
         int n=nums.size();
         int l=0,r=n-1;
         while(l+1<n && nums[l]<=nums[l+1]) l++;
-        if(l==r)  return 0;
-        while(r-1>=0 && nums[r]>=nums[r-1]) r--;
+        if(l==r) return 0;
+        while(r-1>=0 && nums[r-1]<=nums[r]) r--;
         for(int i=l+1;i<n;i++)
             while(l>=0 && nums[l]>nums[i]) l--;
         for(int i=r-1;i>=0;i--)
             while(r<n && nums[r]<nums[i]) r++;
         return r-l-1;
-        
     }
 };
