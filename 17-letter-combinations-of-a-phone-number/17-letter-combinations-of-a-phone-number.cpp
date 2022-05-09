7fb6e070814319fdeafac522851005b86b70c60d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-08 17:26:45 -0700

    Time: 4 ms (26.23%), Space: 6.6 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,18 @@
 class Solution {
     int n;
     vector<string>res;
-    vector<vector<string>>hash{{},{},{"a","b","c"},{"d","e","f"},{"g","h","i"},{"j","k","l"},{"m","n","o"},{"p","q","r","s"},{"t","u","v"},{"w","x","y","z"}};
+    string hash[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 public:
     vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return res;
         n=digits.size();
-        if(n==0) return {};
-        string s;
-        dfs(0,s,digits);
+        dfs(0,digits,"");
         return res;
     }
-    void dfs(int idx,string& s,string& digits){
-        if(idx==n) {
-            res.push_back(s);
-            return;
-        }
-        for(auto c:hash[digits[idx]-'0']){
-            s+=c;
-            dfs(idx+1,s,digits);
-            s.pop_back();
+    void dfs(int idx,string& digits,string s){
+        if(idx==n) res.push_back(s);
+        else {
+            for(auto c:hash[digits[idx]-'0']) dfs(idx+1,digits,s+c);
         }
     }
 };
