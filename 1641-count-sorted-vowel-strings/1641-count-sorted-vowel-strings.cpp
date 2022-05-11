e26ccbcae3f428dadf2f55aaf62f1411a1778e46
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-11 13:02:58 -0700

    Time: 3 ms (45.24%), Space: 6 MB (34.47%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<int>arr(5,1);
+        vector<int> copy(5);
+        int sum=5;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<5;j++) 
+                if(j==0) copy[j]=sum;
+                else copy[j]=copy[j-1]-arr[j-1];
+            arr=copy;
+            sum=0;
+            for(int j=0;j<5;j++) sum+=arr[j];
+        }
+        return sum;
+    }
+};
