0fcb46a40d78ff140f4befee4970e7ecf158b019
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-11 13:13:13 -0700

    Time: 0 ms (100.00%), Space: 6 MB (58.59%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -5,9 +5,8 @@ public:
         vector<int>copy(5);
         int sum=5;
         for(int i=2;i<=n;i++){
-            for(int j=0;j<5;j++) 
-                if(j==0) copy[j]=sum;
-                else copy[j]=copy[j-1]-arr[j-1];
+            copy[0]=sum;
+            for(int j=1;j<5;j++) copy[j]=copy[j-1]-arr[j-1];
             sum=0;
             for(int j=0;j<5;j++) sum+=copy[j];
             arr=copy;
