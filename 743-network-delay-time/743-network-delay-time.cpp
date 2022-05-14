c846ef8a7a2cd81e8a39cb6a3bb1ed139429abf7
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-13 21:23:49 -0700

    Time: 145 ms (66.76%), Space: 40.5 MB (55.72%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,10 +1,10 @@
 const int N=110,M=6010,INF=0x3f3f3f3f;
-int h[N],w[M],ne[M],e[M],idx;
+int h[N],e[M],ne[M],w[M],idx;
 int dist[N];
 bool st[N];
 class Solution {
     void add(int a,int b,int c){
-        e[idx]=b,w[idx]=c,ne[idx]=h[a],h[a]=idx++;
+        e[idx]=b,w[idx]=c;ne[idx]=h[a],h[a]=idx++;
     }
     void spfa(int start){
         queue<int>q;
@@ -12,7 +12,7 @@ class Solution {
         memset(dist,0x3f,sizeof dist);
         dist[start]=0;
         while(q.size()){
-            auto t=q.front();q.pop();
+            int t=q.front();q.pop();
             st[t]=false;
             for(int i=h[t];~i;i=ne[i]){
                 int j=e[i];
@@ -26,14 +26,17 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         memset(h,-1,sizeof h);
+        //dist[k]=0;
         idx=0;
-        for(auto e:times){
-            int a=e[0],b=e[1],c=e[2];
+        for(auto item:times){
+            int a=item[0],b=item[1],c=item[2];
             add(a,b,c);
         }
-        int res=0;
         spfa(k);
-        for(int i=1;i<=n;i++) res=max(res,dist[i]);
+        int res=0;
+        for(int i=1;i<=n;i++){
+            res=max(res,dist[i]);
+        }
         if(res==INF) res=-1;
         return res;
     }
