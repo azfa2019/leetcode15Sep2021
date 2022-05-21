cc709f65bb61f711f543eabcb8a57c0d1926681c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-21 09:13:41 -0700

    Time: 4 ms (62.05%), Space: 7.9 MB (13.15%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -6,7 +6,7 @@ public:
         int m=o[0].size();
         vector<vector<int>>f(n,vector<int>(m));
         for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
+            for(int j=0;j<m;j++){
                 if(!o[i][j]){
                     if(!i && !j) f[i][j]=1;
                     else{
@@ -14,6 +14,8 @@ public:
                         if(j) f[i][j]+=f[i][j-1];
                     }
                 }
+                
+            }
         return f[n-1][m-1];
     }
 };
