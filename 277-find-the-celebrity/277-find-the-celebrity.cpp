4b2b7836ec1c755b3a93ef263aa1834482e2621f
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-21 19:22:30 -0700

    Time: 174 ms (75.86%), Space: 9.5 MB (69.46%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.cpp
+++ 277-find-the-celebrity/277-find-the-celebrity.cpp
@@ -4,18 +4,11 @@
 class Solution {
 public:
     int findCelebrity(int n) {
-        vector<bool>st(n,true);
+        int res=0;
+        for(int i=1;i<n;i++)
+            if(knows(res,i)) res=i;
         for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++){
-                if(i!=j && st[i]){
-                    if(knows(i,j) || !knows(j,i)) {
-                        st[i]=false;
-                        break;
-                    }else st[j]=false;
-                }
-            }
-        int res=-1;
-        for(int i=0;i<n;i++) if(st[i]) res=i;
+            if((knows(res,i) && i!=res) ||!knows(i,res)) return -1;
         return res;
     }
 };
