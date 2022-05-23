a421e57d3ba278a8033de234ccb5e380f4942be2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-23 01:45:37 -0700

    Time: 98 ms (69.07%), Space: 14.5 MB (54.49%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int m) {
-        vector<int>f(m+1,1e8);
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>f(amount+1,1e8);
         f[0]=0;
-        for(auto v:coins)
-            for(int j=v;j<=m;j++)
-                f[j]=min(f[j],f[j-v]+1);
-        if(f[m]==1e8) return -1;
-        return f[m];
+        for(int c:coins){
+            for(int j=c;j<=amount;j++)
+                f[j]=min(f[j],f[j-c]+1);
+        }
+        if(f[amount]==1e8) return -1;
+        return f[amount];
     }
 };
