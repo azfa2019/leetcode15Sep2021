3828863526a733e32c3b8d28bf4efd42d03e096d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-23 17:50:48 -0700

    Time: 8 ms (37.09%), Space: 7.3 MB (42.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -10,7 +10,6 @@ public:
                     stk.pop();
                     if(stk.size()) res=max(res,i-stk.top());
                     else res=max(res,i-start);
-                    
                 }else start=i;
             }
         }
