921b6f61578adfabe94fcfd32e8f53739fe3e22f
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-24 23:45:27 -0700

    Time: 690 ms (15.47%), Space: 87.7 MB (25.32%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]==b[0]) return a[1]>b[1];
+        else return a[0]<b[0];
+    }
+public:
+    int maxEnvelopes(vector<vector<int>>& w) {
+        sort(w.begin(),w.end(),cmp);
+        vector<int>q;
+        for(auto e:w){
+            if(q.empty() || e[1]>q.back()) q.push_back(e[1]);
+            else{
+                auto iter=lower_bound(q.begin(),q.end(),e[1]);
+                *iter=e[1];
+            }
+        }
+        return q.size();
+    }
+};
