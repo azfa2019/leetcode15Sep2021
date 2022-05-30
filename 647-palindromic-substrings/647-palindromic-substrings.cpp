8a7f85fcce972b3841bcbbf8c21beedb83d3a6e6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-30 10:45:47 -0700

    Time: 3 ms (91.08%), Space: 6.2 MB (96.14%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -3,10 +3,14 @@ public:
     int countSubstrings(string s) {
         int res=0;
         for(int i=0;i<s.size();i++){
-            for(int j=i,k=i;j>=0 && k<s.size();j--,k++) if(s[j]==s[k]) res++;
-            else break;
-            for(int j=i,k=i+1;j>=0 && k<s.size();j--,k++) if(s[j]==s[k]) res++;
-            else break;
+            for(int j=i,k=i;j>=0 && k<s.size();j--,k++){
+                if(s[j]==s[k]) res++;
+                else break;
+            }
+            for(int j=i,k=i+1;j>=0 && k<s.size();j--,k++){
+                if(s[j]==s[k]) res++;
+                else break;
+            }
         }
         return res;
     }
