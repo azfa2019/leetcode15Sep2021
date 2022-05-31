4bc930a7b7721bf4bc7bf2bfa032824b97dcbee3
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-30 18:13:51 -0700

    Time: 0 ms (100.00%), Space: 7.9 MB (43.00%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& o) {
         int m=o.size(),n=o[0].size();
-        if(!m) return 0;
         vector<vector<int>>f(m,vector<int>(n));
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++){
                 if(!o[i][j]){
-                    if(!i && !j) f[i][j]=1;
-                    else{
-                        if(i) f[i][j]+=f[i-1][j];
-                        if(j) f[i][j]+=f[i][j-1];
-                    }
+                    if(i==0 &&j==0) f[i][j]=1;
+                    if(i) f[i][j]+=f[i-1][j];
+                    if(j) f[i][j]+=f[i][j-1];
                 }
             }
         return f[m-1][n-1];
