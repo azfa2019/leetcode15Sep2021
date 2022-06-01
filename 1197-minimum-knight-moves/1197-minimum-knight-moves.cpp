66065d861e4ef529b169f49ca4cc81cbbade10b4
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-05-31 21:33:11 -0700

    Time: 850 ms (35.15%), Space: 48.4 MB (45.32%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int minKnightMoves(int x, int y) {
-        x += 310;
-        y += 310;
-        int ans = 0;
-        queue<pair<int, int>> q;
-        q.push({310, 310});
-        vector<vector<bool>> vis(700, vector<bool>(700));
-        vis[310][310] = true;
-        vector<vector<int>> dirs = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
-        while (!q.empty())
-        {
-            for (int k = q.size(); k > 0; --k)
-            {
-                auto p = q.front();
-                q.pop();
-                if (p.first == x && p.second == y) return ans;
-                for (auto& dir : dirs)
-                {
-                    int c = p.first + dir[0], d = p.second + dir[1];
-                    if (!vis[c][d])
-                    {
-                        vis[c][d] = true;
-                        q.push({c, d});
+        x+=310,y+=310;
+        int ans=0;
+        queue<pair<int,int>>q;
+        q.push({310,310});
+        vector<vector<bool>>vis(700,vector<bool>(700));
+        vis[310][310]=true;
+        vector<vector<int>>dir={{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
+        while (q.size()){
+            int l=q.size();
+            while(l--){
+                auto t=q.front();q.pop();
+                if(t.first==x && t.second==y) return ans;
+                for(auto& d:dir){
+                    int a=t.first+d[0],b=t.second+d[1];
+                    if(!vis[a][b]){
+                        vis[a][b]=true;
+                        q.push({a,b});
                     }
                 }
             }
-            ++ans;
+            ans++;
         }
         return -1;
     }
