2b31f7438c2bbb428162dbbbe188bc070917503d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-06 15:41:08 -0700

    Time: 220 ms (62.43%), Space: 69 MB (16.71%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int>din(n+1),dout(n+1);
-        for(auto t:trust) din[t[1]]++,dout[t[0]]++;
-        int res=-1;
-        for(int i=1;i<=n;i++) if(din[i]==n-1 && dout[i]==0) res=i;
-        return res;
+        for(auto t:trust){
+            din[t[1]]++,dout[t[0]]++;
+        }
+        for(int i=1;i<=n;i++) 
+            if(din[i]==n-1 && dout[i]==0) return i;
+        return -1;
     }
 };
