90ebc33edd9bc857a38f29225e7a2e49b117a8a6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-06 00:44:29 -0700

    Time: 252 ms (82.64%), Space: 9.7 MB (75.40%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         vector<vector<int>>f(m+1,vector<int>(n+1));
-        for(auto str:strs){
+        for(auto s:strs){
             int a=0,b=0;
-            for(auto c:str)
-                c=='0'? a++:b++;
+            for(char c:s) 
+                c=='0'?a++:b++;
             for(int i=m;i>=a;i--)
                 for(int j=n;j>=b;j--)
                     f[i][j]=max(f[i][j],f[i-a][j-b]+1);
         }
         return f[m][n];
+
     }
 };
