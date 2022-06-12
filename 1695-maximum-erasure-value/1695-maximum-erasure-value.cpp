0a153521a22f03f1e95ffddcaf12ede46c391141
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-11 17:15:41 -0700

    Time: 298 ms (84.45%), Space: 103.7 MB (59.54%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int>s(n+1);
+        s[0]=0;
+        for(int i=1;i<=n;i++) s[i]=s[i-1]+nums[i-1];
+        //for(int i:s) cout<<i<<" ";
+        //cout<<endl;
+        unordered_map<int,int>fr;
+        int ans=0;
+        for(int l=0,r=0;r<n;r++){
+            fr[nums[r]]++;
+            while(fr[nums[r]]>1) fr[nums[l]]--,l++;
+            ans=max(ans,s[r+1]-s[l]);
+        }
+        return ans;
+    }
+};
