cd9db5d944bb0eb60d263ebb358fd9aa2e9666f8
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-14 23:02:18 -0700

    Time: 134 ms (54.53%), Space: 13.1 MB (83.98%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool check(string&a, string& b){
+    bool check(string& a,string& b){
         if(a.size()+1!=b.size()) return false;
         int i=0;
         for(auto c:b){
@@ -9,17 +9,18 @@ public:
         return i==a.size();
     }
     int longestStrChain(vector<string>& words) {
+        int n=words.size();
+        vector<int>f(n);
         sort(words.begin(),words.end(),[](string& a,string& b){
             return a.size()<b.size();
         });
-        vector<int>f(words.size());
         int res=0;
-        for(int i=0;i<words.size();i++){
+        for(int i=0;i<n;i++){
             f[i]=1;
             for(int j=0;j<i;j++){
                 if(check(words[j],words[i])) f[i]=max(f[i],f[j]+1);
             }
-            res=max(f[i],res);
+            res=max(res,f[i]);
         }
         return res;
     }
