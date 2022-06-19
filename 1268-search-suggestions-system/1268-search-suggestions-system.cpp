61ce9d29428fad9909ed5382038d787e6128cd65
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-18 18:03:20 -0700

    Time: 285 ms (31.55%), Space: 106.8 MB (27.67%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -6,8 +6,8 @@ class Solution {
 public:
     void insert(Trie* root,string& word){
         Trie* cur=root;
-        for(char& c:word){
-            if(!cur->child.count(c)) cur->child[c]=new Trie();
+        for(char& c:word) {
+            if(!cur->child[c]) cur->child[c]=new Trie();
             cur=cur->child[c];
             cur->words.push(word);
             if(cur->words.size()>3) cur->words.pop();
@@ -15,19 +15,18 @@ public:
     }
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         Trie* root=new Trie();
-        for(string& w:products) insert(root,w);
+        for(string& s:products) insert(root,s);
         vector<vector<string>>ans;
         Trie* cur=root;
         bool flag=false;
         for(char& c:searchWord){
-            if(flag||!cur->child.count(c)) ans.push_back({}),flag=true;
-            else{
+            if(flag||!cur->child.count(c)){
+                ans.push_back({});
+                flag=true;
+            }else{
                 cur=cur->child[c];
                 vector<string>select;
-                while(cur->words.size()){
-                    select.push_back(cur->words.top());
-                    cur->words.pop();
-                }
+                while(cur->words.size()) select.push_back(cur->words.top()), cur->words.pop();
                 reverse(select.begin(),select.end());
                 ans.push_back(move(select));
             }
