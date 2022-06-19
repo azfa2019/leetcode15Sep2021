f54e1f93b6ed58e81a3df8bc3be14fc05263215b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-18 17:35:16 -0700

    Time: 311 ms (29.31%), Space: 106.7 MB (27.83%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,37 @@
+struct Trie{
+    unordered_map<char, Trie*>child;
+    priority_queue<string>words;
+};
+class Solution {
+public:
+    void insert(Trie* root,string& word){
+        Trie* cur=root;
+        for(char& c:word){
+            if(!cur->child.count(c)) cur->child[c]=new Trie();
+            cur=cur->child[c];
+            cur->words.push(word);
+            if(cur->words.size()>3) cur->words.pop();
+        }
+    }
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        Trie* root=new Trie();
+        for(string& w:products) insert(root,w);
+        vector<vector<string>>ans;
+        Trie* cur=root;
+        bool flag=false;
+        for(char& c:searchWord){
+            if(flag||!cur->child.count(c)) ans.push_back({}),flag=true;
+            else{
+                cur=cur->child[c];
+                vector<string>select;
+                while(cur->words.size()){
+                    select.push_back(cur->words.top());
+                    cur->words.pop();
+                }
+                reverse(select.begin(),select.end());
+                ans.push_back(move(select));
+            }
+        }
+        return ans;
+    }
+};
