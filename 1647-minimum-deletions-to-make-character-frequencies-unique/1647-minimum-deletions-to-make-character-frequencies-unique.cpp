0488a73191df076ae2e98a3f145bf5dffd336f1c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-06-28 07:17:33 -0700

    Time: 50 ms (95.84%), Space: 17.4 MB (37.46%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        set<int>st;
+        int a[26]={0};
+        for(char c:s) a[c-'a']++;
+        int cnt=0;
+        for(int i=0;i<26;i++){
+            if(!st.count(a[i])){
+                st.insert(a[i]);
+                continue;
+            }
+            while(st.count(a[i])&& a[i]!=0) a[i]--,cnt++;
+            st.insert(a[i]);
+        }
+        return cnt;
+    }
+};
