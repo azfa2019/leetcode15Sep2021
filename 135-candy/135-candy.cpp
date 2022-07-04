334ce0d93f41d28b4bda2c52f4079226193400d1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-07-03 17:30:39 -0700

    Time: 32 ms (55.29%), Space: 19.6 MB (9.20%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int>f;
+    vector<int>w;
+    int n;
+    int candy(vector<int>& ratings) {
+        n=ratings.size();
+        w=ratings;
+        f.resize(n,-1);
+        int res=0;
+        for(int i=0;i<n;i++) res+=dp(i);
+        return res;
+    }
+    int dp(int x){
+        if(f[x]!=-1) return f[x];
+        f[x]=1;
+        if(x && w[x]>w[x-1]) f[x]=max(dp(x-1)+1,f[x]);
+        if(x+1<n && w[x]>w[x+1]) f[x]=max(f[x],dp(x+1)+1);
+        return f[x];
+    }
+};
