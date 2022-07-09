38c42d8472e904fe78f9a34432fd7ba211095159
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-07-09 08:41:38 -0700

    Time: 358 ms (39.47%), Space: 77.9 MB (90.28%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -4,7 +4,7 @@ public:
         deque<pair<int,int>>dq;
         dq.push_back({nums[0],0});
         for(int i=1;i<nums.size();i++){
-            while(dq.size() && dq.front().second<i-k) dq.pop_front();
+            if(dq.size() && dq.front().second<i-k) dq.pop_front();
             int res=dq.front().first+nums[i];
             while(dq.size() && dq.back().first<=res) dq.pop_back();
             dq.push_back({res,i});
