d68a1fd8ed5af2c9354f6c040ae89d42e35b3c32
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-07-23 08:20:14 -0700

    Time: 108 ms (68.96%), Space: 14.5 MB (59.03%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,9 +4,8 @@ public:
         vector<int>f(m+1,1e9);
         f[0]=0;
         for(int c:coins)
-            for(int j=c;j<=m;j++){
+            for(int j=c;j<=m;j++)
                 f[j]=min(f[j],f[j-c]+1);
-            }
         return f[m]==1e9?-1:f[m];
     }
 };
