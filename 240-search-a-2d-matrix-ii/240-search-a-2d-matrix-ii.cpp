db689e90aaf423bdac547744a902edce357f4b2b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-07-23 23:45:02 -0700

    Time: 179 ms (47.32%), Space: 14.8 MB (88.55%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty()) return false;
+        int i=0,j=matrix[0].size()-1;
+        while(i<matrix.size() && j>=0){
+            int t=matrix[i][j];
+            if(t==target) return true;
+            else if(t>target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
