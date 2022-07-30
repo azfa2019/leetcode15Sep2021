07bd9b98608cb4ff2006e891c2346f7bee42fb64
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-07-29 17:41:05 -0700

    Time: 838 ms (17.08%), Space: 161.7 MB (20.68%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char,int>maxcnt;
+        for(auto& w:words2) {
+            unordered_map<char,int>cnt;
+            for(auto c:w) cnt[c]++;
+            for(auto [k,v]:cnt) maxcnt[k]=max(maxcnt[k],v);
+        }
+        vector<string>res;
+        for(auto& w:words1){
+            unordered_map<char,int>cnt;
+            for(auto c:w) cnt[c]++;
+            bool flag=true;
+            for(auto [k,v]:maxcnt)
+                if(cnt[k]<v) {
+                    flag=false;
+                    break;
+                }
+            if(flag) res.push_back(w);
+        }
+        return res;
+    }
+};
