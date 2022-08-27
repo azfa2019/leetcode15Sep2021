f53bd2495afad8519ec2ea624421a0b652b86942
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-08-26 18:48:45 -0700

    Time: 1678 ms (58.05%), Space: 294.1 MB (25.61%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -5,16 +5,15 @@ public:
         return s[x2][y2]-s[x1-1][y2]-s[x2][y1-1]+s[x1-1][y1-1];
     }
     int maxSumSubmatrix(vector<vector<int>>& w, int K) {
-        int n = w.size(), m = w[0].size();
-        s = vector<vector<int>>(n + 1, vector<int>(m + 1));
-        for (int i = 1; i <= n; i ++ )
-            for (int j = 1; j <= m; j ++ )
-                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + w[i - 1][j - 1];
-
-        int res = INT_MIN;
-        for (int i = 1; i <= m; i ++ )
-            for (int j = i; j <= m; j ++ ) {
-                set<int> S;
+        int n=w.size(),m=w[0].size();
+        s=vector<vector<int>>(n+1,vector<int>(m+1));
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++)
+                s[i][j]=s[i-1][j]+s[i][j-1]-s[i-1][j-1]+w[i-1][j-1];
+        int res=INT_MIN;
+        for(int i=1;i<=m;i++)
+            for(int j=i;j<=m;j++){
+                set<int>S;
                 S.insert(0);
                 for (int k = 1; k <= n; k ++ ) {
                     int si = get(1, i, k, j);
@@ -23,8 +22,6 @@ public:
                     S.insert(si);
                 }
             }
-
         return res;
-
     }
 };
