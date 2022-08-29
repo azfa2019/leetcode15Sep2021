77ea4e74d248581fbf833a7626cb18179b2fdea7
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-08-28 18:05:45 -0700

    Time: 49 ms (64.88%), Space: 12.8 MB (34.20%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,23 +1,21 @@
 class Solution {
-    vector<vector<char>>g;
-    int dx[4]={1,0,-1,0},dy[4]={0,-1,0,1};
 public:
+    vector<vector<char>>g;
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,-1,0,1};
     int numIslands(vector<vector<char>>& grid) {
         g=grid;
         int cnt=0;
         for(int i=0;i<g.size();i++)
             for(int j=0;j<g[0].size();j++)
-                if(g[i][j]=='1') {
-                    dfs(i,j);
-                    cnt++;
-                }
+                if(g[i][j]=='1') dfs(i,j),cnt++;
         return cnt;
     }
     void dfs(int x,int y){
-        g[x][y]=0;
+        g[x][y]='0';
         for(int i=0;i<4;i++){
-            int nx=x+dx[i],ny=y+dy[i];
-            if(nx>=0 && nx<g.size() && ny>=0 && ny<g[0].size() && g[nx][ny]=='1') dfs(nx,ny);
+            int a=dx[i]+x,b=dy[i]+y;
+            if(a>=0&&a<g.size()&&b>=0&&b<g[0].size() && g[a][b]=='1') dfs(a,b);
         }
     }
 };
