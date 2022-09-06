7c6733ade0834bcb2eefc1fb96b18e834fe51e87
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-09-06 08:27:11 -0700

    Time: 85 ms (14.88%), Space: 12.6 MB (34.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int n,m;
     vector<vector<char>>g;
-    int dx[4]{1,0,-1,0},dy[4]={0,-1,0,1};
+    int n,m;
+    int dx[4]={1,0,-1,0},dy[4]={0,-1,0,1};
     int numIslands(vector<vector<char>>& grid) {
         g=grid;
+        int cnt=0;
         n=g.size(),m=g[0].size();
-        int res=0;
         for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
-                if(g[i][j]=='1') dfs(i,j),res++;
-        return res;
+                if(g[i][j]=='1') dfs(i,j),cnt++;
+        return cnt;
     }
     void dfs(int x,int y){
         g[x][y]='0';
