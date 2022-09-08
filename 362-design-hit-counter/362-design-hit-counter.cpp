7525bacf55a35d42e90838f546c90b179c996f8e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-09-07 22:18:30 -0700

    Time: 9 ms (6.13%), Space: 7.4 MB (47.78%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.cpp
@@ -0,0 +1,23 @@
+class HitCounter {
+public:
+    queue<int>q;
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        q.push(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+        while(q.size()&&timestamp-q.front()>=300) q.pop();
+        return q.size();
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
