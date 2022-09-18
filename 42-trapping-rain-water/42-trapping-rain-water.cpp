78c9702da48c46a953d42d352346ab20e3c07b7a
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-09-18 10:32:00 -0700

    Time: 50 ms (5.15%), Space: 20.3 MB (40.49%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -5,12 +5,12 @@ public:
         int res=0;
         for(int i=0;i<height.size();i++){
             int last=0;
-            while(stk.size()&&height[stk.top()]<=height[i]){
-                res+=(height[stk.top()]-last)*(i-stk.top()-1);
+            while(stk.size()&&height[stk.top()]<=height[i]) {
+                res+=(i-1-stk.top())*(height[stk.top()]-last);
                 last=height[stk.top()];
                 stk.pop();
             }
-            if(stk.size()) res+=(i-stk.top()-1)*(height[i]-last);
+            if(stk.size()) res+=(i-1-stk.top())*(height[i]-last);
             stk.push(i);
         }
         return res;
