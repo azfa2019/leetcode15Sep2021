73e5c685330581decffd4dce833f9b2a8c928a55
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-09-28 17:58:33 -0700

    Time: 48 ms (91.78%), Space: 31.8 MB (71.10%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>>heap;
-        for(auto v:arr){
-            heap.push({abs(v-x),v});
-            if(heap.size()>k) heap.pop();
+    vector<int> findClosestElements(vector<int>& arr, int k, int t) {
+        int l=0,r=arr.size()-1;
+        while(l<r){
+            int mid=l+r>>1;
+            if(arr[mid]>=t) r=mid;
+            else l=mid+1;
+        }
+        if(r){
+            int x=arr[r-1],y=arr[r];
+            if(make_pair(abs(x-t),x)<make_pair(abs(y-t),y)) r--;
+        }
+        int i=r,j=r;
+        for(int u=0;u<k-1;u++){
+            if(i-1<0) j++;
+            else if(j+1>=arr.size()) i--;
+            else{
+                int x=arr[i-1],y=arr[j+1];
+                pair<int,int> a{abs(t-x),x},b{abs(t-y),y};
+                if(a<b) i--;
+                else j++;
+            }
         }
         vector<int>res;
-        while(heap.size()) res.push_back(heap.top().second),heap.pop();
-        sort(res.begin(),res.end());
+        for(int u=i;u<=j;u++) res.push_back(arr[u]);
         return res;
+
     }
 };
