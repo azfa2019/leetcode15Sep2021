229885279d13f563df6af7650040847c6bcf7d51
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-10-22 18:09:33 -0700

    Time: 90 ms (39.98%), Space: 21.4 MB (86.33%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int>res(2);
+        for(auto x:nums){
+            int k=abs(x);
+            if(nums[k-1]<0) res[0]=k;
+            nums[k-1]*=-1;
+        }
+        for(int i=0;i<nums.size();i++)
+            if(nums[i]>0&&res[0]!=i+1) res[1]=i+1;
+        return res;
+    }
+};
