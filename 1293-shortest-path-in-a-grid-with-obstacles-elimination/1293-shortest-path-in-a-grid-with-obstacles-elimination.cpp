81e7c10c17a79b674086c7e698a6f2d8a3934321
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2022-10-29 18:04:10 -0700

    Time: 158 ms (59.80%), Space: 31.8 MB (55.89%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,30 @@
+struct Node{
+    int x,y,z;
+};
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& g, int k) {
+        int n=g.size(),m=g[0].size(),INF=1e9;
+        vector<vector<vector<int>>>dist(n,vector<vector<int>>(m,vector<int>(k+1,INF)));
+        queue<Node>q;
+        dist[0][0][0]=0;
+        q.push({0,0,0});
+        int dx[4]={1,0,-1,0},dy[4]={0,-1,0,1};
+        while(q.size()){
+            auto t=q.front();q.pop();
+            int d=dist[t.x][t.y][t.z];
+            if(t.x==n-1&&t.y==m-1) return d;
+            for(int i=0;i<4;i++){
+                int x=t.x+dx[i],y=t.y+dy[i];
+                if(x>=0 && x<n && y>=0 && y<m){
+                    int z=t.z+g[x][y];
+                    if(z<=k && dist[x][y][z]>d+1){
+                        dist[x][y][z]=d+1;
+                        q.push({x,y,z});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
