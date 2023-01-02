a95c7d296c1734a00487908388e0aef0814a638f
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-01-01 17:21:45 -0800

    Time: 0 ms (100.00%), Space: 5.9 MB (89.31%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(word.size()==1) return true;
-        char c=word[0];
-        if(c>='a'&&c<='z'){ //lower case
-            for(int i=1;i<word.size();i++){
-                char cur=word[i];
-                if(cur<'a'||cur>'z') return false;
-            }
-        }else{ //upper case
-            char one=word[1];
-            if(one>='a'&&one<='z'){//lower case
-                for(int i=2;i<word.size();i++)
-                    if(word[i]<'a'||word[i]>'z') return false;
-            }else//upper case
-                for(int i=2;i<word.size();i++)
-                    if(word[i]>='a'&&word[i]<='z') return false;
-        }
-        return true;
+        int s=0;
+        for(int i=0;i<word.size();i++) 
+            if(isupper(word[i])) s++;
+        return s==0||s==word.size()||s==1&&isupper(word[0]);
     }
 };
