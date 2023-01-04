d6da70146bbaa4fbd53bee5b5c8d0f7d230a2cb6
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-01-03 22:22:49 -0800

    Time: 388 ms (52.58%), Space: 104.7 MB (11.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    int check(int x){
-        if(x==1) return -1;
-        if(x%3==0) return x/3;
-        return x/3+1;
-    }
-    int minimumRounds(vector<int>& ta) {
-        sort(ta.begin(),ta.end());
-        ta.push_back(0);
-        vector<int>num;
-        int n=ta.size();
-        int cnt=0;
-        for(int i=0;i<n;i++)
-            if(!i||ta[i]!=ta[i-1]) num.push_back(cnt),cnt=1;
-            else cnt++;
-        int res=0;
-        for(int x:num) {
-            int t=check(x);
-            if(t<0) return -1;
-            res+=t;
+    int minimumRounds(vector<int>& tasks) {
+        map<int, int> cnt;
+        for (int x : tasks) cnt[x]++;
+        int ans = 0;
+        for (auto it = cnt.begin(); it != cnt.end(); it++) {
+            int x = it->second;
+            if (x == 1) return -1;
+            else if (x == 2) ans++;
+            else if (x % 3 == 0) ans += x / 3;
+            else ans += x / 3 + 1;
         }
-        return res;
+        return ans;
     }
 };
