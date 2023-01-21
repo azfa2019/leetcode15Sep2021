12a0fc06d46fef5685d6e4451e4a7534c1d54ad7
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-01-20 22:57:00 -0800

    Time: 0 ms (100.00%), Space: 6.7 MB (42.06%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string>res;
+    string s;
+    vector<string> restoreIpAddresses(string _s) {
+        s=_s;
+        dfs(0,0,"");
+        return res;
+    }
+    void dfs(int u,int k,string path){
+        if(u==s.size()&&k==4) {
+            path.pop_back();
+            res.push_back(path);
+            return;
+        }
+        if(k==4) return;
+        for(int i=u,t=0;i<s.size();i++){
+            if(i>u && s[u]=='0') break;
+            t=t*10+s[i]-'0';
+            if(t<=255) dfs(i+1,k+1,path+to_string(t)+".");
+            else break;
+        }
+    }
+};
