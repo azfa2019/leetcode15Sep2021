3a41317664d25ccf8035b2b7f29a84d339d595f9
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-01-21 16:15:08 -0800

    Time: 235 ms (36.19%), Space: 75.8 MB (34.66%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>>res;
+    vector<vector<bool>>f;
+    vector<string> path;
+    vector<vector<string>> partition(string s) {
+        int n=s.size();
+        f=vector<vector<bool>>(n,vector<bool>(n));
+        for(int j=0;j<n;j++)
+            for(int i=0;i<=j;i++)
+                if(i==j) f[i][j]=true;
+                else if(s[i]==s[j]) if(i+1>j-1||f[i+1][j-1]) f[i][j]=true;
+        dfs(s,0);
+        return res;
+    }
+    void dfs(string&s,int u){
+        if(u==s.size()) res.push_back(path);
+        else{
+            for(int i=u;i<s.size();i++){
+                if(f[u][i]){
+                    path.push_back(s.substr(u,i-u+1));
+                    dfs(s,i+1);
+                    path.pop_back();
+                }
+            }
+        }
+    }
+};
