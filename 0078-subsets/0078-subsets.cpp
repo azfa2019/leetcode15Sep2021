5a540bff8e97ad0e4fa6bb473c284a10c47175e5
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-03-25 12:22:23 -0700

    Time: 0 ms (100.00%), Space: 7.2 MB (56.67%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>>res;
+    vector<int>path;
+    int n;
+    vector<int>nums;
+    vector<vector<int>> subsets(vector<int>& _nums) {
+        nums=_nums;
+        n=nums.size();
+        dfs(0);
+        return res;
+    }
+    void dfs(int start){
+        res.push_back(path);
+        for(int i=start;i<n;i++){
+            path.push_back(nums[i]);
+            dfs(i+1);
+            path.pop_back();
+        }
+    }
+};
