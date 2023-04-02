7f88c35aa5dac3889850d5d227a5291142f3b493
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-04-01 18:36:19 -0700

    Time: 255 ms (57.84%), Space: 101.9 MB (11.92%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+    vector<int>ans;
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long s) {
         sort(potions.begin(),potions.end());
-        for(int &x:spells){
-            x=potions.end()-upper_bound(potions.begin(),potions.end(),(success-1)/x);
-            cout<<x<<endl;
+        int m=potions.size();
+        for(auto& spell:spells){
+            int idx=search(potions,spell,s);
+            ans.push_back(m-idx);
         }
-        return spells;
+        return ans;
+    }
+    int search(vector<int>& potions,int x,long long s){
+        int l=0,r=potions.size();
+        while(l<r){
+            int mid=l+r>>1;
+            if((long long)x*potions[mid]>=s) r=mid;
+            else l=mid+1;
+        }
+        return r;
     }
 };
