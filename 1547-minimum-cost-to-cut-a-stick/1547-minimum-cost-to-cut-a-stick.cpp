f549573182cc2e86789e20ecfe765f188571da99
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-05-27 20:21:50 -0700

    Time: 88 ms (54.53%), Space: 10.2 MB (60.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
-        sort(cuts.begin(),cuts.end());
-        cuts.insert(cuts.begin(),0);
-        cuts.push_back(n);
-        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),2e9));
-        for(int i=0;i<cuts.size()-1;i++) dp[i][i+1]=0;
-        return dfs(0,cuts.size()-1,dp,cuts);
-    }
-    int dfs(int i,int j,vector<vector<int>>&dp,vector<int>&cuts){
-        if(dp[i][j]<2e9) return dp[i][j];
-        for(int k=i+1;k<j;k++)
-            dp[i][j]=min(dp[i][j],dfs(i,k,dp,cuts)+dfs(k,j,dp,cuts)+cuts[j]-cuts[i]);
-        return dp[i][j];
+      int m=cuts.size();
+      sort(cuts.begin(),cuts.end());
+      cuts.insert(cuts.begin(),0);
+      cuts.push_back(n);
+      vector<vector<int>>dp(m+2,vector<int>(m+2,INT_MAX));
+      for(int i=0;i<m+1;i++) dp[i][i+1]=0; //when len=2, there are 2 cuts next to each other, there is no other cut between these two cuts, when there is no cut the score can only be 0
+      //dp[i][j]: minimum score when cutting interval i:j, when doing cuts[i] followed by cuts[j]
+      for(int len=3;len<=m+2;len++){ //len is the # of cuts 
+        for(int i=0;i+len-1<=m+1;i++){
+          int j=i+len-1;
+          for(int k=i+1;k<=j-1;k++){
+            //cout<<i<<","<<j<<","<<k<<""<<endl;
+            dp[i][j]=min(dp[i][j],cuts[j]-cuts[i]+dp[i][k]+dp[k][j]);
+          }
+        }
+      }
+      return dp[0][m+1];
     }
 };
