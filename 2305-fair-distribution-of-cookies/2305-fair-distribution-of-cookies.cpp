cc0ed3d5c1ece78970852daf9e2217971534ecb0
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-06-30 18:06:51 -0700

    Time: 6 ms (90.00%), Space: 7 MB (23.17%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n=cookies.size();
+        vector<int>sum(1<<n);
+        for(int i=1;i<1<<n;i++)
+            for(int j=0;j<n;j++)
+                if(i>>j&1) sum[i]+=cookies[j];
+        vector<int>f(sum);
+        for(int i=1;i<k;i++)
+            for(int j=(1<<n)-1;j;j--)
+                for(int s=j;s;s=(s-1)&j)
+                    f[j]=min(f[j],max(f[j^s],sum[s]));
+        return f[(1<<n)-1];
+    }
+};
