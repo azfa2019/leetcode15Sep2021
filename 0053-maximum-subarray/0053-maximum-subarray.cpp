297a4a95860155ef860e6155aa5d122dd84ae75c
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-07-08 18:52:33 -0700

    Time: 126 ms (50.46%), Space: 67.6 MB (99.69%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res=INT_MIN;
+        for(int i=0,last=0;i<nums.size();i++){
+            last=nums[i]+max(last,0);
+            res=max(res,last);
+        }
+        return res;
+    }
+};
