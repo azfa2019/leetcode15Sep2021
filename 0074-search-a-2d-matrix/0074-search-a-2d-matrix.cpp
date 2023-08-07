656de8dc0204d9e8509ffdff113c1614a1069ea3
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-08-06 17:19:58 -0700

    Time: 3 ms (82.89%), Space: 9.4 MB (52.82%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int l=0,r=m*n-1;
+        while(l<r){
+            int mid=(l+r)>>1;
+            int x=mid/m,y=mid%m;
+            if(matrix[x][y]<target) l=mid+1;
+            else r=mid;
+        }
+        return matrix[l/m][l%m]==target;
+    }
+};
