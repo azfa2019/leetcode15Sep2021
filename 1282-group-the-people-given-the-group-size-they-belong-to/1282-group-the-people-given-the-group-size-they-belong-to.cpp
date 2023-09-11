9196435dcf06316d6cbb85e6e8a3690980f4ca79
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-09-10 17:30:28 -0700

    Time: 13 ms (41.63%), Space: 12.7 MB (80.59%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& g) {
-        unordered_map<int,int>freq;
         unordered_map<int,vector<int>>pos;
-        for(int i=0;i<g.size();i++) pos[g[i]].push_back(i);
         vector<vector<int>> res;
-        for(auto& e:pos) {
-            int cnt=e.second.size(),gsz=e.first;
-            auto group=e.second;
-            for(int i=0;i<cnt/gsz;i++){
-                vector<int>cur;
-                for(int j=0;j<gsz;j++) cur.push_back(group[i*gsz+j]);
-                res.push_back(cur);
+        for(int i=0;i<g.size();i++) {
+            int x=g[i];
+            pos[x].push_back(i);
+            if(pos[x].size()==x) {
+                res.push_back(pos[x]);
+                pos[x].clear();
             }
         }
         return res;
