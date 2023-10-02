0c63be8d9f7f0df76143c6aba7ab22734d2036c2
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-10-02 01:34:11 -0700

    Time: 8 ms (96.79%), Space: 9.9 MB (60.07%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        reverse(s.begin(),s.end());
-        string res="";
         for(int i=0;i<s.size();i++){
             if(s[i]==' ') {
-                res=s[i]+res;
                 continue;
             }
             int j=i;
             while(j<s.size()&&s[j]!=' ') j++;
-            res=s.substr(i,j-i)+res;
+            reverse(s.begin()+i,s.begin()+j);
             i=j-1;
         }
-        return res;
+        return s;
     }
 };
