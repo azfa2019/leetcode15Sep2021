cdeca0e0d72bda58c2dabb11bf51ea09b4d11b51
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-10-22 17:34:17 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (26.74%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+        int r=sqrt(n);
+        if(r*r!=n) return false;
+        return (1<<30)%n==0;
+    }
+};
