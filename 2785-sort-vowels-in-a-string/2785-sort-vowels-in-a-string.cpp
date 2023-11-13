86a2a24c8478a8bfb0c272d59ddc48569b2818d7
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-11-12 18:16:34 -0800

    Time: 65 ms (16.80%), Space: 18.3 MB (6.40%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        unordered_set<char>v{'A','E','I','O','U','a','e','i','o','u'};
+        string tmp;
+        vector<int>pos;
+        for(int i=0;i<s.size();i++) if(v.count(s[i])) tmp+=s[i],pos.push_back(i);
+        sort(tmp.begin(),tmp.end());
+        cout<<tmp<<endl;
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(v.count(s[i])) s[i]=tmp[j++];
+        }
+        return s;
+    }
+};
