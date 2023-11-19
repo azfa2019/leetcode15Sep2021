28f50ea9eed5489f6e228e3d5ed5030e9849c917
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-11-18 16:52:55 -0800

    Time: 208 ms (49.39%), Space: 83.1 MB (88.96%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,19 +2,10 @@ class Solution {
 public:
     typedef pair<int,int> PII;
     int reductionOperations(vector<int>& nums) {
-        unordered_map<int,int>all;
-        
-        for(int n:nums) all[n]++;
-        priority_queue<PII>heap;
-        for(auto& p:all) heap.push(p);
+        sort(nums.rbegin(),nums.rend());
         int cnt=0;
-        while(heap.size()>1){
-            auto t1=heap.top();heap.pop();
-            auto t2=heap.top();heap.pop();
-            t2.second+=t1.second;
-            cnt+=t1.second;
-            heap.push(t2);
-        }
+        for(int i=0;i<nums.size()-1;i++) 
+            if(nums[i]>nums[i+1]) cnt+=i+1;
         return cnt;
     }
 };
