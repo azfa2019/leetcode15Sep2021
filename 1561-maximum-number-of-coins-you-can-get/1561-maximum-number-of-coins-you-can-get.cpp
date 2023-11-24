80b11013e6037a6b98d3ee900730d3350dddaca5
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-11-23 17:44:16 -0800

    Time: 138 ms (5.22%), Space: 53.7 MB (54.72%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -3,9 +3,8 @@ public:
     int maxCoins(vector<int>& piles) {
         sort(piles.rbegin(),piles.rend());
         int n=piles.size(),res=0;
-        for(int i=0,j=1;i<n/3;i++){
-            res+=piles[j];
-            j+=2;
+        for(int i=0;i<n/3;i++){
+            res+=piles[i*2+1];
         }
         return res;
     }
