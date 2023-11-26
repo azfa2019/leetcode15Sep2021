0bc0a386368f48d4190d837fa4b84763d856abb4
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2023-11-25 19:24:30 -0800

    Time: 154 ms (66.48%), Space: 78.6 MB (16.48%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -3,16 +3,17 @@ public:
     int largestSubmatrix(vector<vector<int>>& m) {
         int h=m.size(),w=m[0].size();
         vector<vector<int>>up(h,vector<int>(w,0));
-        for(int i=0;i<h;i++)
+        for(int i=1;i<h;i++)
             for(int j=0;j<w;j++)
-                if(m[i][j]) up[i][j]=(i==0?0:up[i-1][j])+1;
+                if(m[i][j]) m[i][j]+=m[i-1][j];
         int res=0;
         for(int i=0;i<h;i++){
-            vector<int>buf;
-            for(int j=0;j<w;j++)
-                buf.push_back(up[i][j]);
-            sort(buf.begin(),buf.end());
-            for(int j=0;j<w;j++) res=max(res,buf[j]*(w-j));        
+            sort(m[i].begin(),m[i].end());
+            
+            for(int j=w-1;j>=0;j--) {
+                int height=m[i][j];
+                res=max(res,height*(w-j));   
+            }
         }
         return res;
     }
