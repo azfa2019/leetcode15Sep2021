551eb3a10b9db70aef85bcd636d7df8c25afce71
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-01-01 03:33:30 -0800

    Time: 42 ms (8.92%), Space: 18 MB (15.57%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size(),m=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int res=0;
+        for(int i=0,j=0;i<n;i++){
+            while(j<m && g[i]>s[j]) j++;
+            if(j<m && g[i]<=s[j]) res++,j++;
+        }
+        return res;
+    }
+};
