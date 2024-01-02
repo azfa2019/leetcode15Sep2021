e8ebb978bf60fa1ea64e4695e118e09eab37376e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-01-01 16:43:43 -0800

    Time: 29 ms (5.35%), Space: 30 MB (62.82%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        unordered_map<int,int>hash;
-        int mx=0;
-        for(int num:nums) hash[num]++,mx=max(mx,hash[num]);
-        vector<vector<int>>res(mx);
-        for(auto e:hash){
-            int n=e.second,num=e.first;
-            for(int i=0;i<n;i++)
-                res[i].push_back(num);
+        
+        int n=nums.size();
+        int cnt[201]={0};
+        for(int num:nums) cnt[num]++;
+        vector<vector<int>>res;
+        while(n){
+            vector<int>v;
+            for(int i=1;i<=200;i++){
+                if(cnt[i]){
+                    v.push_back(i);
+                    cnt[i]--,n--;
+                }
+            }
+            res.push_back(v);
         }
         return res;
     }
