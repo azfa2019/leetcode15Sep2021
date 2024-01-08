1bd8d983588e6e96d7420b1612e725bf67976a60
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-01-07 17:04:17 -0800

    Time: 4 ms (78.01%), Space: 8.5 MB (37.70%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
@@ -10,19 +10,22 @@
 
 class Solution {
 public:
-    int leftMostColumnWithOne(BinaryMatrix &mat) {
-        auto dim=mat.dimensions();
-        int n=dim[0],m=dim[1];
+    int n,m;
+    int leftMostColumnWithOne(BinaryMatrix &b) {
+        n=b.dimensions()[0],m=b.dimensions()[1];
         int res=m;
-        for(int i=0;i<n;i++){
-            int l=0,r=m-1;
-            while(l<r){
-                int mid=l+r>>1;
-                if(mat.get(i,mid)==1) r=mid;
-                else l=mid+1;
-            }
-            if(mat.get(i,l)==1) res=min(res,l);
-        } 
+        for(int i=0;i<n;i++)
+            res=min(res,helper(i,b));
         return res==m?-1:res;
+    }
+    int helper(int row,BinaryMatrix &b){
+        int l=0,r=m-1;
+        while(l<r){
+            int mid=l+r>>1;
+            if(b.get(row,mid)) r=mid;
+            else l=mid+1;
+        }
+        //cout<<l<<endl;
+        return b.get(row,l)==1?l:m;
     }
 };
