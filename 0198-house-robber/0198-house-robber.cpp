9b6fce069490cccb3626a9e7635e9097cd94714d
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-01-20 16:28:33 -0800

    Time: 4 ms (25.45%), Space: 9.6 MB (9.67%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>f(n,vector<int>(2));
+        f[0][0]=0;
+        f[0][1]=nums[0];
+        for(int i=1;i<n;i++){
+            f[i][0]=max(f[i-1][0],f[i-1][1]);
+            f[i][1]=nums[i]+f[i-1][0];
+        }
+        return max(f[n-1][0],f[n-1][1]);
+    }
+};
