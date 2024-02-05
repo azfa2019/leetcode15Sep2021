83e27c31aaa0d3e42b1c686630e93de1c046e413
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-02-04 19:04:27 -0800

    Time: 35 ms (48.00%), Space: 13.2 MB (10.77%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,13 +3,8 @@ public:
     int firstUniqChar(string s) {
         int n=s.size();
         unordered_map<char,int>cnt;
-        vector<int> pos(26,1e6);
-        for(int i=n-1;i>=0;i--){
-            cnt[s[i]]++;
-            pos[s[i]-'a']=min(pos[s[i]-'a'],i);
-        }
-        for(int i=0;i<n;i++)
-            if(cnt[s[i]]==1) return pos[s[i]-'a'];
+        for(char&c:s) cnt[c]++;
+        for(int i=0;i<n;i++) if(cnt[s[i]]==1) return i;
         return -1;
     }
 };
