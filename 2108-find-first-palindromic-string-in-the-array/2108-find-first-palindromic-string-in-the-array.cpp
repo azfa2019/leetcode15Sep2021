0dbd9a916affd7e359ca3cd2aae41d729bd768fd
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-02-12 16:30:24 -0800

    Time: 53 ms (33.67%), Space: 23.3 MB (75.99%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -3,34 +3,19 @@ public:
     string firstPalindrome(vector<string>& words) {
         string ans="";
         for(string& w:words){
-            if(w.size()%2==0){
-                if(checkeven(w)) {
+            if(check(w)) {
                     ans=w;
                     break;
-                }
-            }
-            else{
-                if(checkodd(w)){
-                    ans=w;
-                    break;
-                }
             }
         }
         return ans;
     }
-    bool checkodd(string& s){
+    bool check(string& s){
         int n=s.size();
-        int c=n/2;
-        for(int i=c,j=c;i>=0 && j<n;i--,j++){
+        int i=0,j=n-1;
+        while(i<=j){
             if(s[i]!=s[j]) return false;
-        }
-        return true;
-    }
-    bool checkeven(string& s){
-        int n=s.size();
-        int c=n/2-1;
-        for(int i=c,j=c+1;i>=0 && j<n;i--,j++){
-            if(s[i]!=s[j]) return false;
+            i++,j--;
         }
         return true;
     }
