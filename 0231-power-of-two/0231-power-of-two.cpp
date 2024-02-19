53f5dbcc0ec052c0c41b3363b33106df049fc120
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-02-18 18:11:40 -0800

    Time: 5 ms (20.09%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        long long res=1;
-        for(int i=0;i<32;i++){
-            if(res==n) return true;
-            res=res*2;
-        }
-        return false;
+        return n>0 && (n&-n)==n;
     }
 };
