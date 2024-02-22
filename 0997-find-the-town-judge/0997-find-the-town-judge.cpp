2dffd478deb02b30f7614520e6ca07f132455907
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-02-21 22:03:54 -0800

    Time: 147 ms (46.70%), Space: 72.4 MB (21.11%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>hash1(n+1),hash2(n+1);
-        
-        for(auto& p:trust) hash1[p[0]]++,hash2[p[1]]++;
-        for(int i=1;i<=n;i++) if(hash1[i]==0&&hash2[i]==n-1) return i;
-        return -1;
+        vector<int>trout(n+1);
+        vector<int>trin(n+1);
+        for(auto t:trust) {
+            trout[t[0]]++;
+            trin[t[1]]++;
+        }
+        int res=-1;
+        for(int i=1;i<=n;i++) if(trout[i]==0 && trin[i]==n-1) res=i;
+        return res;
     }
 };
