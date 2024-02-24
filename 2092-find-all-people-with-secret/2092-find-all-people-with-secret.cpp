1944ef2c401d40487e61c67a00ff99e1920777af
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-02-23 18:45:24 -0800

    Time: 597 ms (47.54%), Space: 239.8 MB (21.72%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-    vector<int>f,sz;
-    int find(int x){
-        return x==f[x]?x:f[x]=find(f[x]);
-    }
-    void add(int x,int y){
-        int fx=find(x),fy=find(y);
-        if(fx==fy) return;
-        if(sz[fx]<sz[fy]){
-            f[fx]=fy;
-            sz[fy]+=sz[fx];
-        }else{
-            f[fy]=fx;
-            sz[fy]+=sz[fx];
+    vector<int>p;
+        int find(int x){
+            if(p[x]!=x) p[x]=find(p[x]);
+            return p[x];
         }
-    }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        f.resize(n);
-        sz.resize(n,1);
-        for(int i=0;i<n;i++) f[i]=i;
-        add(0,firstPerson);
-        sort(meetings.begin(),meetings.end(),[&](const vector<int>&m1,const vector<int>&m2){
-            return m1[2]<m2[2];
-        });
-        int last=0;
-        int m=meetings.size();
-        for(int i=1;i<=m;i++){
-            if(i<m && meetings[i][2]==meetings[i-1][2]) continue;
-            for(int j=last;j<i;j++){
-                int x=meetings[j][0],y=meetings[j][1];
-                add(x,y);
+        p.resize(n+1);
+        for(int i=1;i<=n;i++) p[i]=i;
+        p[firstPerson]=0;
+        map<int,vector<vector<int>>>hash;
+        for(auto& m:meetings){
+            hash[m[2]].push_back({m[0],m[1]});
+        }
+        for(auto h:hash){
+            auto meet=h.second;
+            for(auto& l:meet){
+                int a=l[0],b=l[1];
+                if(p[find(a)]==0||p[find(b)]==0){
+                    p[find(a)]=0;
+                    p[find(b)]=0;
+                }
+                p[find(a)]=p[find(b)];
             }
-            for(int j=last;j<i;j++){
-                int x=meetings[j][0],y=meetings[j][1];
-                if(find(x)==find(0)) continue;
-                f[x]=x,sz[x]=1;
-                f[y]=y,sz[y]=1;
+            for(auto& l:meet){
+                int a=l[0],b=l[1];
+                if(p[find(a)]==0||p[find(b)]==0){
+                    p[find(a)]=0;
+                    p[find(b)]=0;
+                }else{
+                    p[a]=a;
+                    p[b]=b;
+                }
             }
-            last=i;
         }
         vector<int>ans;
         for(int i=0;i<n;i++)
-            if(find(i)==find(0)) ans.push_back(i);
+            if(p[find(i)]==0) ans.push_back(i);
         return ans;
     }
 };
