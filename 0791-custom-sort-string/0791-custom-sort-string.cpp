3f67d8b95b7b82b032ccb16be51c4cc21859f934
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-03-10 19:51:31 -0700

    Time: 5 ms (8.56%), Space: 9.3 MB (8.68%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -2,24 +2,8 @@ class Solution {
 public:
     string customSortString(string order, string s) {
         unordered_map<char,int>pos;
-        unordered_set<char>st;
-        for(char c:order) st.insert(c);
-        string ans;
-        for(char c:order){
-            for(char c2:s){
-                if(c==c2) ans=ans+c;
-            }
-        }
-        for(int i=0;i<s.size();i++) 
-            if(!st.count(s[i])) {
-                int m=ans.size();
-                if(i<m){
-                    ans=ans.substr(0,i+1)+s[i]+ans.substr(i+1);
-                }else if(i==m){
-                    ans=ans.substr(0,i+1)+s[i];
-                }  
-            }
-        //cout<<ans;
-        return ans;
+        for(int i=0;i<order.size();i++) pos[order[i]]=i;
+        sort(s.begin(),s.end(),[&](char a,char b){ return pos[a]<pos[b];});
+        return s;
     }
 };
