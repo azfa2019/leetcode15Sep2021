a595340dab30983129aa20f4285b77b7c366815e
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-03-16 17:36:07 -0700

    Time: 10 ms (71.09%), Space: 20.4 MB (58.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n=intervals.size();
-        if(intervals.empty()) return {newInterval};
+    vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& b) {
+        int n=a.size();
+        int k=0;
         vector<vector<int>>res;
-        int i=0;while(i<n&&intervals[i][1]<newInterval[0]) res.push_back(intervals[i]),i++;
-        int st=newInterval[0],ed=newInterval[1];
-        while(i<n&& intervals[i][0]<=newInterval[1]) st=min(intervals[i][0],st),ed=max(intervals[i][1],ed),i++;
-        res.push_back({st,ed});
-        for(;i<n;i++) res.push_back(intervals[i]);
+        while(k<n && a[k][1]<b[0]) res.push_back(a[k++]);
+        
+        if(k<n){
+            b[0]=min(a[k][0],b[0]);
+            while(k<n && a[k][0]<=b[1]) b[1]=max(b[1],a[k++][1]);
+        }
+        res.push_back(b);
+        while(k<n) res.push_back(a[k++]);
         return res;
     }
 };
