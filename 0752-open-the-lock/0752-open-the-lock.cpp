54424ec129613945eb462379eac0e2c513a0c813
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-04-21 18:35:54 -0700

    Time: 115 ms (82.74%), Space: 38.6 MB (58.98%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-int openLock(vector<string>& deadends, string target) {
-        string start = "0000";
-        if (start == target) return 0;
-        unordered_set<string> S;
-        for (auto& s: deadends) S.insert(s);
-        if (S.count(start)) return -1;
-        queue<string> q;
+    int openLock(vector<string>& deadends, string target) {
+        string start="0000";
+        if(start==target) return 0;
+        unordered_set<string>S;
+        for(auto& d:deadends) S.insert(d);
+        if(S.count(start)) return -1;
+        queue<string>q;
         q.push(start);
-        unordered_map<string, int> dist;
-        dist[start] = 0;
-        while (q.size()) {
-            auto t = q.front();
-            q.pop();
-            for (int i = 0; i < 4; i ++ )
-                for (int j = -1; j <= 1; j += 2) {
-                    auto state = t;
-                    state[i] = (state[i] - '0' + j + 10) % 10 + '0';
-                    if (!dist.count(state) && !S.count(state)) {
-                        dist[state] = dist[t] + 1;
-                        if (state == target) return dist[state];
+        unordered_map<string,int>dist;
+        dist[start]=0;
+        while(q.size()){
+            auto t=q.front();q.pop();
+            for(int i=0;i<4;i++){
+                for(int j=-1;j<=1;j+=2){
+                    auto state=t;
+                    state[i]=(state[i]-'0'+j+10)%10+'0';
+                    
+                    if(!dist.count(state)&& !S.count(state)){
+                        dist[state]=dist[t]+1;
+                        if(state==target) return dist[state];
                         q.push(state);
                     }
+                    
                 }
+            }
         }
         return -1;
     }
-
 };
