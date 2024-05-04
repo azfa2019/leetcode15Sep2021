26419312390e02d9c5bee42b25428debcf9148ec
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-05-03 17:59:43 -0700

    Time: 56 ms (64.90%), Space: 45.5 MB (46.86%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int ans=0;
-        for(int i=0,j=people.size()-1;i<=j;j--){
-            if(people[i]+people[j]<=limit) i++;
-            ans++;
+    int numRescueBoats(vector<int>& p, int l) {
+        sort(p.begin(),p.end());
+        int res=0;
+        for(int i=0,j=p.size()-1;i<=j;j--){
+            if(p[i]+p[j]<=l) i++;
+            res++;
+            
         }
-        return ans;
+        return res;
     }
 };
