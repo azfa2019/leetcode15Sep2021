3845bd10e052ed6440dd20500f412b4c709b8b61
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-05-10 21:45:21 -0700

    Time: 25 ms (69.55%), Space: 26.2 MB (74.86%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,23 @@
+typedef pair<double,int> PDI;
+#define x first
+#define y second
+
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        vector<PDI>q;
+        for(int i=0;i<quality.size();i++) q.push_back({(double)wage[i]/quality[i],quality[i]});
+        sort(q.begin(),q.end());
+        priority_queue<int>heap;
+        double res=1e18,sum=0;
+        for(auto& p:q){
+            heap.push(p.y);
+            sum+=p.y;
+            if(heap.size()>k){
+                sum-=heap.top();heap.pop();
+            }
+            if(heap.size()==k) res=min(res,sum*p.x);
+        }
+        return res;
+    }
+};
