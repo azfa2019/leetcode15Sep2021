49bdf332f2e0f8895d00752992d767ae5417ec10
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-05-14 22:20:38 -0700

    Time: 32 ms (58.33%), Space: 25.9 MB (63.89%) - LeetHub

---
+++ 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfCleanRooms(vector<vector<int>>& room) {
+        int m=room.size(),n=room[0].size();
+        bool vis[m][n][4];
+        memset(vis,false,sizeof vis);
+        int dir[5]={0,1,0,-1,0};
+        int ans=0;
+        function<void(int,int,int)>dfs=[&](int i,int j,int k){
+            if(vis[i][j][k]) return;
+            ans+=room[i][j]==0;
+            room[i][j]=-1;
+            vis[i][j][k]=true;
+            int x=i+dir[k],y=j+dir[k+1];
+            if(x>=0&&x<m&&y>=0&&y<n&&room[x][y]!=1) 
+                dfs(x,y,k);
+            else
+                dfs(i,j,(k+1)%4);
+        };
+        dfs(0,0,0);
+        return ans;
+    }
+};
