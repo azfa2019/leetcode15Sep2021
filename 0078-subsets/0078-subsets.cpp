059405c08dbc22d48e210f038fbc91caea4b85f0
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-05-20 17:41:57 -0700

    Time: 0 ms (100.00%), Space: 8.4 MB (62.90%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int n;
-    vector<vector<int>>ans;
+    vector<vector<int>> ans;
     vector<int> nums;
-    void dfs(vector<int>& path,int cur){
+    vector<vector<int>> subsets(vector<int>& _nums) {
+        vector<int>path;
+        n=_nums.size();
+        nums=_nums;
+        dfs(0,path);
+        return ans;
+    }
+    void dfs(int cur,vector<int>&path){
         ans.push_back(path);
         if(cur==n) return;
         for(int j=cur;j<n;j++){
             path.push_back(nums[j]);
-            dfs(path,j+1);
+            dfs(j+1,path);
             path.pop_back();
         }
     }
-    vector<vector<int>> subsets(vector<int>& _nums) {
-        nums=_nums;
-        n=nums.size();
-        vector<int>path;
-        dfs(path,0);
-        return ans;
-    }
-
 };
