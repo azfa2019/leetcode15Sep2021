5ff8fc2d884c1673f2753989cf0064bd549db1fb
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-06-01 12:56:42 -0700

    Time: 295 ms (49.06%), Space: 146.3 MB (44.34%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& q) {
-        int n=s.size(),m=q.size(),cnt[n+1];
-        cnt[0]=0;
-        vector<bool>ans(m);
+        int n=s.size(),m=q.size();
+        vector<vector<int>>cnt(n+1,vector<int>(26,0));
         for(int i=0;i<n;i++){
-            int bit=1<<(s[i]-'a');
-            cnt[i+1]=cnt[i]^bit;
+            cnt[i+1]=cnt[i];
+            cnt[i+1][s[i]-'a']++;
         }
+        vector<bool>ans(m);
         for(int i=0;i<m;i++){
-            int l=q[i][0],r=q[i][1],k=q[i][2];
-            int cur=__builtin_popcount(cnt[r+1]^cnt[l]);
+            int l=q[i][0],r=q[i][1],k=q[i][2],cur=0;
+            for(int j=0;j<26;j++){
+                cur+=(cnt[r+1][j]-cnt[l][j])%2;
+            }
             ans[i]=cur/2<=k;
         }
         return ans;
