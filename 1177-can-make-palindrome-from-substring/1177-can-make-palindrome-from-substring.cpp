7400c80ac86dcae9a860c551aea59e3470f98c08
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-06-01 09:47:13 -0700

    Time: 197 ms (95.69%), Space: 96.3 MB (100.00%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& q) {
-        int n=s.size(),m=q.size(),sum[n+1];
-        sum[0]=0;
+        int n=s.size(),m=q.size(),cnt[n+1];
+        cnt[0]=0;
+        vector<bool>ans(m);
         for(int i=0;i<n;i++){
             int bit=1<<(s[i]-'a');
-            sum[i+1]=sum[i]^bit;
+            cnt[i+1]=cnt[i]^bit;
         }
-        vector<bool>ans(m);
         for(int i=0;i<m;i++){
-            int l=q[i][0],r=q[i][1],k=q[i][2],cnt;
-            for(int j=0;j<26;j++)
-                cnt=__builtin_popcount(sum[r+1]^sum[l]);
-            ans[i]=cnt/2<=k;
+            int l=q[i][0],r=q[i][1],k=q[i][2];
+            int cur=__builtin_popcount(cnt[r+1]^cnt[l]);
+            ans[i]=cur/2<=k;
         }
         return ans;
     }
