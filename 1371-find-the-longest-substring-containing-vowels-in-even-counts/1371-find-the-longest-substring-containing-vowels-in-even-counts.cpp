f2bd4f830ce067d6c294d9b81f5bf0ba53098250
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-06-02 18:36:50 -0700

    Time: 41 ms (92.34%), Space: 17.4 MB (89.86%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        vector<int>pos(1<<5,-1);
+        int pos[32],n=s.size(),status=0,ans=0;
+        memset(pos,-1,sizeof pos);
         pos[0]=0;
-        int res=0,cur=0;
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<n;i++){
             char c=s[i];
-            if(c=='a') cur^=(1<<0);
-            else if(c=='e') cur^=(1<<1);
-            else if(c=='i') cur^=(1<<2);
-            else if(c=='o') cur^=(1<<3);
-            else if(c=='u') cur^=(1<<4);
-            if(pos[cur]!=-1) res=max(res,i-pos[cur]+1);
-            else pos[cur]=i+1;
+            if(c=='a') status^=(1<<0);
+            else if(c=='e') status^=(1<<1);
+            else if(c=='i') status^=(1<<2);
+            else if(c=='o') status^=(1<<3);
+            else if(c=='u') status^=(1<<4);
+        if(pos[status]!=-1) ans=max(ans,i-pos[status]+1);
+        else pos[status]=i+1;
         }
-        return res;
+        return ans;
+        
     }
 };
