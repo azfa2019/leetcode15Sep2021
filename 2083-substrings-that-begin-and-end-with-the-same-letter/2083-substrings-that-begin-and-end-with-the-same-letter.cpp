7acd553cdbb6e3a8cae9cd7c6b7de8bf1ad7a679
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-06-07 17:42:18 -0700

    Time: 17 ms (35.92%), Space: 10.6 MB (87.38%) - LeetHub

---
+++ 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long numberOfSubstrings(string s) {
+        int cnt[26];
+        long long ans=0;
+        memset(cnt,0,sizeof cnt);
+        for(char& c:s){
+            ans+=++cnt[c-'a'];
+        }
+        return ans;
+    }
+};
