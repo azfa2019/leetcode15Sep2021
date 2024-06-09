88266c3cbb7e01d5fc59da4d142627bd92245726
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-06-08 18:15:17 -0700

    Time: 49 ms (26.35%), Space: 35.2 MB (44.61%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size(),ans=0,sum=0;
         unordered_map<int,int>hash;
         hash[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+        for(int i=1;i<=n;i++){
+            sum+=nums[i-1];
             int t=(sum%k+k)%k;
             if(hash.count(t)) ans+=hash[t];
             hash[t]++;
