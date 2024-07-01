3553308432ee27890753d48b8bf94a0eb8863c2b
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-06-30 18:15:15 -0700

    Time: 19 ms (42.44%), Space: 16.4 MB (86.11%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<int>p;
+    int cnt;
     int find(int x){
         if(p[x]!=x) p[x]=find(p[x]);
         return p[x];
     }
     void merge(int x,int y){
         int px=find(x),py=find(y);
-        if(px!=py) p[px]=py;
+        if(px!=py) p[px]=py,cnt--;
     }
     int earliestAcq(vector<vector<int>>& logs, int n) {
         sort(logs.begin(),logs.end());
         p.resize(n);
         for(int i=0;i<n;i++) p[i]=i;
+        cnt=n;
         for(auto& e:logs){
             int t=e[0],x=e[1],y=e[2];
             merge(x,y);
-            int px=find(0),flag=1;
-            for(int i=1;i<n;i++){
-                if(find(i)!=px) {
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag) return t;
+            if(cnt==1) return t;
         }
         return -1;
     }
