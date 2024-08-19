93b927949ca6b6c124aec5ae60b09d8eed6c2632
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-08-18 17:28:09 -0700

    Time: 0 ms (100.00%), Space: 7.5 MB (55.97%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int res=0;
+        for(int i=2;i<=n/i;i++){
+            while (n%i==0) res+=i,n/=i;
+        }
+        if(n>1) res+=n;
+        return res;
+    }
+};
