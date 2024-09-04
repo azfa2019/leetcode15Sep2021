6304ff9b7f0cd102f6934c15a1f663cdb12e53ef
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-09-03 17:22:35 -0700

    Time: 116 ms (30.11%), Space: 40.6 MB (41.99%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string get(int x,int y){
+        return to_string(x)+"#"+to_string(y);
+    }
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<string>S;
+        for(auto& p:obstacles) S.insert(get(p[0],p[1]));
+        int x=0,y=0,d=0;
+        int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
+        int res=0;
+        for(auto& c:commands){
+            if(c==-2) d=(d+3)%4;
+            else if(c==-1) d=(d+1)%4;
+            else{
+                for(int i=0;i<c;i++){
+                    int a=x+dx[d],b=y+dy[d];
+                    if(S.count(get(a,b))) break;
+                    x=a,y=b;
+                    res=max(res,x*x+y*y);
+                }
+            }
+        }
+        return res;
+    }
+};
