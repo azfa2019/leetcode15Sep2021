da73fecd94edf4f3e90b1756bb28e244734d1135
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-09-22 19:49:46 -0700

    Time: 125 ms (56.76%), Space: 86.8 MB (52.70%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int minExtraChar(string s, vector<string>& d) {
+        unordered_set<string>S(d.begin(),d.end());
         int n=s.size();
-        unordered_set<string>set(d.begin(),d.end());
         vector<int>f(n+1);
         for(int i=0;i<n;i++){
             f[i+1]=f[i]+1;
             for(int j=0;j<=i;j++){
-                if(set.count(s.substr(j,i-j+1))) 
-                   f[i+1]=min(f[i+1],f[j]);
+                if(S.count(s.substr(j,i-j+1))){
+                    f[i+1]=min(f[i+1],f[j]);
+                }
             }
         }
         return f[n];
