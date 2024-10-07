cfa9883cfbe31f1986828e1ae2b3fe3ec15998f1
azfa2019 <53455814+azfa2019@users.noreply.github.com>
2024-10-06 17:07:29 -0700

    Time: 0 ms (100.00%), Space: 11.6 MB (31.24%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>stk;
+        for(auto c:s){
+            if(stk.size())
+                if(stk.top()=='A'&& c=='B' || stk.top()=='C'&&c=='D') {
+                    stk.pop();
+                    continue;
+                }
+            stk.push(c);
+        }
+        return stk.size();
+    }
+};
